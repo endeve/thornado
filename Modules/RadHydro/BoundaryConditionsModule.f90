7c7d08f36bcb132932a789e300566d68ca3f4339
Eirik Endeve <endevee@ornl.gov>
2016-06-15 12:09:41 +0000

    Added boundary conditions for radiation fields (periodic only)

--- Modules/RadHydro/BoundaryConditionsModule.f90
+++ Modules/RadHydro/BoundaryConditionsModule.f90
@@ -1,14 +1,17 @@
 MODULE BoundaryConditionsModule
 
   USE ProgramHeaderModule, ONLY: &
-    nX, bcX
+    nX, bcX, nE
   USE FluidFieldsModule, ONLY: &
     uCF, nCF, uPF, nPF, uAF, nAF
+  USE RadiationFieldsModule, ONLY: &
+    uCR, nCR, nSpecies
 
   IMPLICIT NONE
   PRIVATE
 
   PUBLIC :: ApplyBoundaryConditions_Fluid
+  PUBLIC :: ApplyBoundaryConditions_Radiation
 
 CONTAINS
 
@@ -106,9 +109,63 @@ CONTAINS
 
       CASE DEFAULT
 
+        WRITE(*,*)
+        WRITE(*,'(A5,A41,I2.2)') &
+          '', 'Invalid Boundary Condition for Fluid X1: ', bcX(1)
+        STOP
+
     END SELECT
 
   END SUBROUTINE ApplyBoundaryConditions_Fluid_X1
+
+
+  SUBROUTINE ApplyBoundaryConditions_Radiation
+
+    CALL ApplyBoundaryConditions_Radiation_X1
+
+  END SUBROUTINE ApplyBoundaryConditions_Radiation
+
+
+  SUBROUTINE ApplyBoundaryConditions_Radiation_X1
+
+    INTEGER :: iS, iX2, iX3, iE, iCR
+
+    SELECT CASE ( bcX(1) )
+
+      CASE ( 0 ) ! No Boundary Condition
+
+      CASE ( 1 ) ! Periodic
+
+        DO iS = 1, nSpecies
+          DO iX3 = 1, nX(3)
+            DO iX2 = 1, nX(2)
+              DO iE = 1, nE
+
+                DO iCR = 1, nCR
+
+                  uCR(:,iE,0,iX2,iX3,iCR,iS) &
+                    = uCR(:,iE,nX(1),iX2,iX3,iCR,iS)
+
+                  uCR(:,iE,nX(1)+1,iX2,iX3,iCR,iS) &
+                    = uCR(:,iE,1,iX2,iX3,iCR,iS)
+
+                END DO
+
+              END DO
+            END DO
+          END DO
+        END DO
+
+      CASE DEFAULT
+
+        WRITE(*,*)
+        WRITE(*,'(A5,A45,I2.2)') &
+          '', 'Invalid Boundary Condition for Radiation X1: ', bcX(1)
+        STOP
+
+    END SELECT
+
+  END SUBROUTINE ApplyBoundaryConditions_Radiation_X1
 
 
 END MODULE BoundaryConditionsModule
