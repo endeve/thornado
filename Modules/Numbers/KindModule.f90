af11c38e41254c7926e426abf2c300cecfbe82c5
Eirik Endeve <endevee@ornl.gov>
2016-07-18 19:23:33 +0000

    Additional numbers

--- Modules/Numbers/KindModule.f90
+++ Modules/Numbers/KindModule.f90
@@ -6,6 +6,8 @@ MODULE KindModule
   INTEGER, PUBLIC, PARAMETER :: &
     DP = KIND( 1.d0 )
   REAL(DP), PUBLIC, PARAMETER :: &
-    Pi = ACOS( - 1.0_DP )
+    Pi     = ACOS( - 1.0_DP ), &
+    TwoPi  = 2.0_DP * Pi, &
+    FourPi = 4.0_DP * Pi
 
 END MODULE KindModule
