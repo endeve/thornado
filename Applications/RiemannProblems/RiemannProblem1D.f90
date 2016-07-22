25ce7c85e8a8ac2962e7983ce62f24f0571b375d
Eirik Endeve <endevee@ornl.gov>
2016-07-22 18:03:32 +0000

    Restoring Sod problem as default

--- Applications/RiemannProblems/RiemannProblem1D.f90
+++ Applications/RiemannProblems/RiemannProblem1D.f90
@@ -16,23 +16,23 @@ PROGRAM RiemannProblem1D
   CALL InitializeProgram &
          ( ProgramName_Option = 'RiemannProblem1D', &
            nX_Option &
-             = [ 128, 1, 1 ], &
+             = [ 1024, 1, 1 ], &
            swX_Option &
              = [ 1, 0, 0 ], &
            bcX_Option = [ 2, 0, 0 ], &
            xL_Option = [ 0.0_DP, 0.0_DP, 0.0_DP ], &
            xR_Option = [ 1.0_DP, 1.0_DP, 1.0_DP ], &
-           nNodes_Option = 4, &
+           nNodes_Option = 1, &
            EquationOfState_Option = 'IDEAL', &
            Gamma_IDEAL_Option = 1.4_DP, &
            FluidSolver_Option = 'Euler_DG', &
            FluidRiemannSolver_Option = 'HLLC', &
            EvolveFluid_Option = .TRUE., &
-           nStages_SSP_RK_Option = 2 )
+           nStages_SSP_RK_Option = 1 )
 
   CALL InitializeRiemannProblem1D &
-         ( D_L = 1.0_DP, V_L = [+1.0_DP, 0.0_DP, 0.0_DP], P_L = 1.0d-6, &
-           D_R = 1.0_DP, V_R = [-1.0_DP, 0.0_DP, 0.0_DP], P_R = 1.0d-6 )
+         ( D_L = 1.000_DP, V_L = [0.0_DP, 0.0_DP, 0.0_DP], P_L = 1.0_DP, &
+           D_R = 0.125_DP, V_R = [0.0_DP, 0.0_DP, 0.0_DP], P_R = 0.1_DP )
 
   CALL EvolveFields &
          ( t_begin = 0.0_DP, t_end = 1.0d-0, dt_write = 1.0d-2, &
