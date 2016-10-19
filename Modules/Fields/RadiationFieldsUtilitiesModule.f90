f76a973f6d7879101bbdf3df9cd1fa1c19bf7529
Eirik Endeve <endevee@ornl.gov>
2016-10-19 17:35:00 +0000

    Added initialization of quadrature weights for cell average  of radiation moments

--- Modules/Fields/RadiationFieldsUtilitiesModule.f90
+++ Modules/Fields/RadiationFieldsUtilitiesModule.f90
@@ -3,7 +3,11 @@ MODULE RadiationFieldsUtilitiesModule
   USE KindModule, ONLY: &
     DP
   USE ProgramHeaderModule, ONLY: &
+    nNodesX, &
+    nNodesE, &
     nDOF
+  USE UtilitiesModule, ONLY: &
+    NodeNumber
 
   IMPLICIT NONE
   PRIVATE
@@ -22,7 +26,25 @@ CONTAINS
 
     REAL(DP), DIMENSION(:), INTENT(in) :: w_E, w_X1, w_X2, w_X3
 
+    INTEGER :: iNodeE, iNodeX1, iNodeX2, iNodeX3
+    INTEGER :: iNode
+
     ALLOCATE( Weights(nDOF) )
+
+    DO iNodeX3 = 1, nNodesX(3)
+      DO iNodeX2 = 1, nNodesX(2)
+        DO iNodeX1 = 1, nNodesX(1)
+          DO iNodeE  = 1, nNodesE
+
+            iNode = NodeNumber( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
+
+            Weights(iNode) &
+              = w_E(iNodeE) * w_X1(iNodeX1) * w_X2(iNodeX2) * w_X3(iNodeX3)
+
+          END DO
+        END DO
+      END DO
+    END DO
 
   END SUBROUTINE InitializeRadiationFieldsUtilities
 
