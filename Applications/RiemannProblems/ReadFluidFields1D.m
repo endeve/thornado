b29c86739788271fd60b64c5948f2a8e307734fe
Eirik Endeve <endevee@ornl.gov>
2016-12-12 19:25:44 +0000

    Added electron densities to read

--- Applications/RiemannProblems/ReadFluidFields1D.m
+++ Applications/RiemannProblems/ReadFluidFields1D.m
@@ -1,7 +1,7 @@
 function...
-  [ Time, X, uCF_D, uCF_S1, uCF_S2, uCF_S3, uCF_E,...
-    uPF_D, uPF_V1, uPF_V2, uPF_V3, uPF_E,uAF_P, uAF_T, uAF_Ye,...
-    uAF_S, uAF_E, uAF_Me, uAF_Mp, uAF_Mn, uAF_Gm, uAF_Cs ]...
+  [ Time, X, uCF_D, uCF_S1, uCF_S2, uCF_S3, uCF_E, uCF_Ne,...
+    uPF_D, uPF_V1, uPF_V2, uPF_V3, uPF_E, uPF_Ne, uAF_P, uAF_T,...
+    uAF_Ye, uAF_S, uAF_E, uAF_Me, uAF_Mp, uAF_Mn, uAF_Gm, uAF_Cs ]...
     = ReadFluidFields1D( AppName, FileNumber, Directory )
 
   if( exist( 'Directory', 'var' ) )
@@ -25,21 +25,23 @@ function...
   uCF_S2 = FF(iO+1+03*nX:iO+04*nX);
   uCF_S3 = FF(iO+1+04*nX:iO+05*nX);
   uCF_E  = FF(iO+1+05*nX:iO+06*nX);
-  uPF_D  = FF(iO+1+06*nX:iO+07*nX);
-  uPF_V1 = FF(iO+1+07*nX:iO+08*nX);
-  uPF_V2 = FF(iO+1+08*nX:iO+09*nX);
-  uPF_V3 = FF(iO+1+09*nX:iO+10*nX);
-  uPF_E  = FF(iO+1+10*nX:iO+11*nX);
-  uAF_P  = FF(iO+1+11*nX:iO+12*nX);
-  uAF_T  = FF(iO+1+12*nX:iO+13*nX);
-  uAF_Ye = FF(iO+1+13*nX:iO+14*nX);
-  uAF_S  = FF(iO+1+14*nX:iO+15*nX);
-  uAF_E  = FF(iO+1+15*nX:iO+16*nX);
-  uAF_Me = FF(iO+1+16*nX:iO+17*nX);
-  uAF_Mp = FF(iO+1+17*nX:iO+18*nX);
-  uAF_Mn = FF(iO+1+18*nX:iO+19*nX);
-  uAF_Gm = FF(iO+1+19*nX:iO+20*nX);
-  uAF_Cs = FF(iO+1+20*nX:iO+21*nX);
+  uCF_Ne = FF(iO+1+06*nX:iO+07*nX);
+  uPF_D  = FF(iO+1+07*nX:iO+08*nX);
+  uPF_V1 = FF(iO+1+08*nX:iO+09*nX);
+  uPF_V2 = FF(iO+1+09*nX:iO+10*nX);
+  uPF_V3 = FF(iO+1+10*nX:iO+11*nX);
+  uPF_E  = FF(iO+1+11*nX:iO+12*nX);
+  uPF_Ne = FF(iO+1+12*nX:iO+13*nX);
+  uAF_P  = FF(iO+1+13*nX:iO+14*nX);
+  uAF_T  = FF(iO+1+14*nX:iO+15*nX);
+  uAF_Ye = FF(iO+1+15*nX:iO+16*nX);
+  uAF_S  = FF(iO+1+16*nX:iO+17*nX);
+  uAF_E  = FF(iO+1+17*nX:iO+18*nX);
+  uAF_Me = FF(iO+1+18*nX:iO+19*nX);
+  uAF_Mp = FF(iO+1+19*nX:iO+20*nX);
+  uAF_Mn = FF(iO+1+20*nX:iO+21*nX);
+  uAF_Gm = FF(iO+1+21*nX:iO+22*nX);
+  uAF_Cs = FF(iO+1+22*nX:iO+23*nX);
   
   keyboard
 
