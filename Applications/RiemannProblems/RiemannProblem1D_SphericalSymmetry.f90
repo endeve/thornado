394d5a8d1bf139d3db39cf37d8b0ff1d0cd4a5bf
Eirik Endeve <endevee@ornl.gov>
2016-12-19 15:23:18 +0000

    Updates to driver for spherically symmetric riemann problem

--- Applications/RiemannProblems/RiemannProblem1D_SphericalSymmetry.f90
+++ Applications/RiemannProblems/RiemannProblem1D_SphericalSymmetry.f90
@@ -21,7 +21,7 @@ PROGRAM RiemannProblem1D_SphericalSymmetry
            swX_Option &
              = [ 1, 0, 0 ], &
            bcX_Option &
-             = [ 2, 0, 0 ], &
+             = [ 3, 0, 0 ], &
            xL_Option &
              = [ 0.0_DP, 0.0_DP, 0.0_DP ], &
            xR_Option &
@@ -40,16 +40,26 @@ PROGRAM RiemannProblem1D_SphericalSymmetry
              = 'HLLC', &
            EvolveFluid_Option &
              = .TRUE., &
+           ApplySlopeLimiter_Option &
+             = .TRUE., &
+           BetaTVB_Option &
+             = 0.0d0, &
+           BetaTVD_Option &
+             = 2.0d0, &
+           ApplyPositivityLimiter_Option &
+             = .FALSE., &
            nStages_SSP_RK_Option &
              = 1 )
-  
+
   CALL InitializeRiemannProblem1D &
          ( D_L = 1.000_DP, V_L = [ 0.0_DP, 0.0_DP, 0.0_DP ], P_L = 1.0_DP, &
            D_R = 0.125_DP, V_R = [ 0.0_DP, 0.0_DP, 0.0_DP ], P_R = 0.1_DP, &
            X_D_Option = 1.0_DP )
 
   CALL EvolveFields &
-         ( t_begin = 0.0_DP, t_end = 0.5_DP, dt_write = 0.025_DP, &
+         ( t_begin  = 0.0d-0,  &
+           t_end    = 5.0d-1, &
+           dt_write = 2.5d-2, &
            UpdateFields = SSP_RK )
 
   CALL FinalizeProgram
