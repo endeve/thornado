b71dd695087c74585d96623b00ce9a4fc6618465
Brandon Barker <bbarker5@vols.utk.edu>
2017-04-13 21:16:34 +0000

    Cleaned up the UtilitiesModule a bit (maybe some cleaning left), haven't finished putting all the testing/output in ComputeEigensystem as I've encountered a new compilation error regarding OpacityModule_TABLE and TS.

--- Applications/RiemannProblems_NuclearEOS/ComputeEigensystem_NuclearEOS.f90
+++ Applications/RiemannProblems_NuclearEOS/ComputeEigensystem_NuclearEOS.f90
@@ -23,15 +23,9 @@ PROGRAM ComputeEigensystem_NuclearEOS
 
   REAL(DP), DIMENSION(1)   :: D, T, Y, P, E, H, Tau, N
   REAL(DP), DIMENSION(1)   :: TEMP
-  REAL(DP), DIMENSION(1)   :: dPdD
-  REAL(DP), DIMENSION(1)   :: dPdT
-  REAL(DP), DIMENSION(1)   :: dPdY
-  REAL(DP), DIMENSION(1)   :: dEdT
-  REAL(DP), DIMENSION(1)   :: dPdE, dPdN, dPdTau, dEdY, dEdD
-  REAL(DP), DIMENSION(6,6) :: J, VL, VR, DD, J0, R, L
+  REAL(DP), DIMENSION(6,6) :: A, R, L
   REAL(DP), DIMENSION(3)   :: v
-  REAL(DP), DIMENSION(6)   :: WR, WI, b
-  REAL(DP), DIMENSION(6,6) :: Ax
+  REAL(DP), DIMENSION(6)   :: lambda
 
   REAL(DP), ALLOCATABLE, DIMENSION(:) :: WORK
 
@@ -45,102 +39,29 @@ PROGRAM ComputeEigensystem_NuclearEOS
   Tau = 1/D
 
   CALL InitializeEquationOfState_TABLE( 'EquationOfStateTable.h5' ) 
-  CALL ComputePressure_TABLE( D, T, Y, P, dPdD, dPdT, dPdY )
-  CALL ComputeSpecificInternalEnergy_TABLE( D, T, Y, E, dEdD, dEdT, dEdY )
-
-  dPdE(1) = (1/dEdT(1)) * (dPdT(1)) !* ( (Dyne / Centimeter**2) / (Erg / Gram) )
-  dPdN(1) = ( BaryonMass * Tau(1) ) * ( dPdY(1) - dEdY(1) * dPdE(1) ) !* ( (Dyne / Centimeter**2) / Centimeter**3 )
-  dPdTau(1) = (-Tau(1)**(-2)) * (dPdD(1) - (Y(1)/BaryonMass)*(dPdN(1)) - dEdD(1) * dPdE(1) ) !& 
-          !* ( (Dyne / Centimeter**2) / (Centimeter**3 / Gram) )
-  N(1) = ( ( D(1) / BaryonMass ) * Y(1) ) !* (Centimeter**(-3)) 
-
-  H(1) = ( E(1) + 0.5*(v(1)**2 + v(2)**2 + v(3)**2) + P(1) * Tau(1) )
-
-  CALL ComputeEigenvectors_R(D, T, Y, v(1), v(2), v(3), R, .FALSE.)
+  CALL ComputeEigenvectors_R(D, T, Y, v(1), v(2), v(3), lambda, R, A, .FALSE.)
 
 
-!  print*,"D = ", D(1) / ( Gram / Centimeter**3 )
-!  print*,"P = ", P(1) / (Dyne / Centimeter**2 )
-!  print*,"tau = ",(1 / D(1)) / ( Centimeter**3 / Gram )
-!  print*,"Y = ", Y(1)
-!  print*,"T = ", T(1) / MeV
-!  print*,"v = (", v(1),", ", v(2),", ", v(3),")"
-!  print*,"E = ", E(1) / ( Erg / Gram )
-!  print*,"n = ", N(1) * Centimeter**3 
-!  print*,"dPdD = ", dPdD
-!  print*,"dPdT = ", dPdT
-!  print*,"dPdY = ", dPdY
-!  print*,"dEdT = ", dEdT
-!  print*, " "
-!  print*,"dPdE = ", (1/dEdT(1)) * (dPdT(1))
-!  print*,"dPdN = ", dPdN
-!  print*,"dPdTau =", - dPdD(1) * (1/D(1))**(-2) 
-!  print*,"H = ", H(1)
-  print*," "
-! ! Note to Self: E is epsilon.
+  DO i = 1, 6
+   print*,"Eigenvalue",i,"is: ", lambda(i) / ( Meter / Second  ) / SpeedOfLightMKS
+  END DO
 
-!  J(1,1) = 0
-!  J(1,2) = 1
-!  J(1,3) = 0
-!  J(1,4) = 0
-!  J(1,5) = 0
-!  J(1,6) = 0
-!  J(2,1) = -v(1)**2 - (Tau(1)**2)*dPdTau(1) - Tau(1) * dPdE(1) & 
-!          * (E(1) - 0.5*(v(1)**2 + v(2)**2 + v(3)**2 ) )
-!  J(2,2) = v(1)*( 2 - Tau(1) * dPdE(1) )   
-!  J(2,3) = - dPdE(1) * v(2) * Tau(1)
-!  J(2,4) = - dPdE(1) * v(3) * Tau(1)
-!  J(2,5) = dPdE(1) * Tau(1)
-!  J(2,6) = dPdN(1)
-!  J(3,1) = - v(1) * v(2)
-!  J(3,2) = v(2)
-!  J(3,3) = v(1)
-!  J(3,4) = 0
-!  J(3,5) = 0
-!  J(3,6) = 0
-!  J(4,1) = - v(1) * v(3)
-!  J(4,2) = v(3)
-!  J(4,3) = 0
-!  J(4,4) = v(1)
-!  J(4,5) = 0
-!  J(4,6) = 0
-!  J(5,1) = v(1) * ( -H(1) - dPdTau(1) * (Tau(1)**2) - Tau(1) * dPdE(1) & 
-!          * ( E(1)  - 0.5 * (v(1)**2 + v(2)**2 + v(3)**2)) )
-!  J(5,2) = H(1) - dPdE(1) * v(1)**2 * Tau(1)
-!  J(5,3) = - dPdE(1) * v(1) * v(2) * Tau(1)
-!  J(5,4) = - dPdE(1) * v(1) * v(3) * Tau(1)
-!  J(5,5) = v(1) * ( 1 + dPdE(1) * Tau(1) )
-!  J(5,6) = v(1) * dPdN(1)
-!  J(6,1) = - ( v(1) / BaryonMass ) * Y(1)
-!  J(6,2) = Y(1) / BaryonMass
-!  J(6,3) = 0
-!  J(6,4) = 0
-!  J(6,5) = 0
-!  J(6,6) = v(1)
+!  print*,"Analytical Sound Speed = ", sqrt( Tau(1) * ( N(1) * dPdN(1) + P(1) * dPdE(1) * Tau(1) - dPdTau(1)*Tau(1))) &
+!                    / ( Meter / Second  ) / SpeedOfLightMKS
 
-!  DO i = 2, 6
-!    DO k = 1, 6
-!     print*, "J(",i,",",k,") = ", J(i,k)
-!    END DO
-!  END DO
 
-!  print*," "
+  DD = 0.0d0
 
-!  J0 = J
+  DO i = 1, 6
+   DD(i,i) = lambda(i)
+  END DO
 
-!  LWORK = -1
-
-!  CALL DGEEV('V', 'V', 6, J, 6, WR, WI, VL, 6, VR, 6, TEMP, LWORK, INFO)
-
-!  LWORK = TEMP(1)
-!  ALLOCATE(WORK(LWORK))
-
-!  CALL DGEEV('V', 'V', 6, J, 6, WR, WI, VL, 6, VR, 6, WORK, LWORK, INFO)
-
-!  DD = 0.0d0
+  print*,"AR' - DR' = ", matmul(A0, VR) - matmul(DD,VR)
 
 !  DO i = 1, 6
-!    DD(i,i) = WR(i)
+!    DO k = 1, 6
+!     print*, "A(",i,",",k,") = ", A(i,k)
+!    END DO
 !  END DO
 
 !  print*,"L'J - DL' = ", matmul(transpose(VL),J0) - matmul(DD,transpose(VL))
@@ -148,25 +69,10 @@ PROGRAM ComputeEigensystem_NuclearEOS
 !  DO i = 1, 6
 !    print*,"Eigenvalue",i,"is: ", WR(i) / ( 2.98*10**8 * (Meter / Second) )
 !  END DO
-
+!
 !  print*," "
 !  print*,"Analytical Sound Speed = ", sqrt( Tau(1) * ( N(1) * dPdN(1) + P(1) * dPdE(1) * Tau(1) - dPdTau(1)*Tau(1)) ) & 
 !          / ( 2.98*10**8 * (Meter / Second) )
 
-!print*," "
-!print*,"Left Eigenvectors"
-!print*," "
-
-!DO i = 1, 6
-!    print*,VL(:,i)
-!END DO
-
-!print*," "
-!print*,"Right Eigenvectors"
-!print*," "
-
-!DO i = 1, 6
-!    print*,VR(:,i)
-!END DO
 
 END PROGRAM ComputeEigensystem_NuclearEOS
--- Applications/RiemannProblems_NuclearEOS/UtilitiesModule_NuclearEOS.f90
+++ Applications/RiemannProblems_NuclearEOS/UtilitiesModule_NuclearEOS.f90
@@ -2,10 +2,7 @@ MODULE UtilitiesModule_NuclearEOS
 
   USE KindModule, ONLY: &
     DP
-  USE FluidFieldsModule, ONLY: &
-    nCF!, iCF_D, iCF_S1, iCF_S2, iCF_S3, iCF_E, iCF_Ne, &
-!    nPF, iPF_D, iPF_V1(1), iPF_V2, iPF_V3, iPF_E, iPF_Ne        
-    USE EquationOfStateModule_TABLE, ONLY: &
+  USE EquationOfStateModule_TABLE, ONLY: &
     InitializeEquationOfState_TABLE, &
     ComputePressure_TABLE, &
     ComputeSpecificInternalEnergy_TABLE, &
@@ -16,6 +13,8 @@ MODULE UtilitiesModule_NuclearEOS
     Second, MeV, Meter
   USE PhysicalConstantsModule, ONLY: &
     SpeedOfLightMKS
+  USE UtilitiesModule, ONLY: &
+    WriteMatrix
 
   IMPLICIT NONE
   PRIVATE
@@ -25,18 +24,17 @@ MODULE UtilitiesModule_NuclearEOS
 
 CONTAINS
 
-  SUBROUTINE ComputeEigenvectors_R(D, T, Y, V1, V2, V3, VR, Componentwise)
+  SUBROUTINE ComputeEigenvectors_R(D, T, Y, V1, V2, V3, lambda, VR, A0, Componentwise)
     
     REAL(DP), DIMENSION(1),       INTENT(in)  :: D, T, Y, V1, V2, V3
-    REAL(DP), DIMENSION(6,6),     INTENT(out) :: VR
+    LOGICAL,                      INTENT(in)  :: Componentwise
+    REAL(DP), DIMENSION(6),       INTENT(out) :: lambda
+    REAL(DP), DIMENSION(6,6),     INTENT(out) :: VR, A0
+
     REAL(DP), DIMENSION(1)                    :: dPdE, dPdN, dPdTau, dEdY, dEdD, dEdT, dPdY, dPdT, dPdD
     REAL(DP), DIMENSION(6,6)                  :: A
     REAL(DP), DIMENSION(6)                    :: WR, WI
-    ! What should the dimensions be???
-
-    LOGICAL,                      INTENT(in)  :: Componentwise
-
-    REAL(DP), DIMENSION(1)                    :: k, h, B, E, P, Cs, Tau, TEMP, N
+    REAL(DP), DIMENSION(1)                    :: E, P, Cs, Tau, TEMP, N
     REAL(DP), ALLOCATABLE, DIMENSION(:)       :: WORK
     INTEGER                                   :: INFO, LWORK
 
@@ -50,26 +48,23 @@ CONTAINS
       A(:,6) = [ 0.0_DP, 0.0_DP, 0.0_DP, 0.0_DP, 0.0_DP, 1.0_DP ]
 
     ELSE
-      print*,"One"
-      print*," "
       CALL ComputePressure_TABLE( D, T, Y, P, dPdD, dPdT, dPdY )
       CALL ComputeSpecificInternalEnergy_TABLE( D, T, Y, E, dEdD, dEdT, dEdY )    
-      print*,"Two"
-      print*," "
-! compute Cs
 
       Tau(1) = 1/D(1)
 
       dPdE(1) = (1/dEdT(1)) * (dPdT(1)) 
       dPdN(1) = ( AtomicMassUnit * Tau(1) ) * ( dPdY(1) - dEdY(1) * dPdE(1) )
       dPdTau(1) = (-Tau(1)**(-2)) * (dPdD(1) - (Y(1)/AtomicMassUnit)*(dPdN(1)) - dEdD(1) * dPdE(1) )      
-      N(1) = ( ( D(1) / AtomicMassUnit ) * Y(1) ) 
+      N(1) = ( ( D(1) / AtomicMassUnit ) * Y(1) )
+      H(1) =  (E(1) + 0.5*V1(1)**2 + V2(1)**2 + V3(1)**2 + P(1) *Tau(1) )
 
-      k(1) = ( - N(1) * dPdN(1) + ( dPdTau(1) + dPdE(1) * (E(1) + (V1(1)**2 + V2(1)**2 + V3(1)**2) / 2 ) ) )
-      h(1) = ( Cs(1)**2 ) / (dPdE(1) * Tau(1)) + k(1)
-      B(1) = dPdTau(1) * Tau(1) - dPdE(1) * ( 0.5 * (-V1(1)**2 + V2(1)**2 + V3(1)**2 ) )
-
-      A(:,1) = [ 0.0_DP, 1.0_DP, 0.0_DP, 0.0_DP, 0.0_DP, 0.0_DP]
+      A(1,1) = 0.0_DP
+      A(1,2) = 1.0_DP
+      A(1,3) = 0.0_DP
+      A(1,4) = 0.0_DP
+      A(1,5) = 0.0_DP
+      A(1,6) = 0.0_DP
       A(2,1) = -V1(1)**2 - (Tau(1)**2)*dPdTau(1) - Tau(1) * dPdE(1) &
                * (E(1) - 0.5*(V1(1)**2 + V2(1)**2 + V3(1)**2 ) ) 
       A(2,2) = V1(1)*( 2 - Tau(1) * dPdE(1) )
@@ -77,8 +72,18 @@ CONTAINS
       A(2,4) = - dPdE(1) * V3(1) * Tau(1)
       A(2,5) = dPdE(1) * Tau(1)
       A(2,6) = dPdN(1)
-      A(:,3) = [ -V1(1)*V2(1), V2(1), V1(1), 0.0_DP, 0.0_DP, 0.0_DP]
-      A(:,4) = [ -V1(1)*V3(1), V3(1), 0.0_DP, V1(1), 0.0_DP, 0.0_DP]
+      A(3,1) = - V1(1) * V2(1)
+      A(3,2) = V2(1)
+      A(3,3) = V1(1)
+      A(3,4) = 0
+      A(3,5) = 0
+      A(3,6) = 0
+      A(4,1) = - V1(1) * V3(1)
+      A(4,2) = V3(1)
+      A(4,3) = 0
+      A(4,4) = V1(1)
+      A(4,5) = 0
+      A(4,6) = 0
       A(5,1) = V1(1) * ( -H(1) - dPdTau(1) * (Tau(1)**2) - Tau(1) * dPdE(1) &
                * ( E(1)  - 0.5 * (V1(1)**2 + V2(1)**2 + V3(1)**2)) )
       A(5,2) = H(1) - dPdE(1) * V1(1)**2 * Tau(1)
@@ -86,20 +91,23 @@ CONTAINS
       A(5,4) = - dPdE(1) * V1(1) * V3(1) * Tau(1)
       A(5,5) = V1(1) * ( 1 + dPdE(1) * Tau(1) )
       A(5,6) = V1(1) * dPdN(1)
-      A(:,6) = [ - ( V1(1) / AtomicMassUnit ) * Y(1), Y(1) / AtomicMassUnit, 0.0_DP, 0.0_DP, 0.0_DP, V1(1) ]
+      A(6,1) = - ( V1(1) / AtomicMassUnit ) * Y(1)
+      A(6,2) = Y(1) / (AtomicMassUnit)
+      A(6,3) = 0.0_DP
+      A(6,4) = 0.0_DP
+      A(6,5) = 0.0_dp
+      A(6,6) = V1(1)
 
-      print*,"Three"
-      print*," "
-               
+      A0 = A
+
       LWORK = -1
 
       CALL DGEEV('N', 'V', 6, A, 6, WR, WI, 0, 6, VR, 6, TEMP, LWORK, INFO)
+
       LWORK = TEMP(1)
       ALLOCATE(WORK(LWORK))
 
-      CALL DGEEV('N', 'V', 6, A, 6, WR, WI, 0, 6, VR, 6, WORK, LWORK, INFO)  
-
-      print*,"Four"
+      CALL DGEEV('N', 'V', 6, A, 6, WR, WI, 0, 6, VR, 6, WORK, LWORK, INFO) 
 
     END IF
 
@@ -107,10 +115,12 @@ CONTAINS
   END SUBROUTINE ComputeEigenvectors_R
 
 
-  SUBROUTINE ComputeEigenvectors_L(D, T, Y, V1, V2, V3, VL, Componentwise)
+  SUBROUTINE ComputeEigenvectors_L(D, T, Y, V1, V2, V3, lambda, VL, A0, Componentwise)
 
     REAL(DP), DIMENSION(1),   INTENT(in)      :: D, T, Y, V1, V2, V3
-    REAL(DP), DIMENSION(6,6), INTENT(out)     :: VL
+    REAL(DP), DIMENSION(6),   INTENT(out)     :: lambda
+    REAL(DP), DIMENSION(6,6), INTENT(out)     :: VL, A0
+
     REAL(DP), DIMENSION(1)                    :: dPdE, dPdN, dPdTau, dEdY, dEdD, dEdT, dPdY, dPdT, dPdD
     REAL(DP), DIMENSION(1)                    :: Tau, TEMP, N
     REAL(DP), DIMENSION(6,6)                  :: A
@@ -136,20 +146,20 @@ CONTAINS
       CALL ComputePressure_TABLE( D, T, Y, P, dPdD, dPdT, dPdY )
       CALL ComputeSpecificInternalEnergy_TABLE( D, T, Y, E, dEdD, dEdT, dEdY )
 
-! compute Cs
-
       Tau(1) = 1/D(1)
 
       dPdE(1) = (1/dEdT(1)) * (dPdT(1))
       dPdN(1) = ( AtomicMassUnit * Tau(1) ) * ( dPdY(1) - dEdY(1) * dPdE(1) )
       dPdTau(1) = (-Tau(1)**(-2)) * (dPdD(1) - (Y(1)/AtomicMassUnit)*(dPdN(1)) - dEdD(1) * dPdE(1) )
       N(1) = ( ( D(1) / AtomicMassUnit ) * Y(1) )
+      H(1) =  (E(1) + 0.5*V1(1)**2 + V2(1)**2 + V3(1)**2 + P(1) *Tau(1) ) 
 
-      k(1) = ( - N(1) * dPdN(1) + ( dPdTau(1) + dPdE(1) * (E(1) + (V1(1)**2 + V2(1)**2 + V3(1)**2) / 2 ) ) )
-      h(1) = ( Cs(1)**2 ) / (dPdE(1) * Tau(1)) + k(1)
-      B(1) = dPdTau(1) * Tau(1) - dPdE(1) * ( 0.5 * (-V1(1)**2 + V2(1)**2 + V3(1)**2 ) )
-
-      A(:,1) = [ 0.0_DP, 1.0_DP, 0.0_DP, 0.0_DP, 0.0_DP, 0.0_DP]
+      A(1,1) = 0.0_DP
+      A(1,2) = 1.0_DP
+      A(1,3) = 0.0_DP
+      A(1,4) = 0.0_DP
+      A(1,5) = 0.0_DP
+      A(1,6) = 0.0_DP
       A(2,1) = -V1(1)**2 - (Tau(1)**2)*dPdTau(1) - Tau(1) * dPdE(1) &
                * (E(1) - 0.5*(V1(1)**2 + V2(1)**2 + V3(1)**2 ) )
       A(2,2) = V1(1)*( 2 - Tau(1) * dPdE(1) )
@@ -157,8 +167,18 @@ CONTAINS
       A(2,4) = - dPdE(1) * V3(1) * Tau(1)
       A(2,5) = dPdE(1) * Tau(1)
       A(2,6) = dPdN(1)
-      A(:,3) = [ -V1(1)*V2(1), V2(1), V1(1), 0.0_DP, 0.0_DP, 0.0_DP]
-      A(:,4) = [ -V1(1)*V3(1), V3(1), 0.0_DP, V1(1), 0.0_DP, 0.0_DP]
+      A(3,1) = - V1(1) * V2(1)
+      A(3,2) = V2(1)
+      A(3,3) = V1(1)
+      A(3,4) = 0
+      A(3,5) = 0
+      A(3,6) = 0
+      A(4,1) = - V1(1) * V3(1)
+      A(4,2) = V3(1)
+      A(4,3) = 0
+      A(4,4) = V1(1)
+      A(4,5) = 0
+      A(4,6) = 0
       A(5,1) = V1(1) * ( -H(1) - dPdTau(1) * (Tau(1)**2) - Tau(1) * dPdE(1) &
                * ( E(1)  - 0.5 * (V1(1)**2 + V2(1)**2 + V3(1)**2)) )
       A(5,2) = H(1) - dPdE(1) * V1(1)**2 * Tau(1)
@@ -166,8 +186,12 @@ CONTAINS
       A(5,4) = - dPdE(1) * V1(1) * V3(1) * Tau(1)
       A(5,5) = V1(1) * ( 1 + dPdE(1) * Tau(1) )
       A(5,6) = V1(1) * dPdN(1)
-      A(:,6) = [ - ( V1(1) / AtomicMassUnit ) * Y(1), Y(1) / AtomicMassUnit, 0.0_DP, 0.0_DP, 0.0_DP, V1(1) ]
-
+      A(6,1) = - ( V1(1) / AtomicMassUnit ) * Y(1)
+      A(6,2) = Y(1) / (AtomicMassUnit)
+      A(6,3) = 0.0_DP
+      A(6,4) = 0.0_DP
+      A(6,5) = 0.0_dp
+      A(6,6) = V1(1)
 
       LWORK = -1
 
