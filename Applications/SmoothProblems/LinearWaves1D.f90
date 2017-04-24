e24155b7f7813b4779d05af9d1b3cf81fbf83639
Eirik Endeve <endevee@ornl.gov>
2017-04-24 20:17:15 +0000

    Updates to LinearWaves1D

--- Applications/SmoothProblems/LinearWaves1D.f90
+++ Applications/SmoothProblems/LinearWaves1D.f90
@@ -14,26 +14,48 @@ PROGRAM LinearWaves1D
   IMPLICIT NONE
 
   CALL InitializeProgram &
-         ( ProgramName_Option = 'LinearWaves1D', &
+         ( ProgramName_Option &
+             = 'LinearWaves1D', &
            nX_Option &
-             = [ 64, 1, 1 ], &
+             = [ 16, 1, 1 ], &
            swX_Option &
              = [ 1, 0, 0 ], &
-           bcX_Option = [ 1, 0, 0 ], &
-           xL_Option = [ 0.0_DP, 0.0_DP, 0.0_DP ], &
-           xR_Option = [ 1.0_DP, 1.0_DP, 1.0_DP ], &
-           nNodes_Option = 3, &
-           EquationOfState_Option = 'IDEAL', &
-           Gamma_IDEAL_Option = 5.0_DP / 3.0_DP, &
-           FluidSolver_Option = 'Euler_DG', &
-           FluidRiemannSolver_Option = 'HLLC', &
-           EvolveFluid_Option = .TRUE., &
-           nStages_SSP_RK_Option = 3 )
+           bcX_Option &
+             = [ 1, 0, 0 ], &
+           xL_Option &
+             = [ 0.0_DP, 0.0_DP, 0.0_DP ], &
+           xR_Option &
+             = [ 1.0_DP, 1.0_DP, 1.0_DP ], &
+           nNodes_Option &
+             = 3, &
+           EquationOfState_Option &
+             = 'IDEAL', &
+           Gamma_IDEAL_Option &
+             = 5.0_DP / 3.0_DP, &
+           FluidSolver_Option &
+             = 'Euler_DG', &
+           FluidRiemannSolver_Option &
+             = 'HLLC', &
+           EvolveFluid_Option &
+             = .TRUE., &
+           ApplySlopeLimiter_Option &
+             = .TRUE., &
+           BetaTVB_Option &
+             = 0.0d0, &
+           BetaTVD_Option &
+             = 1.0d0, &
+           ApplyPositivityLimiter_Option &
+             = .FALSE., &
+           nStages_SSP_RK_Option &
+             = 3 )
 
-  CALL InitializeLinearWaves1D( 'EntropyWaves' )
+  CALL InitializeLinearWaves1D &
+         ( 'AcousticWaves', Amplitude_Option = 1.0d-5 )
 
   CALL EvolveFields &
-         ( t_begin = 0.0_DP, t_end = 2.0d-0, dt_write = 1.0d-1, &
+         ( t_begin  = 0.0d-0, &
+           t_end    = 1.0d+1, &
+           dt_write = 1.0d-1, &
            UpdateFields = SSP_RK )
 
   CALL FinalizeProgram
--- Applications/SmoothProblems/ReadFluidFields1D.m
+++ Applications/SmoothProblems/ReadFluidFields1D.m
@@ -1,39 +1,51 @@
 function...
-  [ X, uCF_D, uCF_S1, uCF_S2, uCF_S3, uCF_E, uPF_D, uPF_V1, uPF_V2, uPF_V3, uPF_E,...
-    uAF_P, uAF_T, uAF_Ye, uAF_S, uAF_E, uAF_Me, uAF_Mp, uAF_Mn, uAF_Gm, uAF_Cs ]...
-    = ReadFluidFields1D( AppName, FileNumber )
+  [ Time, X, uCF_D, uCF_S1, uCF_S2, uCF_S3, uCF_E, uCF_Ne,...
+    uPF_D, uPF_V1, uPF_V2, uPF_V3, uPF_E, uPF_Ne, uAF_P, uAF_T,...
+    uAF_Ye, uAF_S, uAF_E, uAF_Me, uAF_Mp, uAF_Mn, uAF_Gm, uAF_Cs,...
+    Shock ]...
+    = ReadFluidFields1D( AppName, FileNumber, Directory )
 
-  FileName = [ './Output/' AppName '_FluidFields_'...
+  if( exist( 'Directory', 'var' ) )
+    DirName = Directory;
+  else
+    DirName = './Output';
+  end
+
+  FileName = [ DirName '/' AppName '_FluidFields_'...
                  sprintf( '%06d', FileNumber ) '.dat' ];
 
   FF = load( FileName )';
 
-  iO = 1;
+  iO = 2;
 
-  nX     = FF(1);
+  Time   = FF(1);
+  nX     = FF(2);
   X      = FF(iO+1+00*nX:iO+01*nX);
   uCF_D  = FF(iO+1+01*nX:iO+02*nX);
   uCF_S1 = FF(iO+1+02*nX:iO+03*nX);
   uCF_S2 = FF(iO+1+03*nX:iO+04*nX);
   uCF_S3 = FF(iO+1+04*nX:iO+05*nX);
   uCF_E  = FF(iO+1+05*nX:iO+06*nX);
-  uPF_D  = FF(iO+1+06*nX:iO+07*nX);
-  uPF_V1 = FF(iO+1+07*nX:iO+08*nX);
-  uPF_V2 = FF(iO+1+08*nX:iO+09*nX);
-  uPF_V3 = FF(iO+1+09*nX:iO+10*nX);
-  uPF_E  = FF(iO+1+10*nX:iO+11*nX);
-  uAF_P  = FF(iO+1+11*nX:iO+12*nX);
-  uAF_T  = FF(iO+1+12*nX:iO+13*nX);
-  uAF_Ye = FF(iO+1+13*nX:iO+14*nX);
-  uAF_S  = FF(iO+1+14*nX:iO+15*nX);
-  uAF_E  = FF(iO+1+15*nX:iO+16*nX);
-  uAF_Me = FF(iO+1+16*nX:iO+17*nX);
-  uAF_Mp = FF(iO+1+17*nX:iO+18*nX);
-  uAF_Mn = FF(iO+1+18*nX:iO+19*nX);
-  uAF_Gm = FF(iO+1+19*nX:iO+20*nX);
-  uAF_Cs = FF(iO+1+20*nX:iO+21*nX);
-
-  keyboard
+  uCF_Ne = FF(iO+1+06*nX:iO+07*nX);
+  uPF_D  = FF(iO+1+07*nX:iO+08*nX);
+  uPF_V1 = FF(iO+1+08*nX:iO+09*nX);
+  uPF_V2 = FF(iO+1+09*nX:iO+10*nX);
+  uPF_V3 = FF(iO+1+10*nX:iO+11*nX);
+  uPF_E  = FF(iO+1+11*nX:iO+12*nX);
+  uPF_Ne = FF(iO+1+12*nX:iO+13*nX);
+  uAF_P  = FF(iO+1+13*nX:iO+14*nX);
+  uAF_T  = FF(iO+1+14*nX:iO+15*nX);
+  uAF_Ye = FF(iO+1+15*nX:iO+16*nX);
+  uAF_S  = FF(iO+1+16*nX:iO+17*nX);
+  uAF_E  = FF(iO+1+17*nX:iO+18*nX);
+  uAF_Me = FF(iO+1+18*nX:iO+19*nX);
+  uAF_Mp = FF(iO+1+19*nX:iO+20*nX);
+  uAF_Mn = FF(iO+1+20*nX:iO+21*nX);
+  uAF_Gm = FF(iO+1+21*nX:iO+22*nX);
+  uAF_Cs = FF(iO+1+22*nX:iO+23*nX);
+  Shock  = FF(iO+1+23*nX:iO+24*nX);
   
+  keyboard
+
 end
 
