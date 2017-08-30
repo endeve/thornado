fc0b148bdf0c38e66ee9fa58e8dece93253db401
Ran Chu <rchu@vols.utk.edu>
2017-08-30 16:27:03 +0000

    Completed ComputeTimestepPenalization routine.

--- Applications/TransportProblems_Penalization/TimeSteppingModule_Penalization.f90
+++ Applications/TransportProblems_Penalization/TimeSteppingModule_Penalization.f90
@@ -5,12 +5,16 @@ MODULE TimeSteppingModule_Penalization
   USE UnitsModule, ONLY: &
     UnitsDisplay, &
     Millisecond
+  USE UtilitiesModule, ONLY: &
+    NodeNumberX, &
+    NodeNumber
   USE ProgramHeaderModule, ONLY: &
     nX, nNodesX, nDOFX, &
     nE, nNodesE, nDOFE, &
     nDOF
   USE RadiationFieldsModule, ONLY: &
-    iPR_D
+    iCR_N, &
+    iPR_D, uPR
   USE InputOutputModule, ONLY: &
     WriteFields1D
   USE FluidRadiationCouplingUtilitiesModule, ONLY: &
@@ -159,53 +163,63 @@ CONTAINS
     REAL(DP),                 INTENT(in)  :: dt_max, Diff
    
     REAL(DP)                  :: dt_old, dt_i, dt_1, dt_2, &
-                                 LAMB, invLAMB, LNMax, lim
-!    REAL(DP), DIMENSION(nNodesE_G) :: NN, Collision, &
-!                                      lim1sign, lim1, lim1p, &
-!                                      lim2sign, lim2, lim2p
+                                 LAMB, LNMax, lim, lim1max, lim2max
+    REAL(DP), DIMENSION(nNodesE,nE) :: NN, Coll, OnesMat, &
+                                       lim1, lim2
 
-    INTEGER :: iX1, iX2, iX3
-    INTEGER :: iNodeX1, iNodeX2, iNodeX3, iNodeX_G
-
-    dt_old = dt
+    INTEGER :: iE, iX1, iX2, iX3
+    INTEGER :: iNodeE, iNode
+    INTEGER :: iNodeX1, iNodeX2, iNodeX3, iNodeX
 
     dt = 1.0d-2 * Millisecond
     
+    dt_old = dt
+
     dt_i = dt_old
 
-    iNodeX_G = 0
     DO iX3 = 1, nX(3)
       DO iX2 = 1, nX(2)
         DO iX1 = 1, nX(1)
           DO iNodeX3 = 1, nNodesX(3)
             DO iNodeX2 = 1, nNodesX(2)
               DO iNodeX1 = 1, nNodesX(1)
-                
-                iNodeX_G = iNodeX_G + 1
-!                iNodeX = NodeNumberX( iNodeX1, iNodeX2, iNodeX3 )
 
-!                Collision = RHS_J(:,iNodeX_G)
+                iNodeX = NodeNumberX( iNodeX1, iNodeX2, iNodeX3 )
 
-!                LAMB = absLambda(iNodeX,iX1,iX2,iX3)
+                LAMB   = MAX( absLambda(iNodeX,iX1,iX2,iX3), 1.0d-100 )
 
-!                NN = uPR_N(:,iPR_D,iX)
-!
-                invLAMB = 1.0 / LAMB
-!
+                DO iE = 1, nE
+                  DO iNodeE = 1, nNodesE
+
+                    iNode = NodeNumber( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
+
+                    Coll(iNodeE,iE) = rhsCR_C(iNode,iE,iX1,iX2,iX3,iCR_N,1) 
+                     NN (iNodeE,iE) =  uPR   (iNode,iE,iX1,iX2,iX3,iPR_D,1)
+
+                  END DO
+                END DO
+
 !               ! --- Boundary Limit ---
-!    
-!                lim = MINVAL( lim1p + lim2p )    
-!
-                IF( lim < invLAMB )THEN
-                  dt_1 = lim / ( 1.0 - lim * LAMB )
+   
+                OnesMat = 1.0
+
+                lim1 = - Coll / NN
+                lim2 =   Coll / ( OnesMat - NN )
+
+                lim1max = MAXVAL( lim1 ) 
+                lim2max = MAXVAL( lim2 ) 
+                    lim = MAX( lim1max, lim2max ) 
+
+                IF( lim > LAMB )THEN
+                  dt_1 = 1.0 / ( lim - LAMB )
                 ELSE
                   dt_1 = dt_max
                 END IF
              
                 ! --- Accuracy Limit ---
               
-!                LNMax = MAXVAL( ABS( MATMUL(Collision,NN) / NN ) )
-!            
+                LNMax = MAXVAL( ABS( Coll / NN ) )
+            
                 dt_2 = Diff / ( 2.0 * LNMax ) + &
                        SQRT( Diff / ( LAMB * LNMax ) + &
                              Diff**2 / ( 4.0 * LNMax**2 ) )
