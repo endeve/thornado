ef9efb7a7ba49c18259392ffd393d131835af94a
Ran Chu <rchu@vols.utk.edu>
2017-09-20 16:35:27 +0000

    Keep updates.

--- Applications/TransportProblems_Penalization/TimeSteppingModule_Penalization.f90
+++ Applications/TransportProblems_Penalization/TimeSteppingModule_Penalization.f90
@@ -9,15 +9,24 @@ MODULE TimeSteppingModule_Penalization
     NodeNumberX, &
     NodeNumber
   USE ProgramHeaderModule, ONLY: &
+    nX, nDOFX, nNodes, &
+    nE, nDOF
+  USE MeshModule, ONLY: &
+    MeshX
+  USE GeometryFieldsModule, ONLY: &
+    a, b, c
+  USE ProgramHeaderModule, ONLY: &
     nX, nNodesX, nDOFX, &
     nE, nNodesE, nDOFE, &
     nDOF
   USE RadiationFieldsModule, ONLY: &
-    iCR_N, uCR, nCR, &
+    iCR_N, iCR_G1, iCR_G2, iCR_G3, uCR, nCR, &
     iPR_D, uPR, &
-    nSpecies
+    nSpecies, rhsCR
   USE InputOutputModule, ONLY: &
     WriteFields1D
+  USE MomentEquationsSolutionModule_M1_DG, ONLY: &
+    ComputeRHS_M1_DG
   USE FluidRadiationCouplingUtilitiesModule, ONLY: &
     FinalizeFluidFields, &
     FinalizeRadiationFields
@@ -74,7 +83,7 @@ CONTAINS
 
     CALL InitializeFluidRadiationCoupling
   
-!    OPEN(unit = out_unit, file = "tvsdt", action = "write", status = "replace" )
+    OPEN(unit = out_unit, file = "tvsdt", action = "write", status = "replace" )
 
     dt = 1.d-8 * Millisecond 
 
@@ -117,15 +126,31 @@ CONTAINS
 
       END IF
 
+      CALL ComputeRHS_M1_DG( [1,1,1], [nX(1),nX(2),nX(3)] )
+
+!      PRINT*,"max( abs(rhsCR(iCR_N,:)) )", MAXVAL( ABS(rhsCR(:,:,:,:,:,iCR_N,1) ) )
+!      PRINT*, "dt", dt / Millisecond, "ms"
+ 
       CALL ComputeRHS_C_H( dt )
  
       CALL UpdateFields( dt )
 
       t = t + dt
 
+      IF( dt < 1.d-30 * Millisecond ) THEN
+         WriteOutput = .TRUE.
+         WRITE(*,*)
+         WRITE(*,'(A6,A15,ES10.4E2,A1,A2,A6,I8.8,A7,A4,ES10.4E2,A2)') &
+           '', 'Evolved to t = ', t / U % TimeUnit, '', TRIM( U % TimeLabel ), &
+           ' with ', iCycle, ' cycles'
+         WRITE(*,*)
+         t = t_end
+        
+      END IF
+
       IF( WriteOutput )THEN
 
-!        WRITE( out_unit, '(2E15.6,4I4)' ) t/MilliSecond, dt/MilliSecond, SmallestPosition
+        WRITE( out_unit, '(2E15.6,4I4)' ) t/MilliSecond, dt/MilliSecond, SmallestPosition
 
         CALL WriteFields1D &
                ( Time = t, WriteFluidFields_Option = .TRUE., &
@@ -137,7 +162,7 @@ CONTAINS
 
     END DO ! WHILE
 
-!    CLOSE( out_unit )
+    CLOSE( out_unit )
 
     CALL FinalizeFluidRadiationCoupling
 
@@ -162,14 +187,22 @@ CONTAINS
     INTEGER, DIMENSION(4), INTENT(out) :: SmallestPosition 
     REAL(DP), INTENT(inout) :: dt
    
-    REAL(DP)  :: dt_Radiation, dt_max
+    REAL(DP)  :: dt_Radiation, dt_max, dt_Stream
 
     dt_max = 1.0d-2 * Millisecond
 
     CALL ComputeTimestepPenalization &
            ( dt_Radiation, dt_max, t, SmallestPosition )
 
-    dt = MIN( dt_Radiation, dt_max, dt*1.1d0 )
+    CALL ComputeTimeStep_Streaming( dt_Stream )
+
+    dt = MIN( dt_Radiation, dt_max, dt_Stream, dt*1.1d0 )
+
+    IF( dt < 1.0d-30 * Millisecond ) THEN
+      PRINT*,"In ComputeTimestep, dt too small: ", dt / MilliSecond, "ms"
+      PRINT*,"dt_Stream ", dt_Stream / Millisecond, "ms"
+      PRINT*,"dt_Radiation ", dt_Radiation / Millisecond, "ms"
+    END IF
 
   END SUBROUTINE ComputeTimestep
 
@@ -213,8 +246,14 @@ CONTAINS
                     iNode = NodeNumber( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
 
                     Coll = C_J(iNode,iE,iX1,iX2,iX3,1) 
-                    NN   = uCR    (iNode,iE,iX1,iX2,iX3,iCR_N,1)
-
+                    NN   = uCR(iNode,iE,iX1,iX2,iX3,iCR_N,1)
+                    IF( NN < 0.d0 ) THEN
+                      PRINT*,''
+                      PRINT*,"ERROR in ComputeTimestepPenalization"
+                      PRINT*,"uCR(:,iCR_N) has NEGATIVE VALUE"
+                      PRINT*,''
+                      STOP
+                    END IF
                     ! --- Boundary Limit ---
 
                     lim1 = - Coll / NN
@@ -234,7 +273,7 @@ CONTAINS
 
                     dt_2 = Diff_FE / ( 2.0 * LNMax ) + &
                            SQRT( Diff_FE / ( LAMB * LNMax ) + &
-                                 Diff_FE**2 / ( 4.0 * LNMax**2 ) )
+                                 Diff_FE * Diff_FE / ( 4.0 * LNMax * LNMax ) )
 
                     IF( dt >= MIN( dt_1, dt_2 ) )THEN 
                       SmallestPosition = (/iE, iX1, iX2, iX3/)
@@ -256,12 +295,47 @@ CONTAINS
 
 !   CLOSE( out_unit_debug )
 
-    IF( dt < 1.0d-15 * Millisecond ) THEN
-      PRINT*,"dt too small: ", dt / MilliSecond, "ms"
-      STOP
-    END IF
-
   END SUBROUTINE ComputeTimestepPenalization
+
+
+  SUBROUTINE ComputeTimeStep_Streaming( dt )
+
+    REAL(DP), INTENT(out) :: dt
+
+    INTEGER  :: iX1, iX2, iX3, iNodeX
+    REAL(DP) :: CFL, dt_X1, dt_X2, dt_X3
+
+    dt = HUGE( 1.0_DP )
+
+    ASSOCIATE( dX1 => MeshX(1) % Width (1:nX(1)), &
+               dX2 => MeshX(2) % Width (1:nX(2)), &
+               dX3 => MeshX(3) % Width (1:nX(3)), &
+               X1  => MeshX(1) % Center(1:nX(1)), &
+               X2  => MeshX(2) % Center(1:nX(2)), &
+               X3  => MeshX(3) % Center(1:nX(3)) )
+
+    CFL = 0.2_DP / ( 2.0_DP * DBLE( nNodes - 1 ) + 1.0_DP ) ! For Debugging
+
+    DO iX3 = 1, nX(3)
+      DO iX2 = 1, nX(2)
+        DO iX1 = 1, nX(1)
+
+          dt_X1 = CFL * dX1(iX1)
+          dt_X2 = CFL * a( [ X1(iX1), X2(iX2), X3(iX3) ] ) &
+                      * dX2(iX2)
+          dt_X3 = CFL * b( [ X1(iX1), X2(iX2), X3(iX3) ] ) &
+                      * c( [ X1(iX1), X2(iX2), X3(iX3) ] ) &
+                      * dX3(iX3)
+
+          dt = MIN( dt, dt_x1, dt_X2, dt_X3 )
+
+        END DO
+      END DO
+    END DO
+
+    END ASSOCIATE ! dX1, etc.
+
+  END SUBROUTINE ComputeTimeStep_Streaming
 
 
   SUBROUTINE ComputeTimestepForwardEuler( dt )
@@ -321,87 +395,71 @@ CONTAINS
     INTEGER  :: iE, iX1, iX2, iX3, iS, iCR
     INTEGER  :: iNodeE, iNodeX1, iNodeX2, iNodeX3
     INTEGER  :: iNode, iNodeX
-    REAL(DP) :: LAMB
+    REAL(DP) :: LAMB, temp
 
-     DO iS = 1, nSpecies
+    DO iS = 1, nSpecies
+  
+     DO iCR = 1, nCR
+      DO iX3 = 1, nX(3)
+        DO iX2 = 1, nX(2)
+          DO iX1 = 1, nX(1)
+            DO iE = 1, nE
 
-      iCR = iCR_N
+              DO iNodeX3 = 1, nNodesX(3)
+                DO iNodeX2 = 1, nNodesX(2)
+                  DO iNodeX1 = 1, nNodesX(1)
 
-        DO iX3 = 1, nX(3)
-          DO iX2 = 1, nX(2)
-            DO iX1 = 1, nX(1)
-              DO iE = 1, nE
+                    iNodeX &
+                      = NodeNumberX &
+                          ( iNodeX1, iNodeX2, iNodeX3 )
+                    LAMB   &
+                      = MAX( absLambda(iNodeX,iX1,iX2,iX3), 1.0d-100 )
 
-                DO iNodeX3 = 1, nNodesX(3)
-                  DO iNodeX2 = 1, nNodesX(2)
-                    DO iNodeX1 = 1, nNodesX(1)
+                    DO iNodeE = 1, nNodesE
 
-                      iNodeX &
-                        = NodeNumberX &
-                            ( iNodeX1, iNodeX2, iNodeX3 )
-                      LAMB   &
-                        = MAX( absLambda(iNodeX,iX1,iX2,iX3), 1.0d-100 )
+                      iNode &
+                        = NodeNumber &
+                            ( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
 
-                      DO iNodeE = 1, nNodesE
+                      uCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) &
+                        = uCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) &
+                            + (dt/(1.d0 +dt*LAMB)) * C_J(iNode,iE,iX1,iX2,iX3,iS)
 
-                        iNode &
-                          = NodeNumber &
-                              ( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
+                      temp = 1.d0 / ( 1.d0 + dt * Kappa(iNode,iE,iX1,iX2,iX3) )
 
-                        uCR(iNode,iE,iX1,iX2,iX3,iCR,iS) &
-                          = uCR(iNode,iE,iX1,iX2,iX3,iCR,iS) &
-                              / ( 1.d0 + dt * Kappa(iNode,iE,iX1,iX2,iX3) )
+                      uCR(iNode,iE,iX1,iX2,iX3,iCR_G1,iS) &
+                        = uCR(iNode,iE,iX1,iX2,iX3,iCR_G1,iS) * temp
 
-                      END DO
+                      uCR(iNode,iE,iX1,iX2,iX3,iCR_G2,iS) &
+                        = uCR(iNode,iE,iX1,iX2,iX3,iCR_G2,iS) * temp
+
+                      uCR(iNode,iE,iX1,iX2,iX3,iCR_G3,iS) &
+                        = uCR(iNode,iE,iX1,iX2,iX3,iCR_G3,iS) * temp
+
+                      uCR(iNode,iE,iX1,iX2,iX3,iCR,iS) &
+                        = uCR(iNode,iE,iX1,iX2,iX3,iCR,iS) + &
+                          dt * rhsCR(iNode,iE,iX1,iX2,iX3,iCR,iS)
+
                     END DO
+
                   END DO
                 END DO
-
               END DO
+
             END DO
           END DO
         END DO
-
-      DO iCR = iCR_N, nCR
-
-        DO iX3 = 1, nX(3)
-          DO iX2 = 1, nX(2)
-            DO iX1 = 1, nX(1)
-              DO iE = 1, nE
-
-                DO iNodeX3 = 1, nNodesX(3)
-                  DO iNodeX2 = 1, nNodesX(2)
-                    DO iNodeX1 = 1, nNodesX(1)
-
-                      iNodeX &
-                        = NodeNumberX &
-                            ( iNodeX1, iNodeX2, iNodeX3 )
-                      LAMB   &
-                        = MAX( absLambda(iNodeX,iX1,iX2,iX3), 1.0d-100 )
-
-                      DO iNodeE = 1, nNodesE
-
-                        iNode &
-                          = NodeNumber &
-                              ( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
-
-                        uCR(iNode,iE,iX1,iX2,iX3,iCR,iS) &
-                          = uCR(iNode,iE,iX1,iX2,iX3,iCR,iS) &
-                              + (dt/(1.d0 +dt*LAMB)) * C_J(iNode,iE,iX1,iX2,iX3,iS)
-
-                      END DO
-                    END DO
-                  END DO
-                END DO
-
-              END DO
-            END DO
-          END DO
-        END DO
-
       END DO
-
      END DO
+    END DO
+
+    IF( MINVAL( uCR(:,:,:,:,:,iCR_N,:) ) < 0.d0 )THEN
+      PRINT*,''
+      PRINT*,"ERROR in UpdateFields"
+      PRINT*,"NEGATIVE uCR(:,iCR_N) "
+      PRINT*,''
+      STOP
+    END IF
 
   END SUBROUTINE UpdateFields
 
