fd178323b21cabca9e766db68ed611dc5726d770
Ran Chu <rchu@vols.utk.edu>
2017-09-29 17:46:03 +0000

    Keep updates.

--- Applications/TransportProblems_Penalization/TimeSteppingModule_Penalization.f90
+++ Applications/TransportProblems_Penalization/TimeSteppingModule_Penalization.f90
@@ -63,7 +63,8 @@ CONTAINS
     INTEGER  :: iCycle, iDisplayCycle
     INTEGER, PARAMETER :: out_unit = 20
     INTEGER, DIMENSION(4) :: SmallestPosition 
-    REAL(DP) :: t, t_write, dt, dt_fixed
+    REAL(DP) :: t, t_write, dt, dt_fixed, &
+                dt_stream, dt_accur, dt_boundary, dt_lower
 
     FixedTimeStep = .FALSE.
     IF( PRESENT( dt_fixed_Option ) )THEN
@@ -90,7 +91,7 @@ CONTAINS
     CALL InitializeFluidRadiationCoupling
   
     OPEN(unit = out_unit, file = "tvsdt", action = "write", status = "replace" )
-
+    WRITE( out_unit, '(A1,7A15)')'#','time', 'dt','dt_stream','dt_accur','dt_upper', 'dt_lower','Position'
     dt = 1.d-8 * Millisecond 
 
     DO WHILE( t < t_end )
@@ -105,7 +106,7 @@ CONTAINS
 
       ELSE
 
-        CALL ComputeTimeStep( t, dt, SmallestPosition )
+        CALL ComputeTimeStep( t, dt, SmallestPosition, dt_stream, dt_accur, dt_boundary, dt_lower )
 
       END IF
 
@@ -136,9 +137,6 @@ CONTAINS
 
       CALL ComputeRHS_M1_DG( [1,1,1], [nX(1),nX(2),nX(3)] )
 
-!      PRINT*,"max( abs(rhsCR(iCR_N,:)) )", MAXVAL( ABS(rhsCR(:,:,:,:,:,iCR_N,1) ) )
-!      PRINT*, "dt", dt / Millisecond, "ms"
- 
       CALL ComputeRHS_C_H( dt )
  
       CALL UpdateFields( dt )
@@ -154,13 +152,15 @@ CONTAINS
            '', 'Evolved to t = ', t / U % TimeUnit, '', TRIM( U % TimeLabel ), &
            ' with ', iCycle, ' cycles'
          WRITE(*,*)
-         t = t_end
-        
+         STOP 
       END IF
 
       IF( WriteOutput )THEN
 
-        WRITE( out_unit, '(2E15.6,4I4)' ) t/MilliSecond, dt/MilliSecond, SmallestPosition
+        WRITE( out_unit, '(6E15.6,4I4)' ) t/MilliSecond, dt/MilliSecond, &
+                                         dt_stream/MilliSecond, dt_accur/Millisecond, &
+                                         dt_boundary/Millisecond, dt_lower/Millisecond, &
+                                         SmallestPosition
 
         CALL WriteFields1D &
                ( Time = t, WriteFluidFields_Option = .TRUE., &
@@ -191,21 +191,23 @@ CONTAINS
   END SUBROUTINE EvolveFields
 
 
-  SUBROUTINE ComputeTimestep( t, dt, SmallestPosition )
+  SUBROUTINE ComputeTimestep( t, dt, SmallestPosition, dt_streams, dt_accur, dt_boundary, dt_lower )
 
     REAL(DP), INTENT(in) :: t
     INTEGER, DIMENSION(4), INTENT(out) :: SmallestPosition 
     REAL(DP), INTENT(inout) :: dt
+    REAL(DP), INTENT(out) ::  dt_streams, dt_accur, dt_boundary, dt_lower
    
     REAL(DP)  :: dt_Radiation, dt_max, dt_Stream
 
     dt_max = 1.0d-2 * Millisecond
 
     CALL ComputeTimestepPenalization &
-           ( dt_Radiation, dt_max, t, SmallestPosition )
+           ( dt_Radiation, dt_max, t, SmallestPosition, dt_accur, dt_boundary, dt_lower )
 
     CALL ComputeTimeStep_Streaming( dt_Stream )
-
+    dt_streams = dt_Stream
+ 
     dt = MIN( dt_Radiation, dt_max, dt_Stream, dt*1.1d0 )
 
     IF( dt < 1.0d-30 * Millisecond ) THEN
@@ -217,10 +219,10 @@ CONTAINS
   END SUBROUTINE ComputeTimestep
 
 
-  SUBROUTINE ComputeTimestepPenalization( dt, dt_max, t, SmallestPosition )
+  SUBROUTINE ComputeTimestepPenalization( dt, dt_max, t, SmallestPosition, dt_accur, dt_boundary, dt_lower )
 
     REAL(DP), INTENT(in)  :: dt_max, t
-    REAL(DP), INTENT(out) :: dt
+    REAL(DP), INTENT(out) :: dt, dt_accur, dt_lower, dt_boundary
     INTEGER, DIMENSION(4), INTENT(out) :: SmallestPosition 
 
     INTEGER  :: out_unit_debug
@@ -229,16 +231,24 @@ CONTAINS
     INTEGER  :: iNodeX1, iNodeX2, iNodeX3, iNodeX
     CHARACTER(12)  :: FileNumberString
     CHARACTER(19)   :: FileName
-    REAL(DP) :: dt_1, dt_2, LAMB, LNMax, lim
-    REAL(DP) :: NN, Coll, lim1, lim2
+    REAL(DP) :: dt_1, dt_2, LAMB, LNMax, lim, dt_dx, lim_W
+    REAL(DP) :: NN, Coll, CpS
+    REAL(DP) :: mindx
 
     dt = HUGE( 1.0_DP )
-    SmallestPosition = (/0,0,0,0/)
+    dt_accur = dt
+    dt_boundary = dt
+    dt_lower = dt
 
-  WRITE( FileNumberString, FMT='(i12.12)') INT(t/MilliSecond*1.d7)
-  FileName = 'o.dtdis'// FileNumberString
-! OPEN( NEWUNIT = out_unit_debug, file = FileName, action = "write", status = "replace" )
-    
+    SmallestPosition = (/0,0,0,0/)
+    ASSOCIATE( dX1 => MeshX(1) % Width (1:nX(1)), &
+               dX2 => MeshX(2) % Width (1:nX(2)), &
+               dX3 => MeshX(3) % Width (1:nX(3)) )
+
+      mindx = MIN( MINVAL(dX1), MINVAL(dX2), MINVAL(dX3) )
+ 
+    END ASSOCIATE
+ 
     DO iX3 = 1, nX(3)
       DO iX2 = 1, nX(2)
         DO iX1 = 1, nX(1)
@@ -249,48 +259,51 @@ CONTAINS
                 DO iNodeX1 = 1, nNodesX(1)
 
                   iNodeX = NodeNumberX( iNodeX1, iNodeX2, iNodeX3 )
-                  LAMB   = MAX( absLambda(iNodeX,iX1,iX2,iX3), 1.0d-100 )
+                  LAMB   = absLambda(iNodeX,iX1,iX2,iX3)
+                  dt_dx = 0.5d0 * mindx / ( 1.0d0 - 0.5d0 * LAMB * mindx )
+                  IF( dt_dx < 0.0d0 ) dt_dx = HUGE( 1.0_DP )
 
                   DO iNodeE = 1, nNodesE
 
                     iNode = NodeNumber( iNodeE, iNodeX1, iNodeX2, iNodeX3 )
-
-                    Coll = C_J(iNode,iE,iX1,iX2,iX3,1) 
+                    Coll = C_J(iNode,iE,iX1,iX2,iX3,1)
+                    CpS  = Coll + rhsCR(iNode,iE,iX1,iX2,iX3,iCR_N,1)
                     NN   = uCR(iNode,iE,iX1,iX2,iX3,iCR_N,1)
-                    IF( NN < 0.d0 ) THEN
-                      PRINT*,''
-                      PRINT*,"ERROR in ComputeTimestepPenalization"
-                      PRINT*,"uCR(:,iCR_N) has NEGATIVE VALUE"
-                      PRINT*,''
-                      STOP
-                    END IF
+
                     ! --- Boundary Limit ---
 
-                    lim1 = - Coll / NN
-                    lim2 =   Coll / ( FourPi - NN )
+                    lim = 0.0d0
+                    IF( Coll < 0.0d0 ) lim = - NN / Coll
+                    IF( Coll > 0.0d0 ) lim = ( FourPi - NN ) / Coll 
+                    dt_1  = 0.5d0 * lim / ( 1.0d0 - 0.5d0 * LAMB * lim )
+                    IF( dt_1 <= 0.0d0 ) dt_1 = HUGE( 1.0_DP )
 
-                    lim  = MAX( lim1, lim2 ) 
+                    dt_1 = MIN( dt_1, dt_dx )
+                    dt_boundary = MIN( dt_boundary, dt_1 )
 
-                    IF( lim > LAMB )THEN
-                      dt_1 = 1.0 / ( lim - LAMB )
-                    ELSE
-                      dt_1 = dt_max
-                    END IF
-             
+                    ! --- Boundary Limit (lower) ---
+
+                    
+                    lim_W = ( rhsCR(iNode,iE,iX1,iX2,iX3,iCR_N,1) - Coll ) / NN - LAMB
+                  
+                    IF( lim_W > 0.0d0 ) dt_lower = 1.0d0 / lim_W
+              
+                    dt_1 = MIN( dt_lower, dt_1 )
+
                     ! --- Accuracy Limit ---
               
-                    LNMax = ABS( Coll / NN )
+                    LNMax = ABS( CpS / NN )
 
-                    dt_2 = Diff_FE / ( 2.0 * LNMax ) + &
+                    dt_2 = Diff_FE / ( 2.0d0 * LNMax ) + &
                            SQRT( Diff_FE / ( LAMB * LNMax ) + &
-                                 Diff_FE * Diff_FE / ( 4.0 * LNMax * LNMax ) )
+                                 Diff_FE * Diff_FE / ( 4.0d0 * LNMax * LNMax ) )
+                    dt_accur = MIN( dt_accur, dt_2 )
 
                     IF( dt >= MIN( dt_1, dt_2 ) )THEN 
                       SmallestPosition = (/iE, iX1, iX2, iX3/)
                     END IF
  
                     dt = MIN( dt, dt_1, dt_2 )
-!                   WRITE(out_unit_debug,'(5I4,E15.6)'), iNode, iE, iX1, iX2, iX3, dt/ MilliSecond                   
 
                   END DO
 
@@ -302,8 +315,6 @@ CONTAINS
         END DO
       END DO
     END DO
-
-!   CLOSE( out_unit_debug )
 
   END SUBROUTINE ComputeTimestepPenalization
 
@@ -400,7 +411,7 @@ CONTAINS
 
   SUBROUTINE UpdateFields( dt )
 
-    REAL(DP), INTENT(in) :: dt
+    REAL(DP), INTENT(inout) :: dt
 
     INTEGER  :: iE, iX1, iX2, iX3, iS, iCR
     INTEGER  :: iNodeE, iNodeX1, iNodeX2, iNodeX3
@@ -434,6 +445,18 @@ CONTAINS
                             + ( dt / (1.d0+dt*LAMB) ) &
                               * ( rhsCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) &
                                   + C_J(iNode,iE,iX1,iX2,iX3,iS) )
+                      IF( uCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) < 0.0 ) THEN
+                        PRINT*, ''
+                        PRINT*, 'negative uCR= ', uCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS),'at',iE, iX1, iX2, iX3
+                        PRINT*, 'W - 1/dt should be negative or zero:', &
+                                    ( rhsCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) -  C_J(iNode,iE,iX1,iX2,iX3,iS) ) &
+                                     / uCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) &
+                                     - absLambda(iNodeX,iX1,iX2,iX3) - 1.0/dt
+                        PRINT*, 'W should be positive :', &
+                                    ( rhsCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) -  C_J(iNode,iE,iX1,iX2,iX3,iS) ) &
+                                     / uCR(iNode,iE,iX1,iX2,iX3,iCR_N,iS) &
+                                     - absLambda(iNodeX,iX1,iX2,iX3) 
+                      END IF
 
                       temp = 1.d0 / ( 1.d0 + dt * Kappa(iNode,iE,iX1,iX2,iX3) )
 
@@ -470,7 +493,7 @@ CONTAINS
       PRINT*,"ERROR in UpdateFields"
       PRINT*,"NEGATIVE uCR(:,iCR_N) "
       PRINT*,''
-!      STOP
+      STOP
     END IF
 
   END SUBROUTINE UpdateFields
