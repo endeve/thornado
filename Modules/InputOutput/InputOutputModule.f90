1162e6e11a85b0c9d3bfee1d3bd2830896ca720d
Eirik Endeve <endevee@ornl.gov>
2017-11-04 13:16:00 +0000

    Moving to more complete output for some fields

--- Modules/InputOutput/InputOutputModule.f90
+++ Modules/InputOutput/InputOutputModule.f90
@@ -6,7 +6,10 @@ MODULE InputOutputModule
     UnitsDisplay
   USE ProgramHeaderModule, ONLY: &
     ProgramName, &
-    nX, nNodesX, nE
+    nX, nNodesX, &
+    nE, nNodesE
+  USE UtilitiesModule, ONLY: &
+    NodeNumber
   USE PolynomialBasisModule_Lagrange, ONLY: &
     evalL, &
     evalLX
@@ -76,10 +79,10 @@ CONTAINS
       CALL WriteGeometryFields1D( Time )
 
     IF( WriteFluidFields ) &
-      CALL WriteFluidFields1D( Time )
+      CALL WriteFluidFields1D_N( Time )
 
     IF( WriteRadiationFields ) &
-      CALL WriteRadiationFields1D( Time )
+      CALL WriteRadiationFields1D_N( Time )
 
     FileNumber = FileNumber + 1
 
@@ -117,6 +120,23 @@ CONTAINS
     END ASSOCIATE ! X1, etc.
 
   END SUBROUTINE WriteGeometryFields1D
+
+
+  FUNCTION GeometryField1D( u, nX1 )
+
+    REAL(DP), DIMENSION(nX1)             :: GeometryField1D
+    REAL(DP), DIMENSION(:,:), INTENT(in) :: u
+    INTEGER,                  INTENT(in) :: nX1
+
+    INTEGER :: iX1
+
+    DO iX1 = 1, nX1
+      GeometryField1D(iX1) &
+        = evalLX( u(:,iX1), 0.0_DP, 0.0_DP, 0.0_DP )
+    END DO
+
+    RETURN
+  END FUNCTION GeometryField1D
 
 
   SUBROUTINE WriteFluidFields1D( Time )
@@ -193,23 +213,6 @@ CONTAINS
   END SUBROUTINE WriteFluidFields1D
 
 
-  FUNCTION GeometryField1D( u, nX1 )
-
-    REAL(DP), DIMENSION(nX1)             :: GeometryField1D
-    REAL(DP), DIMENSION(:,:), INTENT(in) :: u
-    INTEGER,                  INTENT(in) :: nX1
-
-    INTEGER :: iX1
-
-    DO iX1 = 1, nX1
-      GeometryField1D(iX1) &
-        = evalLX( u(:,iX1), 0.0_DP, 0.0_DP, 0.0_DP )
-    END DO
-
-    RETURN
-  END FUNCTION GeometryField1D
-
-
   FUNCTION FluidField1D( u, nX1 )
 
     REAL(DP), DIMENSION(nX1)             :: FluidField1D
@@ -225,6 +228,101 @@ CONTAINS
 
     RETURN
   END FUNCTION FluidField1D
+
+
+  SUBROUTINE WriteFluidFields1D_N( Time )
+
+    REAL(DP), INTENT(in) :: Time
+
+    CHARACTER(6)   :: FileNumberString
+    CHARACTER(256) :: FileName
+    INTEGER        :: FUNIT
+
+    WRITE( FileNumberString, FMT='(i6.6)') FileNumber
+
+    FileName = OutputDirectory // '/' // TRIM( ProgramName ) // '_' // &
+                 FluidSuffix // '_' // FileNumberString // '.dat'
+
+    ASSOCIATE( U  => UnitsDisplay )
+
+    OPEN( NEWUNIT = FUNIT, FILE = TRIM( FileName ) )
+
+    WRITE( FUNIT, * ) &
+      Time / U % TimeUnit, &
+      nX(1), nNodesX(1), &
+      NodeCoordinatesX1( nX(1), nNodesX(1) ) / U % LengthUnit, &
+      FluidField1D_N( uCF(:,1:nX(1),1,1,iCF_D ), nX(1), nNodesX(1) ) &
+        / U % MassDensityUnit, &
+      FluidField1D_N( uCF(:,1:nX(1),1,1,iCF_S1), nX(1), nNodesX(1) ) &
+        / U % MomentumDensityUnit, &
+      FluidField1D_N( uCF(:,1:nX(1),1,1,iCF_S2), nX(1), nNodesX(1) ) &
+        / U % MomentumDensityUnit, &
+      FluidField1D_N( uCF(:,1:nX(1),1,1,iCF_S3), nX(1), nNodesX(1) ) &
+        / U % MomentumDensityUnit, &
+      FluidField1D_N( uCF(:,1:nX(1),1,1,iCF_E ), nX(1), nNodesX(1) ) &
+        / U % EnergyDensityUnit, &
+      FluidField1D_N( uCF(:,1:nX(1),1,1,iCF_Ne), nX(1), nNodesX(1) ) &
+        / U % ParticleDensityUnit, &
+      FluidField1D_N( uPF(:,1:nX(1),1,1,iPF_D ), nX(1), nNodesX(1) ) &
+        / U % MassDensityUnit, &
+      FluidField1D_N( uPF(:,1:nX(1),1,1,iPF_V1), nX(1), nNodesX(1) ) &
+        / U % VelocityUnit, &
+      FluidField1D_N( uPF(:,1:nX(1),1,1,iPF_V2), nX(1), nNodesX(1) ) &
+        / U % VelocityUnit, &
+      FluidField1D_N( uPF(:,1:nX(1),1,1,iPF_V3), nX(1), nNodesX(1) ) &
+        / U % VelocityUnit, &
+      FluidField1D_N( uPF(:,1:nX(1),1,1,iPF_E ), nX(1), nNodesX(1) ) &
+        / U % EnergyDensityUnit, &
+      FluidField1D_N( uPF(:,1:nX(1),1,1,iPF_Ne), nX(1), nNodesX(1) ) &
+        / U % ParticleDensityUnit, &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_P ), nX(1), nNodesX(1) ) &
+        / U % PressureUnit, &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_T ), nX(1), nNodesX(1) ) &
+        / U % TemperatureUnit, &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Ye), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_S ), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_E ), nX(1), nNodesX(1) ) &
+        / ( U % EnergyDensityUnit / U % MassDensityUnit ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Me), nX(1), nNodesX(1) ) &
+        / U % EnergyUnit, &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Mp), nX(1), nNodesX(1) ) &
+        / U % EnergyUnit, &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Mn), nX(1), nNodesX(1) ) &
+        / U % EnergyUnit, &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Xp), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Xn), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Xa), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Xh), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Gm), nX(1), nNodesX(1) ), &
+      FluidField1D_N( uAF(:,1:nX(1),1,1,iAF_Cs), nX(1), nNodesX(1) ) &
+        / U % VelocityUnit, &
+      Shock(1:nX(1),1,1)
+
+    CLOSE( FUNIT )
+
+    END ASSOCIATE ! U
+
+  END SUBROUTINE WriteFluidFields1D_N
+
+
+  FUNCTION FluidField1D_N( u, nX1, nNX1 )
+
+    REAL(DP), DIMENSION(nX1*nNX1)        :: FluidField1D_N
+    REAL(DP), DIMENSION(:,:), INTENT(in) :: u
+    INTEGER,                  INTENT(in) :: nX1, nNX1
+
+    INTEGER :: i, iX1, iNX1
+
+    i = 1
+    DO iX1 = 1, nX1
+      DO iNX1 = 1, nNX1
+        FluidField1D_N(i) = u(iNX1,iX1)
+        i = i + 1
+      END DO
+    END DO
+
+    RETURN
+  END FUNCTION FluidField1D_N
 
 
   SUBROUTINE WriteRadiationFields1D( Time )
@@ -284,6 +382,84 @@ CONTAINS
 
     RETURN
   END FUNCTION RadiationField1D
+
+
+  SUBROUTINE WriteRadiationFields1D_N( Time )
+
+    REAL(DP), INTENT(in) :: Time
+
+    CHARACTER(6)   :: FileNumberString
+    CHARACTER(256) :: FileName
+    INTEGER        :: FUNIT
+
+    WRITE( FileNumberString, FMT='(i6.6)') FileNumber
+
+    FileName = OutputDirectory // '/' // TRIM( ProgramName ) // '_' // &
+                 RadiationSuffix // '_' // FileNumberString // '.dat'
+
+    ASSOCIATE( U => UnitsDisplay )
+
+    OPEN( NEWUNIT = FUNIT, FILE = TRIM( FileName ) )
+
+    WRITE( FUNIT, * ) &
+      Time / U % TimeUnit, &
+      nE, nNodesE, &
+      NodeCoordinatesE( nE, nNodesE ) / U % EnergyUnit, &
+      nX(1), nNodesX(1), &
+      NodeCoordinatesX1( nX(1), nNodesX(1) ) / U % LengthUnit, &
+      RadiationField1D_N &
+        ( uCR(:,1:nE,1:nX(1),1,1,iCR_N, 1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uCR(:,1:nE,1:nX(1),1,1,iCR_G1,1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uCR(:,1:nE,1:nX(1),1,1,iCR_G2,1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uCR(:,1:nE,1:nX(1),1,1,iCR_G3,1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uPR(:,1:nE,1:nX(1),1,1,iPR_D, 1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uPR(:,1:nE,1:nX(1),1,1,iPR_I1,1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uPR(:,1:nE,1:nX(1),1,1,iPR_I2,1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      RadiationField1D_N &
+        ( uPR(:,1:nE,1:nX(1),1,1,iPR_I3,1), nE, nNodesE, nX(1), nNodesX(1) ), &
+      Discontinuity(1:nE,1:nX(1),1,1)
+
+    CLOSE( FUNIT )
+
+    END ASSOCIATE ! U
+
+  END SUBROUTINE WriteRadiationFields1D_N
+
+
+  FUNCTION RadiationField1D_N( u, nE, nNE, nX1, nNX1 )
+
+    REAL(DP), DIMENSION(nE*nNE,nX1*nNX1)   :: RadiationField1D_N
+    REAL(DP), DIMENSION(:,:,:), INTENT(in) :: u
+    INTEGER,                    INTENT(in) :: nE,  nNE
+    INTEGER,                    INTENT(in) :: nX1, nNX1
+
+    INTEGER :: i, j, iNode
+    INTEGER :: iE, iNE, iX1, iNX1
+
+    j = 1
+    DO iX1 = 1, nX1
+      DO iNX1 = 1, nNX1
+        i = 1
+        DO iE = 1, nE
+          DO iNE = 1, nNE
+            iNode = NodeNumber(iNE,iNX1,1,1)
+            RadiationField1D_N(i,j) &
+              = u(iNode,iE,iX1)
+            i = i + 1
+          END DO
+        END DO
+        j = j + 1
+      END DO
+    END DO
+
+    RETURN
+  END FUNCTION RadiationField1D_N
 
 
   SUBROUTINE WriteFieldsRestart1D &
@@ -399,27 +575,6 @@ CONTAINS
     WRITE(*,*)
 
   END SUBROUTINE WriteFluidFieldsRestart1D
-
-
-  FUNCTION NodeCoordinatesX1( nX1, nNodesX1 )
-
-    REAL(DP), DIMENSION(nX1*nNodesX1) :: NodeCoordinatesX1
-    INTEGER,               INTENT(in) :: nX1
-    INTEGER,               INTENT(in) :: nNodesX1
-
-    INTEGER :: iX1, iNodeX1, iNode
-
-    iNode = 0
-    DO iX1 = 1, nX1
-      DO iNodeX1 = 1, nNodesX1
-        iNode = iNode + 1
-        NodeCoordinatesX1(iNode) &
-          = NodeCoordinate( MeshX(1), iX1, iNodeX1 )
-      END DO
-    END DO
-
-    RETURN
-  END FUNCTION NodeCoordinatesX1
 
 
   FUNCTION FluidFieldRestart1D_Out( u, nX1, nNodesX1 )
@@ -564,6 +719,48 @@ CONTAINS
 
     RETURN
   END FUNCTION FluidFieldRestart1D_In
+
+
+  FUNCTION NodeCoordinatesE( nE, nNodesE )
+
+    REAL(DP), DIMENSION(nE*nNodesE) :: NodeCoordinatesE
+    INTEGER,             INTENT(in) :: nE
+    INTEGER,             INTENT(in) :: nNodesE
+
+    INTEGER :: iE, iNodeE, iNode
+
+    iNode = 0
+    DO iE = 1, nE
+      DO iNodeE = 1, nNodesE
+        iNode = iNode + 1
+        NodeCoordinatesE(iNode) &
+          = NodeCoordinate( MeshE, iE, iNodeE )
+      END DO
+    END DO
+
+    RETURN
+  END FUNCTION NodeCoordinatesE
+
+
+  FUNCTION NodeCoordinatesX1( nX1, nNodesX1 )
+
+    REAL(DP), DIMENSION(nX1*nNodesX1) :: NodeCoordinatesX1
+    INTEGER,               INTENT(in) :: nX1
+    INTEGER,               INTENT(in) :: nNodesX1
+
+    INTEGER :: iX1, iNodeX1, iNode
+
+    iNode = 0
+    DO iX1 = 1, nX1
+      DO iNodeX1 = 1, nNodesX1
+        iNode = iNode + 1
+        NodeCoordinatesX1(iNode) &
+          = NodeCoordinate( MeshX(1), iX1, iNodeX1 )
+      END DO
+    END DO
+
+    RETURN
+  END FUNCTION NodeCoordinatesX1
 
 
 END MODULE InputOutputModule
