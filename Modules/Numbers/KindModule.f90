6dfc15d124b0a5f9557ec329a55a0f364368d200
Eirik Endeve <endevee@ornl.gov>
2017-11-04 13:05:44 +0000

    Adding numbers

--- Modules/Numbers/KindModule.f90
+++ Modules/Numbers/KindModule.f90
@@ -13,9 +13,10 @@ MODULE KindModule
     Four  = 4.0_DP, &
     Five  = 5.0_DP
   REAL(DP), PUBLIC, PARAMETER :: &
-    Fifth = 1.0_DP / 5.0_DP, &
-    Third = 1.0_DP / 3.0_DP, &
-    Half  = 0.5_DP
+    Fifth  = 1.0_DP / 5.0_DP, &
+    Fourth = 0.25_DP, &
+    Third  = 1.0_DP / 3.0_DP, &
+    Half   = 0.5_DP
   REAL(DP), PUBLIC, PARAMETER :: &
     Pi     = ACOS( - 1.0_DP ), &
     TwoPi  = 2.0_DP * Pi, &
