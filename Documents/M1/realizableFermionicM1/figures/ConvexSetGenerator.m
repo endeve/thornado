c4f4a8358913412bf6bd6e8268141720c3f2d10f
Ran Chu <rchu@vols.utk.edu>
2018-01-31 21:24:31 +0000

    Changed xlabel.

--- Documents/M1/realizableFermionicM1/figures/ConvexSetGenerator.m
+++ Documents/M1/realizableFermionicM1/figures/ConvexSetGenerator.m
@@ -50,13 +50,11 @@ xxP_FD = + yy.*(1.0-yy);
 xxM_FD = - yy.*(1.0-yy);
 xxP_MB = + yy;
 xxM_MB = - yy;
-plot( xxP_MB, yy, '-r', 'linewidth', 2 )
-plot( xxM_MB, yy, '-r', 'linewidth', 2 )
 plot( xxP_FD, yy, '-k', 'linewidth', 2 )
 plot( xxM_FD, yy, '-k', 'linewidth', 2 )
 hold off
 axis([-0.5 0.5 0.0 1.0]);
-xlabel('Angular Moment |H|');
+xlabel('Angular Moment H');
 ylabel('Angular Moment J');
 title('Realizble Set R');
 saveas(fig,'RealizableSetFermionic.png');
