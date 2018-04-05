3fb2c180f3b70a826ba95c1be638b527131db664
Eirik Endeve <endevee@ornl.gov>
2018-04-05 00:34:00 +0000

    Added more variables to reader.

--- Workflow/ReadGeometryFields.m
+++ Workflow/ReadGeometryFields.m
@@ -1,5 +1,6 @@
 function...
-  [ Time, X1, X2, X3, uGF_H1, uGF_H2, uGF_H3, uGF_Gm11, uGF_Gm22, uGF_Gm33, uGF_SqrtGm, uGF_Phi_N ]...
+  [ Time, X1, X2, X3, uGF_H1, uGF_H2, uGF_H3, uGF_Gm11, uGF_Gm22, uGF_Gm33, uGF_SqrtGm,...
+    uGF_Phi_N, uGF_Alpha, uGF_Psi, uGF_Beta1, uGF_Beta2, uGF_Beta3 ]...
     = ReadGeometryFields( AppName, FileNumber, Directory )
 
   if( exist( 'Directory', 'var' ) )
@@ -26,5 +27,11 @@ function...
   uGF_SqrtGm = h5read( FileName, '/Geometry Fields/Sqrt of Spatial Metric Determina' );
 
   uGF_Phi_N  = h5read( FileName, '/Geometry Fields/Newtonian Potential' );
+  
+  uGF_Alpha  = h5read( FileName, '/Geometry Fields/Lapse Function' );
+  uGF_Psi    = h5read( FileName, '/Geometry Fields/Conformal Factor' );
+  uGF_Beta1  = h5read( FileName, '/Geometry Fields/Shift Vector (1)' );
+  uGF_Beta2  = h5read( FileName, '/Geometry Fields/Shift Vector (2)' );
+  uGF_Beta3  = h5read( FileName, '/Geometry Fields/Shift Vector (3)' );
   
 end
