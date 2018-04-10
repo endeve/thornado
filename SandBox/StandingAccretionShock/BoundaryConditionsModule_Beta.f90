da9bc706ab73a462e431205e1160914abe9038c2
Jesse Buffaloe <jbuffal1@vols.utk.edu>
2018-04-10 22:25:00 +0000

    Added in BCs for 3rd dimension and reflecting BCs for 2nd dimension.

--- SandBox/StandingAccretionShock/BoundaryConditionsModule_Beta.f90
+++ SandBox/StandingAccretionShock/BoundaryConditionsModule_Beta.f90
@@ -7,9 +7,11 @@ MODULE BoundaryConditionsModule_Beta
   USE ReferenceElementModuleX, ONLY: &
     NodeNumberTableX, WeightsX_q
   USE ProgramHeaderModule, ONLY: &
-    bcX, nDOFX, swX
+    bcX, swX, nDOFX, nNodesX
+  USE UtilitiesModule, ONLY: &
+    NodeNumberX
   USE FluidFieldsModule, ONLY: &
-    iCF_D, iCF_S1, iCF_S2, iCF_S3, iCF_E, nCF
+    nCF, iCF_D, iCF_S1, iCF_S2, iCF_S3, iCF_E
 
   IMPLICIT NONE
   PRIVATE
@@ -157,6 +159,7 @@ CONTAINS
       U(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:)
 
     INTEGER :: iCF, iX1, iX2, iX3
+    INTEGER :: iNodeX, iNodeX1, iNodeX2, iNodeX3, jNodeX, jNodeX2
 
     SELECT CASE ( bcX(2) )
 
@@ -206,6 +209,47 @@ CONTAINS
         END DO
       END DO
 
+    CASE ( 3 ) ! Reflecting
+
+        DO iX3 = iX_B0(3), iX_E0(3)
+          DO iX2 = 1, swX(2)
+            DO iX1 = iX_B0(1), iX_E0(1)
+            
+              DO iNodeX3 = 1, nNodesX(3)
+                DO iNodeX2 = 1, nNodesX(2)
+                  DO iNodeX1 = 1, nNodesX(1)
+
+                    jNodeX2 = ( nNodesX(2) - iNodeX2 ) + 1
+                 
+                    iNodeX = NodeNumberX( iNodeX1, iNodeX2, iNodeX3 )
+                    jNodeX = NodeNumberX( iNodeX1, jNodeX2, iNodeX3 )
+
+                    DO iCF = 1, nCF
+
+                      ! --- Inner boundary ---
+                      U(iNodeX,iX1,iX_B0(2)-iX2,iX3,iCF) &
+                        = U(jNodeX,iX1,iX_B0(2),iX3,iCF)  
+              
+                      ! --- Outer boundary ---
+                      U(iNodeX,iX1,iX_E0(2)+iX2,iX3,iCF) &
+                        = U(jNodeX,iX1,iX_E0(2),iX3,iCF)
+
+                    END DO
+
+                    U(iNodeX,iX1,iX_B0(2)-iX2,iX3,iCF_S2) &
+                      = - U(jNodeX,iX1,iX_B0(2),iX3,iCF_S2)
+
+                    U(iNodeX,iX1,iX_E0(2)+iX2,iX3,iCF_S2) &
+                      = - U(jNodeX,iX1,iX_E0(2),iX3,iCF_S2)
+
+                  END DO
+                END DO
+              END DO
+
+            END DO
+          END DO
+        END DO
+
     CASE DEFAULT
 
       WRITE(*,*)
@@ -224,6 +268,65 @@ CONTAINS
       iX_B0(3), iX_E0(3), iX_B1(3), iX_E1(3)
     REAL(DP), INTENT(inout) :: &
       U(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:)
+
+    INTEGER :: iCF, iX1, iX2, iX3
+
+    SELECT CASE ( bcX(3) )
+
+    CASE ( 0 ) ! No Boundary Condition
+
+    CASE ( 1 ) ! Periodic
+
+      DO iCF = 1, nCF
+        DO iX3 = 1, swX(3)
+          DO iX2 = iX_B0(2), iX_E0(2)
+            DO iX1 = iX_B0(1), iX_E0(1)
+
+              ! --- Inner Boundary ---
+
+              U(:,iX1,iX2,iX_B0(3)-iX3,iCF) &
+                = U(:,iX1,iX2,iX_E0(3)-(iX3-1),iCF)
+
+              ! --- Outer Boundary ---
+
+              U(:,iX1,iX2,iX_E0(3)+iX3,iCF) &
+                = U(:,iX1,iX2,iX_B0(3)+(iX3-1),iCF)
+
+            END DO
+          END DO
+        END DO
+      END DO
+
+    CASE ( 2 ) ! Homogeneous
+
+      DO iCF = 1, nCF
+        DO iX3 = 1, swX(3)
+          DO iX2 = iX_B0(2), iX_E0(2)
+            DO iX1 = iX_B0(1), iX_E0(1)
+
+              ! --- Inner Boundary ---
+
+              U(:,iX1,iX2,iX_B0(3)-iX3,iCF) &
+                = U(:,iX1,iX2,iX_B0(3),iCF)
+
+              ! --- Outer Boundary ---
+
+              U(:,iX1,iX2,iX_E0(3)+iX3,iCF) &
+                = U(:,iX1,iX2,iX_E0(3),iCF)
+
+            END DO
+          END DO
+        END DO
+      END DO
+
+    CASE DEFAULT
+
+      WRITE(*,*)
+      WRITE(*,'(A5,A45,I2.2)') &
+        '', 'Invalid Boundary Condition for Fluid X3: ', bcX(3)
+      STOP
+
+    END SELECT
 
   END SUBROUTINE ApplyBC_Fluid_X3
 
