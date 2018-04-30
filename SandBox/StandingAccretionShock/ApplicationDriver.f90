36ee1c64580bf7437900765b041dd292bad30fc7
Eirik Endeve <endevee@ornl.gov>
2018-04-30 12:53:36 +0000

    Updates to driver to evolve accretion shock problem

--- SandBox/StandingAccretionShock/ApplicationDriver.f90
+++ SandBox/StandingAccretionShock/ApplicationDriver.f90
@@ -1,9 +1,8 @@
 PROGRAM ApplicationDriver
 
   USE KindModule, ONLY: &
-    DP, Half, One, Pi, TwoPi, FourPi
-  USE UnitsModule, ONLY: &
-    Second
+    DP, Half, One, Two, &
+    Pi, TwoPi, FourPi
   USE ProgramHeaderModule, ONLY: &
     iX_B0, iX_B1, iX_E0, iX_E1
   USE ProgramInitializationModule, ONLY: &
@@ -25,7 +24,14 @@ PROGRAM ApplicationDriver
     uCF, uPF, uAF
   USE TimeSteppingModule_SSPRK, ONLY: &
     InitializeFluid_SSPRK, &
-    FinalizeFluid_SSPRK
+    FinalizeFluid_SSPRK, &
+    UpdateFluid_SSPRK
+  USE SlopeLimiterModule_Euler, ONLY: &
+    InitializeSlopeLimiter_Euler, &
+    FinalizeSlopeLimiter_Euler
+  USE PositivityLimiterModule_Euler, ONLY: &
+    InitializePositivityLimiter_Euler, &
+    FinalizePositivityLimiter_Euler
   USE GeometryFieldsModule, ONLY: &
     uGF
   USE EquationOfStateModule, ONLY: &
@@ -33,6 +39,11 @@ PROGRAM ApplicationDriver
     FinalizeEquationOfState
   USE InitializationModule, ONLY: &
     InitializeFields
+  USE EulerEquationsUtilitiesModule_Beta, ONLY: &
+    ComputeFromConserved, &
+    ComputeTimeStep
+  USE dgDiscretizationModule_Euler, ONLY: &
+    ComputeIncrement_Euler_DG_Explicit
 
   IMPLICIT NONE
 
@@ -46,33 +57,36 @@ PROGRAM ApplicationDriver
   REAL(DP), PARAMETER :: Mach   = 1.0d2
 
   INTEGER             :: iCycle, iCycleD, iCycleW
+  INTEGER             :: nX(3), nNodes
   REAL(DP)            :: t, dt, t_end
+
+  nX = [ 256, 1, 1 ]
+
+  nNodes = 2
 
   CALL InitializeProgram &
          ( ProgramName_Option &
              = 'StandingAccretionShock', &
            nX_Option &
-             = [ 128, 32, 1 ], &
+             = nX, &
            swX_Option &
              = [ 1, 1, 1 ], &
            bcX_Option &
-             = [ 1, 1, 1 ], &
+             = [ 11, 3, 1 ], &
            xL_Option &
              = [ 0.2d0, 0.0d0, 0.0d0 ], &
            xR_Option &
              = [ 2.0d0, Pi,    TwoPi ], &
            nNodes_Option &
-             = 2, &
+             = nNodes, &
            CoordinateSystem_Option &
              = 'SPHERICAL', &
-           nStages_SSP_RK_Option &
-             = 1, &  
            BasicInitialization_Option &
              = .TRUE. )
- 
-  t_end = 1.0d-3 * Second
+
+  t_end   = 1.0d+2
   iCycleD = 10
-  iCycleW = 10
+  iCycleW = 1000
 
   CALL InitializeReferenceElementX
 
@@ -84,30 +98,51 @@ PROGRAM ApplicationDriver
   CALL ComputeGravitationalPotential &
          ( iX_B0, iX_E0, iX_B1, iX_E1, uGF, Mass )
 
-  CALL InitializeFluid_SSPRK( nStages = 1 )
+  CALL InitializeFluid_SSPRK( nStages = 2 )
 
   CALL InitializeEquationOfState &
          ( EquationOfState_Option = 'IDEAL', &
            Gamma_IDEAL_Option = Gamma )
 
+  CALL InitializeSlopeLimiter_Euler &
+         ( BetaTVD_Option = 1.8_DP, &
+           BetaTVB_Option = 0.0_DP, &
+           SlopeTolerance_Option = 1.0d-2, &
+           UseSlopeLimiter_Option = .TRUE., &
+           UseTroubledCellIndicator_Option = .FALSE., &
+           LimiterThresholdParameter_Option = 0.12_DP )
+
+  CALL InitializePositivityLimiter_Euler &
+         ( Min_1_Option = 1.0d-12, &
+           Min_2_Option = 1.0d-12, &
+           UsePositivityLimiter_Option = .TRUE. )
+
   CALL InitializeFields &
-         ( mDot, Mass, rShock, Gamma, Mach ) 
+         ( mDot, Mass, rShock, Gamma, Mach )
 
   CALL WriteFieldsHDF &
          ( 0.0_DP, WriteGF_Option = .TRUE., WriteFF_Option = .TRUE. )
 
-  iCycle = 0
+  ! --- Evolve ---
 
+  t = 0.0_DP
+
+  iCycle = 0
   DO WHILE ( t < t_end )
 
-    IF( t + dt < t_end )THEN
-       t = t + dt
-    ELSE
-       dt = t_end - t
-       t = t_end
-    END IF
-
     iCycle = iCycle + 1
+
+    CALL ComputeTimeStep &
+           ( iX_B0, iX_E0, &
+             uGF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
+             uCF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
+             CFL = 0.3_DP / ( Two * DBLE( nNodes - 1 ) + One ), TimeStep = dt )
+
+    IF( t + dt > t_end )THEN
+
+      dt = t_end - t
+
+    END IF
 
     IF( MOD( iCycle, iCycleD ) == 0 )THEN
 
@@ -116,13 +151,19 @@ PROGRAM ApplicationDriver
 
     END IF
 
-!    CALL UpdateFluid_SSPRK &
-!           ( t, dt, uGF, uCF, ComputeIncrement_Euler_GR_DG_Explicit )
+    CALL UpdateFluid_SSPRK &
+           ( t, dt, uGF, uCF, ComputeIncrement_Euler_DG_Explicit )
 
-    ! --- Update primitive fluid variables, pressure, and sound speed
-!    CALL ComputeFromConserved( iX_B0, iX_E0, uGF, uCF, uPF, uAF )
+    t = t + dt
 
     IF( MOD( iCycle, iCycleW ) == 0 )THEN
+
+      CALL ComputeFromConserved &
+             ( iX_B0, iX_E0, &
+               uGF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
+               uCF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
+               uPF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
+               uAF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:) )
 
       CALL WriteFieldsHDF &
              ( t, WriteGF_Option = .TRUE., WriteFF_Option = .TRUE. )
@@ -131,16 +172,19 @@ PROGRAM ApplicationDriver
 
   END DO
 
-
   ! --- Finalize ---
 
-  CALL FinalizeReferenceElementX
+  CALL FinalizePositivityLimiter_Euler
 
-  CALL FinalizeReferenceElementX_Lagrange
+  CALL FinalizeSlopeLimiter_Euler
 
   CALL FinalizeEquationOfState
 
   CALL FinalizeFluid_SSPRK
+
+  CALL FinalizeReferenceElementX_Lagrange
+
+  CALL FinalizeReferenceElementX
 
   CALL FinalizeProgram
 
