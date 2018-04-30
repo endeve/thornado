5f6c5a20a6bedf0e032d0b94b35923cfb33a229f
Eirik Endeve <endevee@ornl.gov>
2018-04-30 15:44:43 +0000

    Updated boundary conditions

--- SandBox/dgExperiments_Euler_GR/BoundaryConditionsModule_Beta.f90
+++ SandBox/dgExperiments_Euler_GR/BoundaryConditionsModule_Beta.f90
@@ -144,14 +144,15 @@ CONTAINS
 
     CASE ( 10 ) ! Custom BCs for Accretion Problem
 
-      R_0 = MeshX(1) % Center(1)
+      R_0 = MeshX(1) % Center(1) &
+              + MeshX(1) % Width(1) &
+                  * MeshX(1) % Nodes(1)
 
       DO iX3 = iX_B0(3), iX_E0(3)
         DO iX2 = iX_B0(2), iX_E0(2)
 
-          ! Eventually change to include curvilinear coordinates
-          D_0 = DOT_PRODUCT( WeightsX_q(:), U(:,iX_B0(1),iX2,iX3,iCF_D) )
-          E_0 = DOT_PRODUCT( WeightsX_q(:), U(:,iX_B0(1),iX2,iX3,iCF_E) )
+          D_0 = U(1,iX_B0(1),iX2,iX3,iCF_D)
+          E_0 = U(1,iX_B0(1),iX2,iX3,iCF_E)
           
           DO iX1 = 1, swX(1)
 
@@ -164,17 +165,8 @@ CONTAINS
               R_q = NodeCoordinate( MeshX(1), iX_B0(1)-iX1, iNodeX1 )
 
               U(iNodeX,iX_B0(1)-iX1,iX2,iX3,iCF_D) &
-                = D_0 * ( R_0 / R_q ) ** 3 
+                = D_0 * ( R_0 / R_q ) ** 3
               
-!              U(iNodeX,iX_B0(1)-iX1,iX2,iX3,iCF_S1) &
-!                = 0.0_DP
-
-!              U(iNodeX,iX_B0(1)-iX1,iX2,iX3,iCF_S2) &
-!                = 0.0_DP
-            
-!              U(iNodeX,iX_B0(1)-iX1,iX2,iX3,iCF_S3) &
-!                = 0.0_DP
- 
               U(iNodeX,iX_B0(1)-iX1,iX2,iX3,iCF_E) &
                 = E_0 * ( R_0 / R_q ) ** 4
 
