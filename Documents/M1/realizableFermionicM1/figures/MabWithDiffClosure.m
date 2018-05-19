da620aecc973bb60c1ff3e2051777d22ba960322
Ran Chu <rchu@vols.utk.edu>
2018-05-19 10:08:39 +0000

    Changed Mab plots.

--- Documents/M1/realizableFermionicM1/figures/MabWithDiffClosure.m
+++ Documents/M1/realizableFermionicM1/figures/MabWithDiffClosure.m
@@ -1,9 +1,6 @@
 % close all
 clear all
 
-nPts = 10;
-
-
 PolynomialCB = @(x) x^2 * ( 3.0 - abs(x) + 3.0 * x^2 ) / 5.0;
 EddingtonFCB = @(e,f) (2.0/3.0)*(1.0-e)*(1.0-2.0*e)*PolynomialCB(f/(1.0-e))+1.0/3.0;
 PolynomialBL = @(x) ( 9.0 * x^2 - 5.0 + sqrt( 33.0 * x^4 - 42.0 * x^2 + 25.0 ) ) /8.0;
@@ -11,50 +8,27 @@ EddingtonFBL = @(e,f) (2.0/3.0)*(1.0-e)*(1.0-2.0*e)*PolynomialBL(f/(1.0-e))+1.0/
 PolynomialKS = @(x) x^2;
 EddingtonFKS = @(e,f) (2.0/3.0)*(1.0-e)*(1.0-2.0*e)*PolynomialKS(f/(1.0-e))+1.0/3.0; 
 
-A = zeros(nPts,1);
-B = zeros(nPts,1);
-for iPt = 1 : nPts
-  
-  A(iPt) = 1.d2*sign(2*(0.5-rand))*10^(6*(rand-1.0));
-  B(iPt) = 1.d2*sign(2*(0.5-rand))*10^(6*(rand-1.0));
-  
+nPts = 2000000;
+J_FD = zeros(nPts,1);
+H_FD = zeros(nPts,1);
+iPt = 1;
+while( iPt <= nPts )
+    J_FD(iPt) = rand(1);
+    temp = (rand(1)-0.5)*0.5;
+    if( abs(temp) <= (1-J_FD(iPt))*J_FD(iPt) )
+        H_FD(iPt) = temp;
+        iPt = iPt + 1;
+    end
 end
 
-nMu = 256;
-MuL = - 1.0;
-MuR = + 1.0;
-Mu  = linspace( MuL, MuR, nMu )';
-dMu = ( MuR - MuL ) / nMu;
-
-% Compute Angular Distributions for Various Parameters:
-
-f_FD = zeros(nMu,nPts);
-
-for iPt = 1 : nPts
-  for iMu = 1 : nMu
-    
-    f_FD(iMu,iPt) = 1.0/(exp(A(iPt)+B(iPt)*Mu(iMu))+1.0); % Fermi-Dirac
-    
-  end
-end
-
-% Compute Angular Moments:
-
-J_FD     = zeros(nPts,1);
-H_FD     = zeros(nPts,1);
-K_FD     = zeros(nPts,1);
 K_CB = zeros(nPts,1);
 K_BL = zeros(nPts,1);
 K_MI = zeros(nPts,1);
 K_KS = zeros(nPts,1);
 
-
 tic
 for iPt = 1 : nPts
   
-  J_FD(iPt)     = + 0.5 * Trapez( Mu, Mu.^0 .* f_FD(:,iPt) );
-  H_FD(iPt)     = + 0.5 * Trapez( Mu, Mu.^1 .* f_FD(:,iPt) );
-  K_FD(iPt)     = + 0.5 * Trapez( Mu, Mu.^2 .* f_FD(:,iPt) );
   K_CB(iPt) = EddingtonFCB( J_FD(iPt), H_FD(iPt)/J_FD(iPt) ) * J_FD(iPt);
   K_BL(iPt) = EddingtonFBL( J_FD(iPt), H_FD(iPt)/J_FD(iPt) ) * J_FD(iPt);
   K_MI(iPt) = EddingtonFCB( 0.0,       H_FD(iPt)/J_FD(iPt) ) * J_FD(iPt);
@@ -62,9 +36,6 @@ for iPt = 1 : nPts
   
 end
 toc
-
-f_FD = 0;
-f_MB = 0;
 
 ClosureName = {'BLKS', 'CBME', 'BLME','MI'};
 
