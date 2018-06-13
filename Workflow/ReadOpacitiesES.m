9b40c69e43abf68ea42dec09bec4c41985434053
Eirik Endeve <endevee@ornl.gov>
2018-06-13 17:30:52 +0000

    Matlab functions to read opacities

---
+++ Workflow/ReadOpacitiesES.m
@@ -0,0 +1,21 @@
+function...
+  [ Time, E, X1, X2, X3, opES ]...
+    = ReadOpacitiesES( AppName, FileNumber, Directory )
+
+  if( exist( 'Directory', 'var' ) )
+    DirName = Directory;
+  else
+    DirName = './Output';
+  end
+
+  FileName = [ DirName '/' AppName '_Opacities_' sprintf( '%06d', FileNumber ) '.h5' ];
+
+  Time = h5read( FileName, '/Time' );
+  E    = h5read( FileName, '/Energy Grid/E' );
+  X1   = h5read( FileName, '/Spatial Grid/X1' );
+  X2   = h5read( FileName, '/Spatial Grid/X2' );
+  X3   = h5read( FileName, '/Spatial Grid/X3' );
+  
+  opES = h5read( FileName, '/Opacities/Species_01/Elastic Scattering Opacities' );
+
+end
