6d1e05800021b0d83a8c2b770f8eb2fb5eef6251
Eirik Endeve <endevee@ornl.gov>
2018-09-28 17:50:18 +0000

    Returning to nodal collision update

--- SandBox/Interface_CASTRO/TimeSteppingModule_Castro.f90
+++ SandBox/Interface_CASTRO/TimeSteppingModule_Castro.f90
@@ -149,7 +149,7 @@ CONTAINS
 
     ! --- Implicit Step ---
 
-    CALL ComputeIncrement_TwoMoment_Implicit_DGFV &
+    CALL ComputeIncrement_TwoMoment_Implicit &
            ( iZ_B0-iZ_SW, iZ_E0+iZ_SW, iZ_B1, iZ_E1, dt, &
              uGE, uGF, &
              U_F, Q1_F &
@@ -221,7 +221,7 @@ CONTAINS
 
     ! --- Implicit Step ---
 
-    CALL ComputeIncrement_TwoMoment_Implicit_DGFV &
+    CALL ComputeIncrement_TwoMoment_Implicit &
            ( iZ_B0-iZ_SW, iZ_E0+iZ_SW, iZ_B1, iZ_E1, Half * dt, &
              uGE, uGF, &
              U_F, Q1_F &
