da18ea5c6c730f6fdb14231e312d507d8f841b01
Eirik Endeve <endevee@ornl.gov>
2018-11-05 20:26:29 +0000

    Skeleton for refinement/coarsening of two moment data

---
+++ Modules/TwoMoment/TwoMoment_MeshRefinementModule.f90
@@ -0,0 +1,37 @@
+MODULE TwoMoment_MeshRefinementModule
+
+  USE KindModule, ONLY: &
+    DP
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: InitializeMeshRefinement_TwoMoment
+  PUBLIC :: FinalizeMeshRefinement_TwoMoment
+  PUBLIC :: Refine_TwoMoment
+  PUBLIC :: Coarsen_TwoMoment
+
+CONTAINS
+
+
+  SUBROUTINE InitializeMeshRefinement_TwoMoment
+
+  END SUBROUTINE InitializeMeshRefinement_TwoMoment
+
+
+  SUBROUTINE FinalizeMeshRefinement_TwoMoment
+
+  END SUBROUTINE FinalizeMeshRefinement_TwoMoment
+
+
+  SUBROUTINE Refine_TwoMoment
+
+  END SUBROUTINE Refine_TwoMoment
+
+
+  SUBROUTINE Coarsen_TwoMoment
+
+  END SUBROUTINE Coarsen_TwoMoment
+
+
+END MODULE TwoMoment_MeshRefinementModule
