3f21af4441fb818f48964530467987ad6657d083
Eirik Endeve <eendeve@ornl.gov>
2018-11-22 16:20:22 -0500

    Progress on driver

--- SandBox/AMReX/Euler/fmain.f90
+++ SandBox/AMReX/Euler/fmain.f90
@@ -7,13 +7,27 @@ PROGRAM main
 
   ! --- thornado Modules ---
 
-  USE KindModule, only: DP
+  USE KindModule,                       ONLY: &
+    DP
+  USE ProgramHeaderModule,              ONLY: &
+    InitializeProgramHeader,                  &
+    DescribeProgramHeaderX
+  USE PolynomialBasisModuleX_Lagrange,  ONLY: &
+    InitializePolynomialBasisX_Lagrange
+  USE PolynomialBasisModuleX_Legendre,  ONLY: &
+    InitializePolynomialBasis_Legendre
+  USE ReferenceElementModuleX,          ONLY: &
+    InitializeReferenceElementX
+  USE ReferenceElementModuleX_Lagrange, ONLY: &
+    InitializeReferenceElementX_Lagrange
 
   IMPLICIT NONE
 
   CHARACTER(LEN=:), ALLOCATABLE :: ProgramName
+  INTEGER :: iDim
   INTEGER :: nNodes
   INTEGER :: nStages
+  INTEGER :: nX(3) = 1, swX(3) = 0
   INTEGER, PARAMETER :: nComp = 10
   INTEGER, PARAMETER :: nGhost = 2
   INTEGER, ALLOCATABLE  :: n_cell(:)
@@ -59,6 +73,24 @@ PROGRAM main
 
   PRINT*, "n_cell = ", n_cell
   PRINT*, "max_grid_size = ", max_grid_size
+
+  DO iDim = 1, amrex_spacedim
+    nX (iDim) = n_cell(iDim)
+    swX(iDim) = nGhost
+  END DO
+
+  CALL InitializeProgramHeader &
+         ( nNodes_Option = nNodes, nX_Option = nX, swX_Option = swX )
+
+  CALL DescribeProgramHeaderX
+print*,"Calling InitializePolynomialBasisX_Lagrange"
+  CALL InitializePolynomialBasisX_Lagrange
+print*,"Calling InitializePolynomialBasisX_Legendre"
+  CALL InitializePolynomialBasis_Legendre
+print*,"Calling InitializeReferenceElementX"
+  CALL InitializeReferenceElementX
+print*,"Calling InitializeReferenceElementX_Lagrange"
+  CALL InitializeReferenceElementX_Lagrange
 
   BX = amrex_box( [ 0, 0, 0 ], [ n_cell(1)-1, n_cell(2)-1, n_cell(3)-1 ] )
 
