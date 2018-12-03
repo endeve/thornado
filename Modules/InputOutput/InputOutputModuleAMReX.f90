e0eac29b178cc62c0d90f184db15fbd2611c1a31
Eirik Endeve <eendeve@ornl.gov>
2018-12-03 18:27:56 -0500

    Some progress on IO

--- Modules/InputOutput/InputOutputModuleAMReX.f90
+++ Modules/InputOutput/InputOutputModuleAMReX.f90
@@ -8,6 +8,8 @@ MODULE InputOutputModuleAMReX
 
   USE KindModule, ONLY: &
     DP
+  USE GeometryFieldsModule, ONLY: &
+    nGF
 
   IMPLICIT NONE
   PRIVATE
@@ -19,9 +21,26 @@ MODULE InputOutputModuleAMReX
 CONTAINS
 
 
-  SUBROUTINE WriteFieldsAMReX_PlotFile( Time )
+  SUBROUTINE WriteFieldsAMReX_PlotFile &
+    ( Time, GEOM, MF_uGF_Option, MF_uCF_Option, MF_uPF_Option, MF_uAF_Option )
 
-    REAL(DP), INTENT(in) :: Time
+    REAL(DP),             INTENT(in)           :: Time
+    TYPE(amrex_geometry), INTENT(in)           :: GEOM
+    TYPE(amrex_multifab), INTENT(in), OPTIONAL :: MF_uGF_Option
+    TYPE(amrex_multifab), INTENT(in), OPTIONAL :: MF_uCF_Option
+    TYPE(amrex_multifab), INTENT(in), OPTIONAL :: MF_uPF_Option
+    TYPE(amrex_multifab), INTENT(in), OPTIONAL :: MF_uAF_Option
+
+    LOGICAL :: WriteGF
+    LOGICAL :: WriteFF_C
+    INTEGER :: MF_nComp
+    TYPE(amrex_multifab) :: MF_PF
+
+    IF( PRESENT( MF_uGF_Option ) )THEN
+      WriteGF  = .TRUE.
+    ELSE
+      WriteGF  = .FALSE.
+    END IF
 
     IF( amrex_parallel_ioprocessor() )THEN
 
@@ -30,7 +49,27 @@ CONTAINS
 
     END IF
 
+    IF( WriteGF )THEN
+
+      CALL amrex_multifab_build &
+             ( MF_PF, MF_uGF_Option % BA, MF_uGF_Option % DM, nGF, 0 )
+
+      CALL MF_ComputeElementAverage( nGF, MF_uGF_Option, MF_PF )
+
+      CALL amrex_multifab_destroy( MF_PF )
+
+    END IF
+
   END SUBROUTINE WriteFieldsAMReX_PlotFile
+
+
+  SUBROUTINE MF_ComputeElementAverage( nComp, MF, MF_A )
+
+    INTEGER,              INTENT(in   ) :: nComp
+    TYPE(amrex_multifab), INTENT(in   ) :: MF
+    TYPE(amrex_multifab), INTENT(inout) :: MF_A
+
+  END SUBROUTINE MF_ComputeElementAverage
 
 
 END MODULE InputOutputModuleAMReX
