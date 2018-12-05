959f841359e8d3b22eace20d5a814c3ebb12285d
Eirik Endeve <eendeve@ornl.gov>
2018-12-04 22:43:54 -0500

    Progress on IO

--- Modules/InputOutput/InputOutputModuleAMReX.f90
+++ Modules/InputOutput/InputOutputModuleAMReX.f90
@@ -6,19 +6,20 @@ MODULE InputOutputModuleAMReX
 
   ! --- thornado Modules ---
 
-  USE KindModule, ONLY: &
+  USE KindModule,              ONLY: &
     DP
-  USE ProgramHeaderModule, ONLY: &
+  USE ProgramHeaderModule,     ONLY: &
     nDOFX
   USE ReferenceElementModuleX, ONLY: &
     WeightsX_q
-  USE GeometryFieldsModule, ONLY: &
-    nGF
+  USE GeometryFieldsModule,    ONLY: &
+    nGF, namesGF
 
   IMPLICIT NONE
   PRIVATE
 
-  INTEGER :: PlotFileNumber = 0
+  CHARACTER(8) :: BaseFileName = 'thornado'
+  INTEGER      :: PlotFileNumber = 0
 
   PUBLIC :: WriteFieldsAMReX_PlotFile
 
@@ -35,22 +36,25 @@ CONTAINS
     TYPE(amrex_multifab), INTENT(in), OPTIONAL :: MF_uPF_Option
     TYPE(amrex_multifab), INTENT(in), OPTIONAL :: MF_uAF_Option
 
-    LOGICAL :: WriteGF
-    LOGICAL :: WriteFF_C
-    INTEGER :: MF_nComp
-    TYPE(amrex_multifab) :: MF_PF
-
-    ! --- Only for debugging ---
-    LOGICAL :: DEBUG = .FALSE.
-    TYPE(amrex_mfiter) :: MFI
-    REAL(amrex_real), CONTIGUOUS, POINTER :: u(:,:,:,:)
-    INTEGER :: iX1, iX2, iX3, iComp
-
+    CHARACTER(08)                   :: NumberString
+    CHARACTER(32)                   :: PlotFileName
+    LOGICAL                         :: WriteGF
+    LOGICAL                         :: WriteFF_C
+    INTEGER                         :: iComp, nLevels
+    INTEGER                         :: MF_nComp
+    TYPE(amrex_multifab)            :: MF_PF
+    TYPE(amrex_string), ALLOCATABLE :: VarNames(:)
 
     IF( PRESENT( MF_uGF_Option ) )THEN
-      WriteGF  = .TRUE.
+      WriteGF = .TRUE.
     ELSE
-      WriteGF  = .FALSE.
+      WriteGF = .FALSE.
+    END IF
+
+    IF( PRESENT( MF_uCF_Option ) )THEN
+      WriteFF_C = .TRUE.
+    ELSE
+      WriteFF_C = .FALSE.
     END IF
 
     IF( amrex_parallel_ioprocessor() )THEN
@@ -60,102 +64,97 @@ CONTAINS
 
     END IF
 
+    WRITE(NumberString,'(I8.8)') PlotFileNumber
+
+    PlotFileName = TRIM( BaseFileName ) // '_' // NumberString
+
+    nLevels = 1
+
     IF( WriteGF )THEN
+
+      ALLOCATE( VarNames(nGF) )
+
+      DO iComp = 1, nGF
+
+        CALL amrex_string_build &
+               ( VarNames(iComp), TRIM( namesGF(iComp) ) )
+
+      END DO
 
       CALL amrex_multifab_build &
              ( MF_PF, MF_uGF_Option % BA, MF_uGF_Option % DM, nGF, 0 )
 
-      CALL MF_ComputeElementAverage( nGF, MF_uGF_Option, MF_PF )
+      CALL MF_ComputeCellAverage( nGF, MF_uGF_Option, MF_PF )
 
-      IF( DEBUG )THEN
+      CALL amrex_write_plotfile &
+             ( PlotFileName, nLevels, [ MF_PF ], VarNames, &
+               [ GEOM ], Time, [ PlotFileNumber ], [1] )
 
-        CALL amrex_mfiter_build( MFI, MF_uGF_Option, tiling = .TRUE. )
-
-        u => MF_PF % DataPtr( MFI )
-        iX1 = 1
-        iX2 = 1
-        iX3 = 1
-        iComp = 8
-        WRITE(*,*) 'iX1, iX2, iX3: ', iX1, iX2, iX3
-        WRITE(*,*) 'iComp: ', iComp
-        WRITE(*,*) 'Cell-average: ', u(iX1,iX2,iX3,iComp)
-
-      END IF
+      DEALLOCATE( VarNames )
 
       CALL amrex_multifab_destroy( MF_PF )
+
+    END IF
+
+    IF( WriteFF_C )THEN
+
+      ! --- Write Conserved Fluid Fields Here ---
 
     END IF
 
   END SUBROUTINE WriteFieldsAMReX_PlotFile
 
 
-  SUBROUTINE MF_ComputeElementAverage( nComp, MF, MF_A )
+  SUBROUTINE MF_ComputeCellAverage( nComp, MF, MF_A )
 
     INTEGER,              INTENT(in   ) :: nComp
     TYPE(amrex_multifab), INTENT(in   ) :: MF
     TYPE(amrex_multifab), INTENT(inout) :: MF_A
 
-    INTEGER                               :: iX1, iX2, iX3, iComp
-    INTEGER                               :: lo(4), hi(4)
-    REAL(amrex_real)                      :: u_K(nDOFX,nComp)
-    TYPE(amrex_box)                       :: BX
-    TYPE(amrex_mfiter)                    :: MFI
-    REAL(amrex_real), CONTIGUOUS, POINTER :: u(:,:,:,:), u_A(:,:,:,:)
-
-    LOGICAL :: DEBUG = .FALSE.
+    INTEGER            :: iX1, iX2, iX3, iComp
+    INTEGER            :: lo(4), hi(4)
+    REAL(amrex_real)   :: u_K(nDOFX,nComp)
+    TYPE(amrex_box)    :: BX
+    TYPE(amrex_mfiter) :: MFI
+    REAL(amrex_real), CONTIGUOUS, POINTER :: u  (:,:,:,:)
+    REAL(amrex_real), CONTIGUOUS, POINTER :: u_A(:,:,:,:)
 
     CALL amrex_mfiter_build( MFI, MF, tiling = .TRUE. )
 
     DO WHILE( MFI % next() )
 
       u   => MF   % DataPtr( MFI )
-      IF( DEBUG ) &
-        WRITE(*,*) 'SHAPE( u   ): ', SHAPE( u )
-
       u_A => MF_A % DataPtr( MFI )
-      IF( DEBUG ) &
-        WRITE(*,*) 'SHAPE( u_A ): ', SHAPE( u_A )
 
       BX = MFI % tilebox()
 
       lo = LBOUND( u ); hi = UBOUND( u )
 
       DO iX3 = BX % lo(3), BX % hi(3)
-        DO iX2 = BX % lo(2), BX % hi(2)
-          DO iX1 = BX % lo(1), BX % hi(1)
+      DO iX2 = BX % lo(2), BX % hi(2)
+      DO iX1 = BX % lo(1), BX % hi(1)
 
-            IF( DEBUG ) &
-              WRITE(*,*) 'iX1, iX2, iX3 = ', iX1, iX2, iX3
+        u_K(1:nDOFX,1:nComp) &
+          = RESHAPE( u(iX1,iX2,iX3,lo(4):hi(4)), [ nDOFX, nComp ] )
 
-            u_K(1:nDOFX,1:nComp) &
-              = RESHAPE( u(iX1,iX2,iX3,lo(4):hi(4)), [ nDOFX, nComp ] )
+        ! --- Compute cell-average ---
 
-            ! --- Compute cell-average ---
-            DO iComp = 1, nComp
+        DO iComp = 1, nComp
 
-              IF( DEBUG )THEN
-                WRITE(*,*) 'iComp: ', iComp
-                WRITE(*,*) 'Nodal values:', u_K(1:nDOFX,iComp)
-              END IF
+          u_A(iX1,iX2,iX3,iComp) &
+            = DOT_PRODUCT( WeightsX_q(:), u_K(:,iComp) )
 
-              u_A(iX1,iX2,iX3,iComp) = SUM( WeightsX_q * u_K(:,iComp) )
-
-              IF( DEBUG )THEN
-                WRITE(*,*) 'Cell-average:', u_A(iX1,iX2,iX3,iComp)
-                WRITE(*,*)
-              END IF
-
-            END DO
-
-          END DO
         END DO
+
+      END DO
+      END DO
       END DO
 
     END DO
 
     CALL amrex_mfiter_destroy( MFI )
 
-  END SUBROUTINE MF_ComputeElementAverage
+  END SUBROUTINE MF_ComputeCellAverage
 
 
 END MODULE InputOutputModuleAMReX
