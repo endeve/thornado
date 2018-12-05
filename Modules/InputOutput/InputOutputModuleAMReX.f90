870347a659994d034421c8c9b824c5a8ed57fc6f
Samuel John Dunham <samueljdunham@gmail.com>
2018-12-05 14:17:03 -0500

    Added conserved, primitive, and auxiliary fields to InputOutModuleAMReX file

--- Modules/InputOutput/InputOutputModuleAMReX.f90
+++ Modules/InputOutput/InputOutputModuleAMReX.f90
@@ -13,7 +13,11 @@ MODULE InputOutputModuleAMReX
   USE ReferenceElementModuleX, ONLY: &
     WeightsX_q
   USE GeometryFieldsModule,    ONLY: &
-    nGF, namesGF
+    nGF, ShortNamesGF
+  USE FluidFieldsModule,       ONLY: &
+    nCF, ShortNamesCF, &
+    nPF, ShortNamesPF, &
+    nAF, ShortNamesAF
 
   IMPLICIT NONE
   PRIVATE
@@ -39,23 +43,23 @@ CONTAINS
     CHARACTER(08)                   :: NumberString
     CHARACTER(32)                   :: PlotFileName
     LOGICAL                         :: WriteGF
-    LOGICAL                         :: WriteFF_C
+    LOGICAL                         :: WriteFF_C, WriteFF_P, WriteFF_A
     INTEGER                         :: iComp, nLevels
-    INTEGER                         :: MF_nComp
     TYPE(amrex_multifab)            :: MF_PF
     TYPE(amrex_string), ALLOCATABLE :: VarNames(:)
 
-    IF( PRESENT( MF_uGF_Option ) )THEN
-      WriteGF = .TRUE.
-    ELSE
-      WriteGF = .FALSE.
-    END IF
+    WriteGF   = .FALSE.
+    IF( PRESENT( MF_uGF_Option ) ) WriteGF   = .TRUE.
 
-    IF( PRESENT( MF_uCF_Option ) )THEN
-      WriteFF_C = .TRUE.
-    ELSE
-      WriteFF_C = .FALSE.
-    END IF
+    WriteFF_C = .FALSE.
+    IF( PRESENT( MF_uCF_Option ) ) WriteFF_C = .TRUE.
+
+    WriteFF_P = .FALSE.
+    IF( PRESENT( MF_uPF_Option ) ) WriteFF_P = .TRUE.
+
+    WriteFF_A = .FALSE.
+    IF( PRESENT( MF_uAF_Option ) ) WriteFF_A = .TRUE.
+
 
     IF( amrex_parallel_ioprocessor() )THEN
 
@@ -66,18 +70,19 @@ CONTAINS
 
     WRITE(NumberString,'(I8.8)') PlotFileNumber
 
-    PlotFileName = TRIM( BaseFileName ) // '_' // NumberString
-
     nLevels = 1
 
+    ! --- Geometry fields ---
     IF( WriteGF )THEN
+
+      PlotFileName = TRIM( BaseFileName ) // '_GeometryFields_' // NumberString
 
       ALLOCATE( VarNames(nGF) )
 
       DO iComp = 1, nGF
 
         CALL amrex_string_build &
-               ( VarNames(iComp), TRIM( namesGF(iComp) ) )
+               ( VarNames(iComp), TRIM( ShortNamesGF(iComp) ) )
 
       END DO
 
@@ -90,15 +95,96 @@ CONTAINS
              ( PlotFileName, nLevels, [ MF_PF ], VarNames, &
                [ GEOM ], Time, [ PlotFileNumber ], [1] )
 
-      DEALLOCATE( VarNames )
-
       CALL amrex_multifab_destroy( MF_PF )
+
+      DEALLOCATE( VarNames )
 
     END IF
 
+    ! --- Conserved fluid fields ---
     IF( WriteFF_C )THEN
 
-      ! --- Write Conserved Fluid Fields Here ---
+      PlotFileName = TRIM( BaseFileName ) // '_FluidFields_C_' // NumberString
+
+      ALLOCATE( VarNames(nCF) )
+
+      DO iComp = 1, nCF
+
+         CALL amrex_string_build &
+                ( VarNames(iComp), TRIM( ShortNamesCF(iComp) ) )
+
+      END DO
+
+      CALL amrex_multifab_build &
+             ( MF_PF, MF_uCF_Option % BA, MF_uCF_Option % DM, nCF, 0 )
+
+      CALL MF_ComputeCellAverage( nCF, MF_uCF_Option, MF_PF )
+
+      CALL amrex_write_plotfile &
+             ( PlotFileName, nLevels, [ MF_PF ], VarNames, &
+               [ GEOM ], Time, [ PlotFileNumber ], [1] )
+
+      CALL amrex_multifab_destroy( MF_PF )
+
+      DEALLOCATE( VarNames )
+
+    END IF
+
+    ! --- Primitive fluid fields ---
+    IF( WriteFF_P )THEN
+
+      PlotFileName = TRIM( BaseFileName ) // '_FluidFields_P_' // NumberString
+
+      ALLOCATE( VarNames(nPF) )
+
+      DO iComp = 1, nPF
+
+         CALL amrex_string_build &
+                ( VarNames(iComp), TRIM( ShortNamesPF(iComp) ) )
+
+      END DO
+
+      CALL amrex_multifab_build &
+             ( MF_PF, MF_uPF_Option % BA, MF_uPF_Option % DM, nPF, 0 )
+
+      CALL MF_ComputeCellAverage( nPF, MF_uPF_Option, MF_PF )
+
+      CALL amrex_write_plotfile &
+             ( PlotFileName, nLevels, [ MF_PF ], VarNames, &
+               [ GEOM ], Time, [ PlotFileNumber ], [1] )
+
+      CALL amrex_multifab_destroy( MF_PF )
+
+      DEALLOCATE( VarNames )
+
+    END IF
+
+    ! --- Auxiliary fluid fields ---
+    IF( WriteFF_A )THEN
+
+      PlotFileName = TRIM( BaseFileName ) // '_FluidFields_A_' // NumberString
+
+      ALLOCATE( VarNames(nAF) )
+
+      DO iComp = 1, nAF
+
+         CALL amrex_string_build &
+                ( VarNames(iComp), TRIM( ShortNamesAF(iComp) ) )
+
+      END DO
+
+      CALL amrex_multifab_build &
+             ( MF_PF, MF_uAF_Option % BA, MF_uAF_Option % DM, nAF, 0 )
+
+      CALL MF_ComputeCellAverage( nAF, MF_uAF_Option, MF_PF )
+
+      CALL amrex_write_plotfile &
+             ( PlotFileName, nLevels, [ MF_PF ], VarNames, &
+               [ GEOM ], Time, [ PlotFileNumber ], [1] )
+
+      CALL amrex_multifab_destroy( MF_PF )
+
+      DEALLOCATE( VarNames )
 
     END IF
 
@@ -138,7 +224,6 @@ CONTAINS
           = RESHAPE( u(iX1,iX2,iX3,lo(4):hi(4)), [ nDOFX, nComp ] )
 
         ! --- Compute cell-average ---
-
         DO iComp = 1, nComp
 
           u_A(iX1,iX2,iX3,iComp) &
