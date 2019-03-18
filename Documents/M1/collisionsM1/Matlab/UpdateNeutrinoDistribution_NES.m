f501c58a8bb26e45c07bace03694b1a10805cdd4
Ming Tse Paul Laiu <plc@titanmath.ornl.gov>
2019-03-18 16:22:11 -0400

    NES kernal (incomplete)

---
+++ Documents/M1/collisionsM1/Matlab/UpdateNeutrinoDistribution_NES.m
@@ -0,0 +1,32 @@
+function [Jout, iter] = UpdateNeutrinoDistribution_NES(Jin, J0, Chi, R_in_NES, R_out_NES)
+
+global g_W2_N;
+
+maxIter = 100;
+Rtol = 1e-8;
+% initial guess
+Jout = Jin;
+
+k = 0;
+CONVERGED = false;
+
+while((~CONVERGED)&&(k<=maxIter))
+    k = k + 1;
+    
+    % solve (66) and (75) in the tech report
+    % Picard iteration
+    NES_in = (4*pi - Jout) .* (R_in_NES*diag(g_W2_N)*Jout);
+    NES_out = Jout .* (R_out_NES*diag(g_W2_N)*(4*pi - Jout));
+    Jnew = (Jin + Chi.*J0 + NES_in - NES_out)./(1 + Chi);
+    
+    % check convergence
+    if (norm(Jnew - Jout)< Rtol * norm(Jin))
+        CONVERGED = true;
+    end
+
+    Jout = Jnew;
+end
+iter = k;
+
+end
+
