ec3d75b1cd972f3ffcd701ec79b251dce65af8ae
Ming Tse Paul Laiu <plc@titanmath.ornl.gov>
2019-03-19 09:36:17 -0400

    NES kernal incomplete

--- Documents/M1/collisionsM1/Matlab/UpdateNeutrinoDistribution_NES.m
+++ Documents/M1/collisionsM1/Matlab/UpdateNeutrinoDistribution_NES.m
@@ -10,13 +10,15 @@ Jout = Jin;
 k = 0;
 CONVERGED = false;
 
+%W2_N = 4 * pi * g_W2_N;
+
 while((~CONVERGED)&&(k<=maxIter))
     k = k + 1;
     
     % solve (66) and (75) in the tech report
     % Picard iteration
-    NES_in = (4*pi - Jout) .* (R_in_NES*diag(g_W2_N)*Jout);
-    NES_out = Jout .* (R_out_NES*diag(g_W2_N)*(4*pi - Jout));
+    NES_in = (4 * pi - Jout) .* (R_in_NES'*diag(g_W2_N)*Jout);
+    NES_out = Jout .* (R_out_NES'*diag(g_W2_N)*(4 * pi - Jout));
     Jnew = (Jin + Chi.*J0 + NES_in - NES_out)./(1 + Chi);
     
     % check convergence
