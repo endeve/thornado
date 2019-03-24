dd88333e5c4a2269519d78a9e0cee0a5d27d96bb
Eirik Endeve <eendeve@ornl.gov>
2019-03-24 15:50:52 -0400

    Initialization of convergence data for implicit solve

--- Modules/TwoMoment/TwoMoment_DiscretizationModule_Collisions_Neutrinos.F90
+++ Modules/TwoMoment/TwoMoment_DiscretizationModule_Collisions_Neutrinos.F90
@@ -158,6 +158,10 @@ CONTAINS
     ALLOCATE( Sig(nE_G,nSpecies,nDOFX) )
     ALLOCATE( fEQ(nE_G,nSpecies,nDOFX) )
 
+    Iterations_Min = + HUGE( 1 )
+    Iterations_Max = - HUGE( 1 )
+    Iterations_Ave = 0
+
     DO iX3 = iX_B0(3), iX_E0(3)
     DO iX2 = iX_B0(2), iX_E0(2)
     DO iX1 = iX_B0(1), iX_E0(1)
