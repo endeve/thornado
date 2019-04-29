8327cccc212eba3c3b7ac84bc4b74f8fe65a7204
Austin Harris <jaharris87@users.noreply.github.com>
2019-04-29 11:14:48 -0400

    Use MAP( release: instead of MAP( delete:

--- Modules/Library/ReferenceElementModule.F90
+++ Modules/Library/ReferenceElementModule.F90
@@ -223,10 +223,10 @@ CONTAINS
 
 #if defined(THORNADO_OMP_OL)
     !$OMP TARGET EXIT DATA &
-    !$OMP MAP( delete: Weights_q, Weights_X1, Weights_X2, Weights_X3, NodeNumberTable4D )
+    !$OMP MAP( release: Weights_q, Weights_X1, Weights_X2, Weights_X3, NodeNumberTable4D )
 #elif defined(THORNADO_OACC)
     !$ACC EXIT DATA &
-    !$ACC FINALIZE( Weights_q, Weights_X1, Weights_X2, Weights_X3, NodeNumberTable4D )
+    !$ACC DELETE( Weights_q, Weights_X1, Weights_X2, Weights_X3, NodeNumberTable4D )
 #endif
 
     DEALLOCATE( NodeNumbersX )
