79bfa8fe39544e2f6e22415e12628ae2d281204f
Eirik Endeve <endeve@users.noreply.github.com>
2019-05-15 15:21:25 -0400

    Skeleton TwoMoment_SlopeLimiterModule

---
+++ Modules/TwoMoment/TwoMoment_SlopeLimiterModule.f90
@@ -0,0 +1,31 @@
+MODULE TwoMoment_SlopeLimiterModule
+
+  USE KindModule, ONLY: &
+    DP
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: TwoMoment_InitializeSlopeLimiter
+  PUBLIC :: TwoMoment_FinalizeSlopeLimiter
+  PUBLIC :: TwoMoment_ApplySlopeLimiter
+
+CONTAINS
+
+
+  SUBROUTINE TwoMoment_InitializeSlopeLimiter
+
+  END SUBROUTINE TwoMoment_InitializeSlopeLimiter
+
+
+  SUBROUTINE TwoMoment_FinalizeSlopeLimiter
+
+  END SUBROUTINE TwoMoment_FinalizeSlopeLimiter
+
+
+  SUBROUTINE TwoMoment_ApplySlopeLimiter
+
+  END SUBROUTINE TwoMoment_ApplySlopeLimiter
+
+
+END MODULE TwoMoment_SlopeLimiterModule
