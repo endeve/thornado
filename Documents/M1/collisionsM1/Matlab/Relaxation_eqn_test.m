00fd314515a12978a6f9ad2eda9f37141dcc8102
Ming Tse Paul Laiu <plc@titanmath.ornl.gov>
2019-05-21 15:01:55 -0400

    matlab codes for fixed-point solvers and relaxation test

--- Documents/M1/collisionsM1/Matlab/Relaxation_eqn_test.m
+++ Documents/M1/collisionsM1/Matlab/Relaxation_eqn_test.m
@@ -1,60 +1,147 @@
-clear all
+clear all;
+% close all;
+plot_track = false;
+saveData = false;
+%%%%%%% 
+% start from Gaussian distributions on both nu_e and nu_e_bar
+% evolve the collision equation until equilibrium reached
+%%%%%%%
 
 readTestConstants();
 readTables();
 
-ReadMatterProfile;
-
-global g_E_N g_W2_N g_W3_N
+global g_E_N g_W2_N g_W3_N;
 % generate neutrino energy grid
 Numbins = 16;
 NumPts = 2;
 [g_E_N, g_W2_N, g_W3_N] = ComputePointsAndWeightsE(Numbins, NumPts);
 
-global BoltzmannConstant
 
+global BoltzmannConstant AtomicMassUnit PlanckConstant SpeedOfLight Erg2MeV;
+
+ReadMatterProfile;
 NumTests = size(MatterProfile,1);
 % NumTests = 10;
 
-Iters = zeros(NumTests,3);
-Diffs =  zeros(NumTests,3);
-DiffT = Diffs;
-DiffY = Diffs;
-DiffE = Diffs;
+hc = PlanckConstant * SpeedOfLight;
+c = SpeedOfLight;
+Theta2_N = 4 * pi * g_W2_N;
+Theta3_N = 4 * pi * g_W3_N;
 
-profile clear
+Theta2_N = Theta2_N / (hc)^3;
+Theta3_N = Theta3_N / (hc)^3;
+
+
+% profile clear
+% profile on
+
+% for density = [1e8 1e10 1e12]
+% for density = [1e8 1e10 1e12 1e14]
+
+for density = [1e8]
+    
+test_idx = find(MatterProfile(:,2)>=density,1);
+% test_idx = 1;
+% test_idx = round(NumTests/2);
+
+% test_idx = NumTests;
+% test_idx = NumTests-68;
+
+
+
+D0 = MatterProfile(test_idx,2);
+T0 = MatterProfile(test_idx,3);
+Y0 = MatterProfile(test_idx,4);
+
+% % original Gaussian initial condition
+% Gau_mean = 2.0 * BoltzmannConstant * T0;
+% % Gau_mean = 150;
+% Gau_std = 10;
+% Gau_dist = 1/Gau_std/2/sqrt(pi) * exp(-(g_E_N-Gau_mean).^2/2/Gau_std^2);
+% Gau_dist = Gau_dist./max(Gau_dist)*0.99;
+% % Gau_dist = Gau_dist./max(Gau_dist)*0.49;
+
+% % thin Gaussian
+Gau_mean = 10.0* BoltzmannConstant * T0;
+Gau_std = 1;
+Gau_dist = 1/Gau_std/2/sqrt(pi) * exp(-(g_E_N-Gau_mean).^2/2/Gau_std^2);
+Gau_dist = Gau_dist./max(Gau_dist)*0.99;
+
+
+
+% compute chemical potential and derivatives
+[Mnu, ~, ~] = ComputeNeutrinoChemicalPotentials(D0, T0, Y0);
+% equilibrium distribution
+[J00.Ne, J00.ANe] = FermiDirac( g_E_N, Mnu, BoltzmannConstant * T0 );
+
+
+% for dt = [1e-5 1e-6 1e-7]
+% for dt = [1e-5 1e-6]
+for dt = [1e-6]
+    
+D = D0; T = T0; Y = Y0;
+J.Ne = Gau_dist;
+J.ANe = Gau_dist;
+
+
+
+switch(dt)
+    case 1e-4
+        NumSteps = 20;
+    
+    case 1e-5
+        NumSteps = 2000;
+    case 1e-6
+        NumSteps = 20;
+    case 1e-7
+        NumSteps = 200000;
+end
+Iters = zeros(NumSteps,4);
+Iters_in = zeros(NumSteps,3);
+E_hist = zeros(NumSteps,2);
+N_hist = zeros(NumSteps,2);
+T_matter = zeros(NumSteps,1);
+Y_hist = zeros(NumSteps,1);
+Mnu_matter = zeros(NumSteps,1);
+Lepton = zeros(NumSteps,1);
+Energy = zeros(NumSteps,1);
+
+% dt is around 10^{-6} second
+% dt = 1e-7;
+t = 0;
+i = 0;
+t_final = dt * NumSteps;
+
+plot_idx = round(linspace(1,NumSteps,100));
+plot_counter =1;
+
 profile on
-for i = 1:NumTests
+while(i<NumSteps)
 
-    status_text = ['Test ', num2str(i), ' out of ', num2str(NumTests)];
+    
+    if ((plot_track) && (i==plot_idx(plot_counter)))
+        plot_counter = plot_counter + 1;
+        figure(1);
+        subplot(3,1,1)
+        plot(g_E_N, Gau_dist);
+        legend('Initial distribution')
+        title(['t = ', num2str(0, '%.1e'), ', D = ', num2str(D0, '%.3e'), ', T = ', num2str(T0, '%.3e'), ', Y = ', num2str(Y0, '%.3e')]);
+        subplot(3,1,2)
+        plot(g_E_N, J.Ne);
+        %hold on
+        legend('$J_{\nu_e}$', 'Interpreter', 'LaTex');
+        title(['t = ', num2str(t, '%.1e'), ', D = ', num2str(D, '%.3e'), ', T = ', num2str(T, '%.3e'), ', Y = ', num2str(Y, '%.3e')]);
+        subplot(3,1,3)
+        plot(g_E_N, J.ANe);
+        %hold on
+        legend('$\bar{J}_{\nu_e}$', 'Interpreter', 'LaTex');
+        title(['t = ', num2str(t, '%.1e'), ', D = ', num2str(D, '%.3e'), ', T = ', num2str(T, '%.3e'), ', Y = ', num2str(Y, '%.3e')]);
+        pause(0.1);
+    end
+    i = i + 1;
+   
+    status_text = ['Time_step = ', num2str(i), ' out of ', num2str(NumSteps)];
     disp(status_text);
-    
-    D = MatterProfile(i,2);
-    T = MatterProfile(i,3);
-    Y = MatterProfile(i,4);
-
-
-    % dt is around 10^{-6} second
-    dt = 1e-6;
-
-    % compute equilibrium distribution 
-    [Mnu, ~, ~] = ComputeNeutrinoChemicalPotentials(D, T, Y);
-    [J0.Ne, J0.ANe] = FermiDirac( g_E_N, Mnu, BoltzmannConstant * T );
-    
-    % initial condition (perturbed equilibrium distribution)
-%     J.Ne = J0.Ne - 1e-2*J0.Ne; 
-%     J.ANe = J0.ANe - 1e-2*J0.ANe; 
-
-    % initial condition (equilibrium distribution w/ perturbed (D,T,Y))
-    J = J0;
-    perturbation = 1e-1;
-    D = D - perturbation * D;
-    T = T - perturbation * T;
-    Y = Y - perturbation * Y;
-
-    % compute new equilibrium distribution 
-    [Mnu, ~, ~] = ComputeNeutrinoChemicalPotentials(D, T, Y);
-    [J0.Ne, J0.ANe] = FermiDirac( g_E_N, Mnu, BoltzmannConstant * T );
     
     % compute chi
     D_N = D * ones(size(g_E_N));
@@ -66,50 +153,113 @@ for i = 1:NumTests
     % compute internal energy
     [E, dEdT, dEdY] = ComputeSpecificInternalEnergy_TABLE(D, T, Y);
 
-    % Pair kernel test
-%     [R_Pr_Ne, R_An_Ne, R_Pr_ANe, R_An_ANe] = ComputePairOpacityOnEGrid_TABLE(g_E_N, D, T, Y);
-    
-    % Newton's method
-    % [J01, D1, T1, Y1, E1, iter1] = SolveMatterEquations_EmAb( J, dt * Chi, D, T, Y, E );
-    % [J00, D0, T0, Y0, E0, iter0] = SolveMatterEquations_EmAb_orig( J, dt * Chi, D, T, Y, E );
+    % solve collision equation
+    [~, ~, ~, ~, ~, ~, iter_TP] = SolveMatterEquations_Pair( J, dt, Chi, D, T, Y, E );
+    iter_in_TP = 0;
+    [~, ~, ~, ~, ~, ~, iter_TPPI, iter_in_PI] = SolveMatterEquations_Pair_Nested( J, dt, Chi, D, T, Y, E );
+    [J_TP, J0_TP, D_TP, T_TP, Y_TP, E_TP, iter_TPAA, iter_in_AA] = SolveMatterEquations_Pair_NestedAA( J, dt, Chi, D, T, Y, E );
+    [~, ~, ~, ~, ~, ~, iter_TPNewton, iter_in_Newton] = SolveMatterEquations_Pair_NestedNewton( J, dt, Chi, D, T, Y, E );
 
-    % Fixed-point
-    % [J02, D2, T2, Y2, E2, iter2, Inneriter2] = SolveMatterEquations_EmAb_FP( J, dt * Chi, D, T, Y, E );
-    % explicit NES
-    % [J02, D2, T2, Y2, E2, iter2, Inneriter2] = SolveMatterEquations_EmAb_NES_FP( J, dt, Chi, R_in_NES, R_out_NES, D, T, Y, E );
     
-    % nested FP
-%     [J01, D1, T1, Y1, E1, iter1, Inneriter1] = SolveMatterEquations_EmAb_NES_FP( J, dt, Chi, D, T, Y, E );
-%     [J01, D1, T1, Y1, E1, iter1AA, Inneriter1AA] = SolveMatterEquations_EmAb_NES_FP_AA( J, dt, Chi, D, T, Y, E );
-    Inneriter1 = 0;
-    Inneriter1AA = 0;
-    iter1 = 0;
-    iter1AA = 0;
+    % update variables
+    D = D_TP; T = T_TP; Y = Y_TP;
     
-    % coupled FP
-%     [J02, D2, T2, Y2, E2, iter2backup] = SolveMatterEquations_EmAb_NES_FP_coupled_backup( J, dt, Chi, D, T, Y, E );
-%     [J0_NES, D_NES, T_NES, Y_NES, E_NES, iter_NES] = SolveMatterEquations_EmAb_NES_FP_coupled( J, dt, Chi, D, T, Y, E );
+    J = J_TP;
 
-%     [J0_NES, D_NES, T_NES, Y_NES, E_NES, iter_NES] = SolveMatterEquations_EmAb_NES_FP_coupledAA_noAN( J, dt, Chi, D, T, Y, E );
-%     [J0_NESAN, D_NESAN, T_NESAN, Y_NESAN, E_NESAN, iter_NESAN] = SolveMatterEquations_EmAb_NES_FP_coupledAA( J, dt, Chi, D, T, Y, E );
-    [J0_TP, D_TP, T_TP, Y_TP, E_TP, iter_TP] = SolveMatterEquations_Pair( J, dt, Chi, D, T, Y, E );
+    J0 = J0_TP;
+    
 
-%     Iters(i,:) = [iter1 Inneriter1 iter1AA Inneriter1AA iter2backup iter2 iter2AA];
-    Iters(i,:) = [iter_TP];
-%     Iters(i,:) = [iter_NES iter_NESAN iter_TP];
-%     Diffs(i,1) = norm(([T_NES Y_NES E_NES]-[T_NESAN Y_NESAN E_NESAN])./[T_NES Y_NES E_NES]);
-%     Diffs(i,2) = norm(([T_NES Y_NES E_NES]-[T_TP Y_TP E_TP])./[T_NES Y_NES E_NES]);
-%     Diffs(i,3) = norm(([T_TP Y_TP E_TP]-[T_NESAN Y_NESAN E_NESAN])./[T_NES Y_NES E_NES]);
-%     DiffT(i,:) = abs([(T_NES - T_NESAN), (T_NES - T_TP), (T_NESAN - T_TP) ] ./ T_NES);
-%     DiffY(i,:) = abs([(Y_NES - Y_NESAN), (Y_NES - Y_TP), (Y_NESAN - Y_TP) ] ./ Y_NES);
-%     DiffE(i,:) = abs([(E_NES - E_NESAN), (E_NES - E_TP), (E_NESAN - E_TP) ] ./ E_NES);
+    % store data
+    N_B = D / AtomicMassUnit;
+
+    Lepton(i) = sum(Theta2_N.*(J.Ne - J.ANe)) + N_B * Y ; %scaled
+    Energy(i) = sum(Theta3_N.*(J.Ne + J.ANe)) + D * E_TP * Erg2MeV ; %scaled
+    N_hist(i,1) = sum(g_W2_N.*J.Ne);
+    E_hist(i,1) = sum(g_W3_N.*J.Ne);
+    N_hist(i,2) = sum(g_W2_N.*J.ANe);
+    E_hist(i,2) = sum(g_W3_N.*J.ANe);
+    T_matter(i) = T;
+    Y_hist(i) = Y;
+    [Mnu, ~, ~] = ComputeNeutrinoChemicalPotentials(D, T, Y);
+    Mnu_matter(i) = Mnu;
+    Iters(i,:) = [iter_TPPI iter_TPAA iter_TPNewton iter_TP];
+    Iters_in(i,:) = [iter_in_PI iter_in_AA iter_in_Newton];
+
+    
+    % evolve time
+    t = t + dt;
+    
     for k = 1:length(status_text)+1
         fprintf('\b');
     end
+    
 end
 
-finish_text = ['All ', num2str(NumTests), ' tests completed.'];
+finish_text = ['All ', num2str(NumSteps), ' steps completed.'];
 disp(finish_text);
 
 profile off
 profile viewer
+
+% figure;
+% subplot(3,1,1)
+% plot(g_E_N, Gau_dist);
+% legend('Initial distribution')
+% title(['t = ', num2str(0, '%.1e'), ', D = ', num2str(D0, '%.3e'), ', T = ', num2str(T0, '%.3e'), ', Y = ', num2str(Y0, '%.3e')]);
+% subplot(3,1,2)
+% plot(g_E_N, J.Ne);
+% legend('$J_{\nu_e}$', 'Interpreter', 'LaTex');
+% title(['t = ', num2str(t, '%.1e'), ', D = ', num2str(D, '%.3e'), ', T = ', num2str(T, '%.3e'), ', Y = ', num2str(Y, '%.3e')]);
+% subplot(3,1,3)
+% plot(g_E_N, J.ANe);
+% legend('$\bar{J}_{\nu_e}$', 'Interpreter', 'LaTex');
+% title(['t = ', num2str(t, '%.1e'), ', D = ', num2str(D, '%.3e'), ', T = ', num2str(T, '%.3e'), ', Y = ', num2str(Y, '%.3e')]);
+% 
+% 
+% 
+% figure;
+% subplot(4,1,1)
+% plot(g_E_N, J00.Ne);
+% legend('$J_{0,\nu_e}$', 'Interpreter', 'LaTex');
+% title(['t = ', num2str(0, '%.1e'), ', D = ', num2str(D0, '%.3e'), ', T = ', num2str(T0, '%.3e'), ', Y = ', num2str(Y0, '%.3e')]);
+% subplot(4,1,2)
+% plot(g_E_N, J00.ANe);
+% legend('$\bar{J}_{0,\nu_e}$', 'Interpreter', 'LaTex');
+% title(['t = ', num2str(0, '%.1e'), ', D = ', num2str(D0, '%.3e'), ', T = ', num2str(T0, '%.3e'), ', Y = ', num2str(Y0, '%.3e')]);
+% subplot(4,1,3)
+% plot(g_E_N, J0.Ne);
+% legend('$J_{0,\nu_e}$', 'Interpreter', 'LaTex');
+% title(['t = ', num2str(t, '%.1e'), ', D = ', num2str(D, '%.3e'), ', T = ', num2str(T, '%.3e'), ', Y = ', num2str(Y, '%.3e')]);
+% subplot(4,1,4)
+% plot(g_E_N, J0.ANe);
+% legend('$\bar{J}_{0,\nu_e}$', 'Interpreter', 'LaTex');
+% title(['t = ', num2str(t, '%.1e'), ', D = ', num2str(D, '%.3e'), ', T = ', num2str(T, '%.3e'), ', Y = ', num2str(Y, '%.3e')]);
+% 
+% figure;
+% subplot(1,2,1)
+% semilogx(Iters(:,1));
+% hold on
+% semilogx(Iters(:,2));
+% semilogx(Iters(:,3));
+% semilogx(Iters(:,4));
+% title(['Iteration count, ''Density = ', num2str(density,'%.0e')])
+% legend('PI','AA','Newton', 'Coupled')
+% % semilogx(ones(1,NumSteps),':');
+% 
+% subplot(1,2,2)
+% semilogx(Iters_in(:,1));
+% hold on
+% semilogx(Iters_in(:,2));
+% semilogx(Iters_in(:,3));
+% title(['[Inner iteration count, ''Density = ', num2str(density,'%.0e')])
+% legend('PI','AA','Newton')
+% % semilogx(ones(1,NumSteps),':');
+
+
+if(saveData)
+    save(['Data_' num2str(test_idx) '_dt_' num2str(dt, '%.0e') '_step_' num2str(NumSteps,'%.0e') '_thin_Gaussian.mat'],...
+        'D0', 'T0', 'Y0', 'Gau_dist', 'J00', 'D', 'T', 'Y', 'J', 'J0', 'Iters', 'Iters_in', 'g_E_N', ...
+        'E_hist', 'N_hist', 'T_matter', 'Y_hist', 'Mnu_matter', 'Lepton', 'Energy');
+end
+end
+end
--- Documents/M1/collisionsM1/Matlab/SolveMatterEquations_Pair_NestedNewton.m
+++ Documents/M1/collisionsM1/Matlab/SolveMatterEquations_Pair_NestedNewton.m
@@ -1,4 +1,4 @@
-function [J, J0, D, T, Y, E, iter] = SolveMatterEquations_Pair( Jin, dt, Chi, D, T, Y, E)
+function [J, J0, D, T, Y, E, iter, iter_in] = SolveMatterEquations_Pair_NestedNewton( Jin, dt, Chi, D, T, Y, E)
 
 % g_E_N       : nE_G x 1 (energy grid)
 % J, J_0    : nE_G x 1 (size of energy grid)
@@ -16,10 +16,11 @@ hc = PlanckConstant * SpeedOfLight;
 c = SpeedOfLight;
 
 Rtol = 1e-8; Utol = 1e-10; maxIter = 100;
+iter_in = 0;
 
 iY = 1;
 iE = 2;
-iJNe = iE + (1:length(Jin.Ne));
+iJNe = 1:length(Jin.Ne);
 iJANe = iJNe(end) + (1:length(Jin.ANe));
 
 U = zeros(2,1);
@@ -74,8 +75,10 @@ CONVERGED = false;
 
 % Anderson acceleration truncation parameter
 m = 5;
-Fvec = zeros(length(U)+length(J.Ne)+length(J.ANe),m);
-Jvec = zeros(length(U)+length(J.Ne)+length(J.ANe),m);
+FVEC = zeros(length(J.Ne)+length(J.ANe),1);
+FJAC = zeros(length(J.Ne)+length(J.ANe));
+Uvec = zeros(length(U),m);
+Fvec = zeros(length(U),m);
 alpha = zeros(m,1);
 
 
@@ -114,7 +117,7 @@ R_An.ANe = R_An.ANe * c * dt;
 
 eta_TP.Ne = R_Pr.Ne' * (W2_N .* (1 - J.ANe));
 Chi_TP.Ne = eta_TP.Ne + R_An.Ne' * (W2_N .* J.ANe);
-
+% 
 eta_TP.ANe = R_Pr.ANe' * (W2_N .* (1 - J.Ne));
 Chi_TP.ANe = eta_TP.ANe + R_An.ANe' * (W2_N .* J.Ne);
 
@@ -128,61 +131,107 @@ while((~CONVERGED)&&(k<=maxIter))
     k = k + 1;
     mk = min(m,k);
  
+    k_in = 0;
+    CONVERGED_IN = false;
+    J_in = J;
+    
+    % Solve inner loop
+    while((~CONVERGED_IN)&&(k_in<=maxIter))
+        
+        % NEWTON
+        
+                
+        k_in = k_in + 1;
+        
+        
+        FVEC(iJNe) = J.Ne - Jin.Ne + Chi.Ne.*(J.Ne - J0.Ne) ...
+            - ((1 - J.Ne).* (R_in_NES.Ne'*(W2_N .* J.Ne)) - J.Ne.*(R_out_NES.Ne'*(W2_N .* (1-J.Ne)))) ...
+            - ((1 - J.Ne).* (R_Pr.Ne'*(W2_N .* (1 - J.ANe))) - J.Ne.*(R_An.Ne'*(W2_N .* J.ANe)));
+        FVEC(iJANe) = J.ANe - Jin.ANe + Chi.ANe.*(J.ANe - J0.ANe) ...
+            - ((1 - J.ANe).* (R_in_NES.ANe'*(W2_N .* J.ANe)) - J.ANe.*(R_out_NES.ANe'*(W2_N .* (1-J.ANe)))) ...
+            - ((1 - J.ANe).* (R_Pr.ANe'*(W2_N .* (1 - J.Ne))) - J.ANe.*(R_An.ANe'*(W2_N .* J.Ne)));
+        
+        % diagonal terms
+        FJAC = diag(1 + [Chi.Ne;Chi.ANe]) ...
+            + diag([R_in_NES.Ne'*(W2_N .* J.Ne) + R_out_NES.Ne'*(W2_N .* (1-J.Ne)); ...
+                  R_in_NES.ANe'*(W2_N .* J.ANe) + R_out_NES.ANe'*(W2_N .* (1-J.ANe))]) ...
+            + diag([R_Pr.Ne'*(W2_N .* (1 - J.ANe)) + R_An.Ne'*(W2_N .* J.ANe); ...
+                  R_Pr.ANe'*(W2_N .* (1 - J.Ne)) + R_An.ANe'*(W2_N .* J.Ne)]);
+        % NES terms
+        FJAC(iJNe,iJNe) = FJAC(iJNe,iJNe) - (R_in_NES.Ne').*((1 - J.Ne)*(W2_N)') ...
+            - (R_out_NES.Ne').*(J.Ne*(W2_N)');
+        FJAC(iJANe,iJANe) = FJAC(iJANe,iJANe) - (R_in_NES.ANe').*((1 - J.ANe)*(W2_N)') ...
+            - (R_out_NES.ANe').*(J.ANe*(W2_N)');
+        
+        % Pair terms
+        FJAC(iJNe,iJANe) = FJAC(iJNe,iJANe) + (R_Pr.Ne').*((1 - J.Ne)*(W2_N)') ...
+            + (R_An.Ne').*(J.Ne*(W2_N)');
+        FJAC(iJANe,iJNe) = FJAC(iJANe,iJNe) + (R_Pr.ANe').*((1 - J.ANe)*(W2_N)') ...
+            + (R_An.Ne').*(J.ANe*(W2_N)');
+        
+        dJ = FJAC\FVEC;
+        
+        Jnew.Ne = J.Ne - dJ(iJNe);
+        Jnew.ANe = J.ANe - dJ(iJANe);
+        
+    
+        if ((norm(Jnew.Ne - J.Ne) <= Rtol * norm(J_in.Ne)) && (norm(Jnew.ANe - J.ANe) <= Rtol * norm(J_in.ANe)))
+            CONVERGED_IN = true;
+        end
+
+        J = Jnew;
+
+%         if (~CONVERGED_IN)
+% 
+%             eta_NES.Ne = R_in_NES.Ne' * (W2_N .* J.Ne);
+%             Chi_NES.Ne = eta_NES.Ne + R_out_NES.Ne' * (W2_N .* (1 - J.Ne));
+% 
+%             eta_NES.ANe = R_in_NES.ANe' * (W2_N .* J.ANe);
+%             Chi_NES.ANe = eta_NES.ANe + R_out_NES.ANe' * (W2_N .* (1 - J.ANe));
+% 
+%             eta_TP.Ne = R_Pr.Ne' * (W2_N .* (1 - J.ANe));
+%             Chi_TP.Ne = eta_TP.Ne + R_An.Ne' * (W2_N .* J.ANe);
+% 
+%             eta_TP.ANe = R_Pr.ANe' * (W2_N .* (1 - J.Ne));
+%             Chi_TP.ANe = eta_TP.ANe + R_An.ANe' * (W2_N .* J.Ne);
+%         end
+    end
+    if(k_in >= maxIter)
+        disp("Inner loop failed to converge within maxIter.");
+    end
+
+    iter_in = iter_in + k_in;
+    
     % Update (U,J)
-    Jvec(iY,mk) = 1 + C(iY) - Theta2_N' * (J.Ne - J.ANe)/ s_Y;
-    Jvec(iE,mk) = 1 + C(iE) - Theta3_N' * (J.Ne + J.ANe) / s_E;
+    Uvec(iY,mk) = 1 + C(iY) - Theta2_N' * (J.Ne - J.ANe)/ s_Y;
+    Uvec(iE,mk) = 1 + C(iE) - Theta3_N' * (J.Ne + J.ANe) / s_E;
     
-    eta_NES.Ne = R_in_NES.Ne' * (W2_N .* J.Ne);
-    Chi_NES.Ne = eta_NES.Ne + R_out_NES.Ne' * (W2_N .* (1 - J.Ne));
-    
-    eta_NES.ANe = R_in_NES.ANe' * (W2_N .* J.ANe);
-    Chi_NES.ANe = eta_NES.ANe + R_out_NES.ANe' * (W2_N .* (1 - J.ANe));
-
-    eta_TP.Ne = R_Pr.Ne' * (W2_N .* (1 - J.ANe));
-    Chi_TP.Ne = eta_TP.Ne + R_An.Ne' * (W2_N .* J.ANe);
-
-    eta_TP.ANe = R_Pr.ANe' * (W2_N .* (1 - J.Ne));
-    Chi_TP.ANe = eta_TP.ANe + R_An.ANe' * (W2_N .* J.Ne);
-
-    % update J for electron type neutrino and antineutrino
-    Jvec(iJNe,mk) = (Jin.Ne + Chi.Ne.*J0.Ne + eta_NES.Ne + eta_TP.Ne)./(1 + Chi.Ne + Chi_NES.Ne + Chi_TP.Ne);
-    Jvec(iJANe,mk) = (Jin.ANe + Chi.ANe.*J0.ANe + eta_NES.ANe + eta_TP.ANe)./(1 + Chi.ANe + Chi_NES.ANe + Chi_TP.ANe);
-    
-    Fvec(:,mk) = Jvec(:,mk) - [U; J.Ne; J.ANe];
+    Fvec(:,mk) = Uvec(:,mk) - U;
     
     if (mk == 1)
-        Unew = Jvec(1:2,1);
-        Jnew.Ne = Jvec(iJNe,1);
-        Jnew.ANe = Jvec(iJANe,1);
+        Unew = Uvec(:,1);
     else
         alpha(1:mk-1) = (Fvec(:,1:mk-1) - Fvec(:,mk)*ones(1,mk-1))\(-Fvec(:,mk));
         alpha(mk) = 1 - sum(alpha(1:mk-1));
-        temp = Jvec(:,1:mk) * alpha(1:mk);
-        Unew = temp(1:2);
-        Jnew.Ne = temp(iJNe);
-        Jnew.ANe = temp(iJANe);
+        Unew = Uvec(:,1:mk) * alpha(1:mk);
     end
     
     % check convergence
-%     if (norm([Unew; Jnew.Ne; Jnew.ANe] - [U; J.Ne; J.ANe]) <= Rtol * norm([U0; Jin.Ne; Jin.ANe]))
-    if ((norm(Unew(1) - U(1)) <= Rtol * norm(U0(1))) && (norm(Unew(2) - U(2)) <= Rtol * norm(U0(2)))...
-        && (norm(Jnew.Ne - J.Ne) <= Rtol * norm(Jin.Ne)) && (norm(Jnew.ANe - J.ANe) <= Rtol * norm(Jin.ANe)))
+    if ((norm(Unew(1) - U(1)) <= Rtol * norm(U0(1))) && (norm(Unew(2) - U(2)) <= Rtol * norm(U0(2))))
         CONVERGED = true;
     end
     
     if (mk == m)
-        Jvec = circshift(Jvec,-1,2);
+        Uvec = circshift(Uvec,-1,2);
         Fvec = circshift(Fvec,-1,2);
     end
     
-    % update U, Y, E, T, J
+    % update U, Y, E, T
     U = Unew;
     
     Y = U(iY) * Y0;
     E = U(iE) * E0 / Erg2MeV; % change unit    
     T = ComputeTemperatureFromSpecificInternalEnergy_TABLE(D, E, Y);
-    
-    J = Jnew;
     
     % compute chemical potential and derivatives
     [Mnu, ~, ~] = ComputeNeutrinoChemicalPotentials(D, T, Y);
@@ -220,7 +269,7 @@ end
 
 
 iter = k;
-
+iter_in = iter_in / iter;
 
 
 end
