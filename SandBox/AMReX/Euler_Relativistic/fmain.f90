9810b8555293c6119b6e39d0a469cc2063564d31
Samuel John Dunham <samueljdunham@gmail.com>
2019-06-09 13:59:50 -0400

    Added ability to write plot-files and checkpoint-files at time increments instead of cycle numbers

--- SandBox/AMReX/Euler_Relativistic/fmain.f90
+++ SandBox/AMReX/Euler_Relativistic/fmain.f90
@@ -116,6 +116,8 @@ PROGRAM main
 
   REAL(amrex_real) :: Timer_Evolution
   REAL(amrex_real) :: Mass
+  REAL(amrex_real) :: t_wrt, t_chk
+  LOGICAL          :: wrt, chk
 
 !!$  CALL MakeMF_Diff( 0, 2929 )
 
@@ -263,7 +265,11 @@ PROGRAM main
 
   ! --- Beginning of evolution ---
 
-  t = 0.0_amrex_real
+  t     = 0.0_amrex_real
+  t_wrt = dt_wrt
+  t_chk = dt_chk
+  wrt   = .FALSE.
+  chk   = .FALSE.
 
   IF( amrex_parallel_ioprocessor() )THEN
     WRITE(*,*)
@@ -313,7 +319,17 @@ PROGRAM main
            ( t, dt, MF_uGF, MF_uCF, &
              GEOM, MF_Euler_ComputeIncrement )
 
-    IF( MOD( StepNo(0), iCycleW ) .EQ. 0 )THEN
+    IF( iCycleW .GT. 0 )THEN
+      IF( MOD( StepNo(0), iCycleW ) .EQ. 0 ) &
+        wrt = .TRUE.
+    ELSE
+      IF( ALL( t + dt .GT. t_wrt ) )THEN
+        t_wrt = t_wrt + dt_wrt
+        wrt   = .TRUE.
+      END IF
+    END IF
+
+    IF( wrt )THEN
 
       CALL MF_ComputeFromConserved( MF_uGF, MF_uCF, MF_uPF, MF_uAF )
 
@@ -324,9 +340,21 @@ PROGRAM main
                MF_uPF_Option = MF_uPF, &
                MF_uAF_Option = MF_uAF )
 
+      wrt = .FALSE.
+
     END IF
 
-    IF( MOD( StepNo(0), iCycleChk ) .EQ. 0 )THEN
+    IF( iCycleChk .GT. 0 )THEN
+      IF( MOD( StepNo(0), iCycleChk ) .EQ. 0 ) &
+        chk = .TRUE.
+    ELSE
+      IF( ALL( t + dt .GT. t_chk ) )THEN
+        t_chk = t_chk + dt_chk
+        chk   = .TRUE.
+      END IF
+    END IF
+
+    IF( chk )THEN
 
       CALL MF_ComputeFromConserved( MF_uGF, MF_uCF, MF_uPF, MF_uAF )
 
@@ -337,6 +365,8 @@ PROGRAM main
                MF_uCF % P, &
                MF_uPF % P, &
                MF_uAF % P )
+
+      chk = .FALSE.
      
     END IF
 
