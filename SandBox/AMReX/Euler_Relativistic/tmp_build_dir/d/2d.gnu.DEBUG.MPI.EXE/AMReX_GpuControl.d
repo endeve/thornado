d2a2c2552b56877e7c38f0627faa92efa67c1a6c
Samuel John Dunham <samueljdunham@gmail.com>
2019-06-14 10:23:42 -0400

    Removed extra files

--- SandBox/AMReX/Euler_Relativistic/tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_GpuControl.d
+++
@@ -1,2 +0,0 @@
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_GpuControl.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_GpuControl.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX_GpuControl.H
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_GpuControl.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_GpuControl.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX_GpuControl.cpp
--- SandBox/AMReX/Euler_Relativistic/tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.d
+++
@@ -1,5 +0,0 @@
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX_GpuQualifiers.H
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX_error_fi.cpp
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX.H
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX_Exception.H
-tmp_build_dir/o/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.o tmp_build_dir/d/2d.gnu.DEBUG.MPI.EXE/AMReX_error_fi.d: /Users/dunhamsj/Research/AMReX/amrex/Src/Base/AMReX_ccse-mpi.H
