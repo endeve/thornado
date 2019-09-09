5645535b4f91849c559cb8bcfac415b4f70bf2a9
Eirik Endeve <endeve@users.noreply.github.com>
2019-09-09 10:59:32 -0400

    Skeleton modules to experiment with order v/c transport discretization

---
+++ SandBox/TwoMoment_OrderV/TwoMoment_DiscretizationModule_Collisions_OrderV.F90
@@ -0,0 +1,21 @@
+MODULE TwoMoment_DiscretizationModule_Collisions_OrderV
+
+  USE KindModule, ONLY: &
+    DP
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: ComputeIncrement_TwoMoment_Implicit
+
+CONTAINS
+
+
+  SUBROUTINE ComputeIncrement_TwoMoment_Implicit
+
+    PRINT*, "ComputeIncrement_TwoMoment_Implicit"
+
+  END SUBROUTINE ComputeIncrement_TwoMoment_Implicit
+
+
+END MODULE TwoMoment_DiscretizationModule_Collisions_OrderV
---
+++ SandBox/TwoMoment_OrderV/TwoMoment_DiscretizationModule_Streaming_OrderV.F90
@@ -0,0 +1,21 @@
+MODULE TwoMoment_DiscretizationModule_Streaming_OrderV
+
+  USE KindModule, ONLY: &
+    DP
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: ComputeIncrement_TwoMoment_Explicit
+
+CONTAINS
+
+
+  SUBROUTINE ComputeIncrement_TwoMoment_Explicit
+
+    PRINT*, "ComputeIncrement_TwoMoment_Explicit"
+
+  END SUBROUTINE ComputeIncrement_TwoMoment_Explicit
+
+
+END MODULE TwoMoment_DiscretizationModule_Streaming_OrderV
