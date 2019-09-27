a5f379c996c25809b9dccf178a8f42e4844402b6
DavidPochik <dpochik@vols.utk.edu>
2019-09-27 18:53:22 -0400

    Continuing to merge nuclearEOS into master

--- SandBox/dgExperiments_Euler_NuclearEOS/ApplicationDriver.f90
+++ SandBox/dgExperiments_Euler_NuclearEOS/ApplicationDriver.f90
@@ -25,6 +25,8 @@ PROGRAM ApplicationDriver
   USE EquationOfStateModule, ONLY: &
     InitializeEquationOfState, &
     FinalizeEquationOfState
+  USE EquationOfStateModule_TABLE, ONLY: &
+    MinD, MaxD, MinT, MaxT, MinY, MaxY
   USE FluidFieldsModule, ONLY: &
     uCF, uPF, uAF
   USE InitializationModule, ONLY: &
@@ -33,14 +35,14 @@ PROGRAM ApplicationDriver
     InitializeFluid_SSPRK, &
     FinalizeFluid_SSPRK, &
     UpdateFluid_SSPRK
-  USE Euler_SlopeLimiterModule_NonRelativistic, ONLY: &
-    Euler_InitializeSlopeLimiter_NonRelativistic, &
-    Euler_FinalizeSlopeLimiter_NonRelativistic, &
-    Euler_ApplySlopeLimiter_NonRelativistic
-  USE Euler_PositivityLimiterModule_NonRelativistic, ONLY: &
-    Euler_InitializePositivityLimiter_NonRelativistic, &
-    Euler_FinalizePositivityLimiter_NonRelativistic, &
-    Euler_ApplyPositivityLimiter_NonRelativistic
+  USE Euler_SlopeLimiterModule_NonRelativistic_TABLE, ONLY: &
+    Euler_InitializeSlopeLimiter_NonRelativistic_TABLE, &
+    Euler_FinalizeSlopeLimiter_NonRelativistic_TABLE, &
+    Euler_ApplySlopeLimiter_NonRelativistic_TABLE
+  USE Euler_PositivityLimiterModule_NonRelativistic_TABLE, ONLY: &
+    Euler_InitializePositivityLimiter_NonRelativistic_TABLE, &
+    Euler_FinalizePositivityLimiter_NonRelativistic_TABLE, &
+    Euler_ApplyPositivityLimiter_NonRelativistic_TABLE
   USE Euler_UtilitiesModule_NonRelativistic, ONLY: &
     Euler_ComputeFromConserved_NonRelativistic, &
     Euler_ComputeTimeStep_NonRelativistic
@@ -64,6 +66,7 @@ PROGRAM ApplicationDriver
   LOGICAL       :: UseSlopeLimiter
   LOGICAL       :: UseCharacteristicLimiting
   LOGICAL       :: UseTroubledCellIndicator
+  LOGICAL       :: UsePositivityLimiter
   INTEGER       :: iCycle, iCycleD
   INTEGER       :: nX(3), bcX(3), nNodes
   REAL(DP)      :: t, dt, t_end, dt_wrt, t_wrt, wTime
@@ -83,10 +86,34 @@ PROGRAM ApplicationDriver
       RiemannProblemName = 'Sod'
 
       nX = [ 100, 1, 1 ]
-      xL = [ - 5.0_DP, 0.0_DP, 0.0_DP ] * Kilometer
-      xR = [ + 5.0_DP, 1.0_DP, 1.0_DP ] * Kilometer
+      xL = [ - 5.0_DP,   0.0_DP, 0.0_DP ] * Kilometer
+      xR = [ + 5.0_DP, + 1.0_DP, 1.0_DP ] * Kilometer
 
       bcX = [ 2, 0, 0 ]
+
+      nNodes = 3
+
+      BetaTVD = 1.75_DP
+      BetaTVB = 0.0d+00
+
+      UseSlopeLimiter           = .TRUE.
+      UseCharacteristicLimiting = .FALSE.
+
+      UseTroubledCellIndicator  = .FALSE.
+      LimiterThresholdParameter = 1.0d-1
+      UsePositivityLimiter      = .TRUE.
+
+      iCycleD = 10
+      t_end   = 7.5d-2 * Millisecond
+      dt_wrt  = 7.5d-5 * Millisecond
+
+    CASE( 'Jet' )
+
+      nX = [ 100, 100, 1 ]
+      xL = [ 0.0_DP, 0.0_DP, 0.0_DP ] * Kilometer
+      xR = [ 1.0_DP, 1.0_DP, 1.0_DP ] * Kilometer
+
+      bcX = [ 2, 2, 0 ]
 
       nNodes = 3
 
@@ -98,33 +125,37 @@ PROGRAM ApplicationDriver
 
       UseTroubledCellIndicator  = .FALSE.
       LimiterThresholdParameter = 1.5d-0
+      UsePositivityLimiter      = .TRUE.
 
       iCycleD = 10
-      t_end   = 2.5d-2 * Millisecond
-      dt_wrt  = 2.5d-4 * Millisecond
+      t_end   = 2.5d-1 * Millisecond
+      dt_wrt  = 2.5d-6 * Millisecond !d-6
 
-    CASE( 'Noh' )
+    CASE( 'Implosion' )
 
-      nX = [ 100, 1, 1 ]
-      xL = [ - 5.0_DP, 0.0_DP, 0.0_DP ] * Kilometer
-      xR = [ + 5.0_DP, 1.0_DP, 1.0_DP ] * Kilometer
+!      Gamma = 1.4_DP
 
-      bcX = [ 2, 0, 0 ]
+      nX = [ 64, 64, 1 ]
+      xL = [ 0.0_DP, 0.0_DP, 0.0_DP ] * Kilometer
+      xR = [ 0.3_DP, 0.3_DP, 1.0_DP ] * Kilometer
 
-      nNodes = 1
+      bcX = [ 3, 3, 0 ]
 
-      BetaTVD = 1.75_DP
+      nNodes = 3
+
+      BetaTVD = 1.50_DP
       BetaTVB = 0.0d+00
 
       UseSlopeLimiter           = .TRUE.
-      UseCharacteristicLimiting = .FALSE.
+      UseCharacteristicLimiting = .TRUE.
+      UsePositivityLimiter      = .TRUE.
 
-      UseTroubledCellIndicator  = .FALSE.
-      LimiterThresholdParameter = 1.5d-0
+      UseTroubledCellIndicator  = .TRUE.
+      LimiterThresholdParameter = 0.30_DP
 
-      iCycleD = 10
-      t_end   = 2.5d-2 * Millisecond
-      dt_wrt  = 2.5d-4 * Millisecond
+      iCycleD = 1
+      t_end   = 2.500_DP * Millisecond
+      dt_wrt  = 0.045_DP * Millisecond
 
   END SELECT
 
@@ -158,9 +189,9 @@ PROGRAM ApplicationDriver
 
   CALL InitializeEquationOfState &
          ( EquationOfState_Option = 'TABLE', &
-           EquationOfStateTableName_Option = 'EquationOfStateTable.h5' )
+           EquationOfStateTableName_Option = 'wl-EOS-DD2-25-50-100.h5' ) !wl-EOS-DD2-25-50-100.h5 wl-EOS-SFHo-25-50-100.h5
 
-  CALL Euler_InitializeSlopeLimiter_NonRelativistic &
+  CALL Euler_InitializeSlopeLimiter_NonRelativistic_TABLE &
          ( BetaTVD_Option = BetaTVD, &
            BetaTVB_Option = BetaTVB, &
            SlopeTolerance_Option &
@@ -174,10 +205,15 @@ PROGRAM ApplicationDriver
            LimiterThresholdParameter_Option &
              = LimiterThresholdParameter )
 
-  CALL Euler_InitializePositivityLimiter_NonRelativistic &
-         ( Min_1_Option = 1.0d-12, &
-           Min_2_Option = 1.0d-12, &
-           UsePositivityLimiter_Option = .FALSE. )
+  CALL Euler_InitializePositivityLimiter_NonRelativistic_TABLE &
+         ( Min_D_Option = ( One + EPSILON(One) ) * MinD, &
+           Max_D_Option = ( One - EPSILON(One) ) * MaxD, &
+           Min_T_Option = ( One + EPSILON(One) ) * MinT, &
+           Max_T_Option = ( One - EPSILON(One) ) * MaxT, &
+           Min_Y_Option = ( One + EPSILON(One) ) * MinY, &
+           Max_Y_Option = ( One - EPSILON(One) ) * MaxY, &
+           UsePositivityLimiter_Option &
+             = UsePositivityLimiter )
 
   CALL InitializeFields &
          ( AdvectionProfile_Option &
@@ -185,16 +221,14 @@ PROGRAM ApplicationDriver
            Direction_Option &
              = TRIM( Direction ), &
            RiemannProblemName_Option &
-             = TRIM( RiemannProblemName ), &
-           SedovEnergy_Option = Eblast )
+             = TRIM( RiemannProblemName ) )
 
-  CALL Euler_ApplySlopeLimiter_NonRelativistic &
+  CALL Euler_ApplySlopeLimiter_NonRelativistic_TABLE &
          ( iX_B0, iX_E0, iX_B1, iX_E1, &
            uGF(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:), &
-           uCF(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:), &
-           SuppressBC = .FALSE. )
+           uCF(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:) )
 
-  CALL Euler_ApplyPositivityLimiter_NonRelativistic &
+  CALL Euler_ApplyPositivityLimiter_NonRelativistic_TABLE &
          ( iX_B0, iX_E0, iX_B1, iX_E1, &
            uGF(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:), &
            uCF(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:) )
@@ -216,7 +250,7 @@ PROGRAM ApplicationDriver
   wTime = MPI_WTIME( )
 
   t     = 0.0_DP * Millisecond
-  t_wrt = dt_wrt !* Millisecond
+  t_wrt = dt_wrt
   wrt   = .TRUE.
 
   CALL Euler_InitializeTally_NonRelativistic &
@@ -229,7 +263,7 @@ PROGRAM ApplicationDriver
 
     iCycle = iCycle + 1
 
-    CALL Euler_ComputeTimeStep_NonRelativistic &
+    CALL Euler_ComputeTimeStep_NonRelativistic&
            ( iX_B0, iX_E0, iX_B1, iX_E1, &
              uGF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
              uCF(:,iX_B0(1):iX_E0(1),iX_B0(2):iX_E0(2),iX_B0(3):iX_E0(3),:), &
@@ -252,7 +286,7 @@ PROGRAM ApplicationDriver
     IF( MOD( iCycle, iCycleD ) == 0 )THEN
 
       WRITE(*,'(A8,A8,I8.8,A2,A4,ES13.6E3,A1,A5,ES13.6E3)') &
-          '', 'Cycle = ', iCycle, '', 't = ',  t / Millisecond, '', 'dt = ', dt
+          '', 'Cycle = ', iCycle, '', 't = ',  t / Millisecond, '', 'dt = ', dt / Millisecond
 
     END IF
 
@@ -310,9 +344,9 @@ PROGRAM ApplicationDriver
     '', 'Finished ', iCycle, ' Cycles in ', wTime, ' s'
   WRITE(*,*)
 
-  CALL Euler_FinalizePositivityLimiter_NonRelativistic
+  CALL Euler_FinalizePositivityLimiter_NonRelativistic_TABLE
 
-  CALL Euler_FinalizeSlopeLimiter_NonRelativistic
+  CALL Euler_FinalizeSlopeLimiter_NonRelativistic_TABLE
 
   CALL FinalizeEquationOfState
 
