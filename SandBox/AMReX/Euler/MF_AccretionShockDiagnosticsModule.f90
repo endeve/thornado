5747a76d3548c9477506966c4cbc16a7fd8af757
Samuel John Dunham <9784969+samueljdunham@users.noreply.github.com>
2020-05-01 19:04:02 -0400

    Working on adding SAS diagnostics to AMReX

---
+++ SandBox/AMReX/Euler/MF_AccretionShockDiagnosticsModule.f90
@@ -0,0 +1,127 @@
+MODULE MF_AccretionShockDiagnosticsModule
+
+  ! --- AMReX Modules ---
+
+  USE amrex_fort_module,     ONLY: &
+    AR => amrex_real
+  USE amrex_box_module,      ONLY: &
+    amrex_box
+  USE amrex_multifab_module, ONLY: &
+    amrex_multifab,     &
+    amrex_mfiter,       &
+    amrex_mfiter_build, &
+    amrex_mfiter_destroy
+  USE amrex_parallel_module, ONLY: &
+    amrex_parallel_reduce_sum
+
+  ! --- thornado Modules ---
+
+  USE ProgramHeaderModule,           ONLY: &
+    swX, &
+    nDOFX
+  USE FluidFieldsModule,             ONLY: &
+    nPF, &
+    nAF
+  USE AccretionShockDiagnosticsModule, ONLY: &
+    ComputeAccretionShockDiagnostics
+
+  ! --- Local Modules ---
+
+  USE MF_UtilitiesModule,       ONLY: &
+    AMReX2thornado, &
+    thornado2AMReX
+  USE MyAmrModule,              ONLY: &
+    nLevels, &
+    DEBUG
+  USE TimersModule_AMReX_Euler, ONLY: &
+    TimersStart_AMReX_Euler, &
+    TimersStop_AMReX_Euler,  &
+    Timer_AMReX_Euler_DataTransfer
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: MF_ComputeAccretionShockDiagnostics
+
+
+CONTAINS
+
+
+  SUBROUTINE MF_ComputeAccretionShockDiagnostics( MF_uPF, MF_uAF )
+
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uPF(0:nLevels-1)
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uAF(0:nLevels-1)
+
+    TYPE(amrex_mfiter) :: MFI
+    TYPE(amrex_box)    :: BX
+
+    REAL(AR), CONTIGUOUS, POINTER :: uPF(:,:,:,:)
+    REAL(AR), CONTIGUOUS, POINTER :: uAF(:,:,:,:)
+
+    REAL(AR), ALLOCATABLE :: P(:,:,:,:,:)
+    REAL(AR), ALLOCATABLE :: A(:,:,:,:,:)
+
+    INTEGER :: iLevel, iX_B0(3), iX_E0(3), iX_B1(3), iX_E1(3)
+
+    INTEGER, PARAMETER :: nLegModes = 3
+    INTEGER            :: iLegMode
+    REAL(AR)           :: Power_Legendre(0:nLevels-1,0:nLegModes-1)
+
+    Power_Legendre = 0.0_AR
+
+    DO iLevel = 0, nLevels-1
+
+      CALL amrex_mfiter_build( MFI, MF_uPF(iLevel), tiling = .TRUE. )
+
+      DO WHILE( MFI % next() )
+
+        uPF => MF_uPF(iLevel) % DataPtr( MFI )
+        uAF => MF_uAF(iLevel) % DataPtr( MFI )
+
+        BX = MFI % tilebox()
+
+        iX_B0 = BX % lo
+        iX_E0 = BX % hi
+        iX_B1 = BX % lo - swX
+        iX_E1 = BX % hi + swX
+
+        CALL TimersStart_AMReX_Euler( Timer_AMReX_Euler_DataTransfer )
+
+        ALLOCATE( P(1:nDOFX,iX_B1(1):iX_E1(1), &
+                            iX_B1(2):iX_E1(2), &
+                            iX_B1(3):iX_E1(3),1:nPF ) )
+
+        ALLOCATE( A(1:nDOFX,iX_B1(1):iX_E1(1), &
+                            iX_B1(2):iX_E1(2), &
+                            iX_B1(3):iX_E1(3),1:nAF ) )
+
+        CALL AMReX2thornado( nPF, iX_B1, iX_E1, uPF, P )
+
+        CALL AMReX2thornado( nAF, iX_B1, iX_E1, uAF, A )
+
+        CALL TimersStop_AMReX_Euler( Timer_AMReX_Euler_DataTransfer )
+
+        CALL ComputeAccretionShockDiagnostics &
+               ( iX_B0, iX_E0, iX_B1, iX_E1, P, A, &
+                 Power_Legendre(iLevel,0:nLegModes-1) )
+
+        DEALLOCATE( A )
+
+        DEALLOCATE( P )
+
+      END DO
+
+      CALL amrex_mfiter_destroy( MFI )
+
+    END DO
+
+    DO iLegMode = 0, nLegModes-1
+
+      CALL amrex_parallel_reduce_sum( Power_Legendre(:,iLegMode), nLevels )
+
+    END DO
+
+  END SUBROUTINE MF_ComputeAccretionShockDiagnostics
+
+
+END MODULE MF_AccretionShockDiagnosticsModule
