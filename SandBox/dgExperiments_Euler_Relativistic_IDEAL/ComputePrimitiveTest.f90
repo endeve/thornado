11ff9d155968f575ab8a045ba34053e354a753f3
Samuel John Dunham <9784969+samueljdunham@users.noreply.github.com>
2020-05-10 19:37:57 -0400

    Relativistic hydro: Modifications to ComputePrimitiveTest

--- SandBox/dgExperiments_Euler_Relativistic_IDEAL/ComputePrimitiveTest.f90
+++ SandBox/dgExperiments_Euler_Relativistic_IDEAL/ComputePrimitiveTest.f90
@@ -41,8 +41,7 @@ PROGRAM ComputePrimitiveTest
     ComputePressureFromPrimitive
   USE Euler_UtilitiesModule_Relativistic, ONLY: &
     ComputeConserved_Euler_Relativistic, &
-    ComputePrimitive_Euler_Relativistic, &
-    ComputePrimitive_Euler_Relativistic_TABLE_Scalar
+    ComputePrimitive_Euler_Relativistic
 
   IMPLICIT NONE
 
@@ -72,7 +71,7 @@ PROGRAM ComputePrimitiveTest
            Gamma_IDEAL_Option = Gamma_IDEAL )
 
   X1 = 5.0e2_DP
-  X2 = Pi / Four
+  X2 = 23.0e0_DP * Pi / 1.80e2_DP
 
   G(iGF_Gm_dd_11) = One * UnitsGm11
   G(iGF_Gm_dd_22) = X1**2 * UnitsGm22
@@ -81,18 +80,14 @@ PROGRAM ComputePrimitiveTest
   Pressure(0) = 1.0e33_DP * UnitsP
   P(0,iPF_D ) = 1.0e14_DP * UnitsD
   P(0,iPF_V1) = -1.0e5     * UnitsV1
-  P(0,iPF_V2) = 3.0e2_DP  * UnitsV2
+  P(0,iPF_V2) = -3.0e2_DP  * UnitsV2
   P(0,iPF_V3) = 3.0e2_DP  * UnitsV3
   P(0,iPF_E ) = Pressure(0) / ( Gamma_IDEAL - One )
   P(0,iPF_Ne) = Zero      * UnitsNe
 
   WRITE(*,*)
-  WRITE(*,'(A,ES23.16E3,A)') &
-    '|V| = ', SQRT( G(iGF_Gm_dd_11) * P(0,iPF_V1)**2 &
-                      + G(iGF_Gm_dd_22) * P(0,iPF_V2)**2 &
-                      + G(iGF_Gm_dd_33) * P(0,iPF_V3)**2 ) / UnitsV1, &
-    ' km/s'
-
+  WRITE(*,'(A,ES10.3E3,A)') 'X1 = ', X1, ' km'
+  WRITE(*,'(A,ES10.3E3,A)') 'X2 = ', X2 * 1.80e2_DP / Pi, ' deg'
   WRITE(*,*)
   WRITE(*,'(A,ES23.16E3)') 'GF_Gm11 = ', G(iGF_Gm_dd_11) / UnitsGm11
   WRITE(*,'(A,ES23.16E3)') 'GF_Gm22 = ', G(iGF_Gm_dd_22) / UnitsGm22
@@ -126,6 +121,13 @@ PROGRAM ComputePrimitiveTest
   WRITE(*,'(A,ES24.16E3)') 'PF_E     = ', P(0,iPF_E ) / UnitsE
   WRITE(*,'(A,ES24.16E3)') 'PF_Ne    = ', P(0,iPF_Ne) / UnitsNe
   WRITE(*,*)
+  WRITE(*,'(A,ES23.16E3,A)') &
+    '|V| = ', SQRT( G(iGF_Gm_dd_11) * P(0,iPF_V1)**2 &
+                      + G(iGF_Gm_dd_22) * P(0,iPF_V2)**2 &
+                      + G(iGF_Gm_dd_33) * P(0,iPF_V3)**2 ) / UnitsV1, &
+    ' km/s'
+
+  WRITE(*,*)
 
   Time = MPI_WTIME()
 
@@ -155,7 +157,7 @@ PROGRAM ComputePrimitiveTest
 
   Time = MPI_WTIME() - Time
 
-  WRITE(*,'(A)') 'Outputs (old):'
+  WRITE(*,'(A)') 'Outputs:'
   WRITE(*,'(A,ES24.16E3)') &
     'dP/P   = ', ( Pressure(1) - Pressure(0) ) / Pressure(0)
   WRITE(*,'(A,ES24.16E3)') &
@@ -171,54 +173,7 @@ PROGRAM ComputePrimitiveTest
   WRITE(*,'(A,ES24.16E3)') &
     'dNe/Ne = ', ( P(1,iPF_Ne) - P(0,iPF_Ne) ) / MAX( P(0,iPF_Ne), SqrtTiny )
   WRITE(*,*)
-  WRITE(*,'(A,ES24.16E3,A)') 'Time (old): ', Time, ' s'
-  WRITE(*,*)
-
-  Time = MPI_WTIME()
-
-  DO ITER = 1, MaxIter
-
-    CALL ComputePrimitive_Euler_Relativistic_TABLE_Scalar &
-           ( U(  iCF_D ), &
-             U(  iCF_S1), &
-             U(  iCF_S2), &
-             U(  iCF_S3), &
-             U(  iCF_E ), &
-             U(  iCF_Ne), &
-             G(  iGF_Gm_dd_11), &
-             G(  iGF_Gm_dd_22), &
-             G(  iGF_Gm_dd_33), &
-             P(1,iPF_D ), &
-             P(1,iPF_V1), &
-             P(1,iPF_V2), &
-             P(1,iPF_V3), &
-             P(1,iPF_E ), &
-             P(1,iPF_Ne) )
-
-    CALL ComputePressureFromPrimitive &
-           ( P(1,iPF_D), P(1,iPF_E), P(1,iPF_Ne), Pressure(1) )
-
-  END DO
-
-  Time = MPI_WTIME() - Time
-
-  WRITE(*,'(A)') 'Outputs (new):'
-  WRITE(*,'(A,ES24.16E3)') &
-    'dP/P   = ', ( Pressure(1) - Pressure(0) ) / Pressure(0)
-  WRITE(*,'(A,ES24.16E3)') &
-    'dD/D   = ', ( P(1,iPF_D ) - P(0,iPF_D ) ) / P(0,iPF_D )
-  WRITE(*,'(A,ES24.16E3)') &
-    'dV1/V1 = ', ( P(1,iPF_V1) - P(0,iPF_V1) ) / P(0,iPF_V1)
-  WRITE(*,'(A,ES24.16E3)') &
-    'dV2/V2 = ', ( P(1,iPF_V2) - P(0,iPF_V2) ) / P(0,iPF_V2)
-  WRITE(*,'(A,ES24.16E3)') &
-    'dV3/V3 = ', ( P(1,iPF_V3) - P(0,iPF_V3) ) / P(0,iPF_V3)
-  WRITE(*,'(A,ES24.16E3)') &
-    'dE/E   = ', ( P(1,iPF_E ) - P(0,iPF_E ) ) / P(0,iPF_E )
-  WRITE(*,'(A,ES24.16E3)') &
-    'dNe/Ne = ', ( P(1,iPF_Ne) - P(0,iPF_Ne) ) / MAX( P(0,iPF_Ne), SqrtTiny )
-  WRITE(*,*)
-  WRITE(*,'(A,ES24.16E3,A)') 'Time (new): ', Time, ' s'
+  WRITE(*,'(A,ES24.16E3,A)') 'Time per iteration: ', Time / MaxIter, ' s'
   WRITE(*,*)
 
   CALL FinalizeEquationOfState
