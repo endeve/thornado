07912e4f3f38b7490c2d6287b20adaf8b1a35efb
Zack <zelledge@vols.utk.edu>
2020-08-17 14:17:58 -0400

    Added Lagrane Decomposition

--- SandBox/AMReX/TwoMoment/fmain.f90
+++ SandBox/AMReX/TwoMoment/fmain.f90
@@ -11,6 +11,8 @@ PROGRAM main
   USE MF_TwoMoment_UtilitiesModule,     ONLY: & 
     MF_ComputeTimeStep,                &
     MF_ComputeFromConserved
+  USE MF_UtilitiesModule,     ONLY: & 
+    WriteNodalDataToFile
   USE MyAmrDataModule,                  ONLY: &
     MF_uCR, &
     MF_uPR, &
@@ -40,6 +42,8 @@ PROGRAM main
     iCycleChk, &
     BA,        &
     GEOM
+  USE ProgramHeaderModule,  ONLY: &
+    nDOFZ
   USE MF_TwoMoment_TimeSteppingModule_Relativistic,      ONLY: &
     MF_Update_IMEX_RK
 
@@ -97,6 +101,12 @@ PROGRAM main
 !
   END DO
  
+  IF (nDOFZ .GT. 1) THEN
+
+    CALL WriteNodalDataToFile( GEOM, MF_uGF, MF_uCF, MF_uCR, 'thornado_')
+
+  END IF
+
   CALL MF_ComputeFromConserved( MF_uGF, MF_uCF, MF_uCR, MF_uPR )
 
   CALL WriteFieldsAMReX_Checkpoint & 
@@ -108,6 +118,7 @@ PROGRAM main
            ( t(0), StepNo, &
              MF_uCR_Option = MF_uCR, &
              MF_uPR_Option = MF_uPR )
+
 
   CALL FinalizeProgram( GEOM )
   
