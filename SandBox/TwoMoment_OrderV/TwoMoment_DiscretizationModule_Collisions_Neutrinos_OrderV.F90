8f59fcb81aea9ba00cb84c41fdc4db877a3d6770
Eirik Endeve <endeve@users.noreply.github.com>
2020-11-18 09:39:34 -0500

    Skeleton module for neutrino-matter coupling

---
+++ SandBox/TwoMoment_OrderV/TwoMoment_DiscretizationModule_Collisions_Neutrinos_OrderV.F90
@@ -0,0 +1,19 @@
+MODULE TwoMoment_DiscretizationModule_Collisions_Neutrinos_OrderV
+
+  USE KindModule, ONLY: &
+    DP
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: ComputeIncrement_TwoMoment_Implicit
+
+CONTAINS
+
+
+  SUBROUTINE ComputeIncrement_TwoMoment_Implicit
+
+  END SUBROUTINE ComputeIncrement_TwoMoment_Implicit
+
+
+END MODULE TwoMoment_DiscretizationModule_Collisions_Neutrinos_OrderV
