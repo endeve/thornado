bc4a2b2ad9b22bc1e5d22b186aaced2af8872e62
ranchu1 <rchu@vols.utk.edu>
2021-02-03 21:11:17 -0500

    Added UpdateSubcellReconstruction routine

--- Modules/Library/SubcellReconstructionModule.f90
+++ Modules/Library/SubcellReconstructionModule.f90
@@ -3,18 +3,23 @@ MODULE SubcellReconstructionModule
   USE KindModule, ONLY: &
     DP, Zero, Half, One
   USE ProgramHeaderModule, ONLY: &
-    nNodesX, nDOFX
+    nNodesX, nDOFX, iX_B0, iX_E0
   USE ReferenceElementModuleX, ONLY: &
     NodesX1, WeightsX1, &
     NodesX2, WeightsX2, &
     NodesX3, WeightsX3
   USE PolynomialBasisModuleX_Lagrange, ONLY: &
     IndLX_Q, L_X1, L_X2, L_X3
+  USE GeometryFieldsModule, ONLY: &
+    CoordinateSystem
+  USE MeshModule, ONLY: &
+    MeshX
 
   IMPLICIT NONE
   PRIVATE
 
   PUBLIC :: InitializeSubcellReconstruction
+  PUBLIC :: UpdateSubcellReconstruction
   PUBLIC :: FinalizeSubcellReconstruction
 
   REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix(:,:)
@@ -35,6 +40,9 @@ CONTAINS
     REAL(DP) :: Eta_X1(nNodesX(1))
     REAL(DP) :: Eta_X2(nNodesX(2))
     REAL(DP) :: Eta_X3(nNodesX(3))
+
+    INTEGER  :: iX1
+    REAL(DP) :: r_l, r_r, r_q1, r_q2
 
     ALLOCATE( ProjectionMatrix(nDOFX,nDOFX) )
 
@@ -91,6 +99,66 @@ CONTAINS
     END DO
     END DO
 
+!!$ <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    IF( TRIM( CoordinateSystem ) .eq. 'SPHERICAL' )THEN
+
+    iS = 0
+    DO iS3 = 1, nNodesX(3)
+    DO iS2 = 1, nNodesX(2)
+    DO iS1 = 1, nNodesX(1)
+
+      iS = iS + 1
+
+      jS = 0
+      DO jS3 = 1, nNodesX(3)
+      DO jS2 = 1, nNodesX(2)
+      DO jS1 = 1, nNodesX(1)
+
+        jS = jS + 1
+
+        iX1 = 1
+        ! 1D spherical coordinate
+        ! use the first element in the block
+        IF( iS == 1 )THEN  ! first subgrid cell
+          r_l  = MeshX(1) % Center(iX1) - Half*MeshX(1) % Width(iX1)
+          r_r  = MeshX(1) % Center(iX1)
+        ELSE ! second subgrid cell
+          r_l  = MeshX(1) % Center(iX1)
+          r_r  = MeshX(1) % Center(iX1) + Half*MeshX(1) % Width(iX1)
+        END IF
+
+        IF( jS == 1 )THEN
+          r_q1 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(iX1)
+          r_q2 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(iX1+1)
+        ELSE
+          r_q1 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(iX1+1)
+          r_q2 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(iX1)
+        END IF
+
+        !!$WRITE(*,'(2I4,4ES12.3)') iS, jS, r_l, r_q1, r_q2, r_r
+
+        ProjectionMatrix(iS,jS) &
+        =  3.0e0 * ( r_r**4 - r_l**4 ) &
+           / ( 4.0e0 * ( r_r**3 - r_l**3 ) ) &
+          - r_q2
+
+        ProjectionMatrix(iS,jS) &
+        = ProjectionMatrix(iS,jS) &
+          / ( r_q1 - r_q2 )
+
+      END DO
+      END DO
+      END DO
+
+    END DO
+    END DO
+    END DO
+
+    END IF
+
+!!$ >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
     ALLOCATE( ReconstructionMatrix(nDOFX,nDOFX) )
 
     ! --- Compute Reconstruction Matrix by Inverting ProjectionMatrix ---
@@ -120,6 +188,108 @@ CONTAINS
     END IF
 
   END SUBROUTINE InitializeSubcellReconstruction
+
+
+  SUBROUTINE UpdateSubcellReconstruction( iX )
+
+    INTEGER, INTENT(in) :: iX
+
+    INTEGER  :: iS1, iS2, iS3, iS
+    INTEGER  :: jS1, jS2, jS3, jS
+    INTEGER  :: iN1, iN2, iN3
+    INTEGER  :: INFO
+    INTEGER  :: IPIV(nDOFX)
+    REAL(DP) :: WORK(nDOFX)
+
+    INTEGER  :: iX1, nNodes
+    REAL(DP) :: r_l, r_r, r_q1, r_q2
+
+
+    IF( TRIM( CoordinateSystem ) .eq. 'SPHERICAL')THEN
+
+      iX1 = iX
+      ! Use the mirror physical cell for ghost cell at origin radius
+      IF( MeshX(1) % Center( iX ) < 0.0 ) &
+        iX1 = iX_B0(1) - iX
+
+    iS = 0
+    DO iS3 = 1, nNodesX(3)
+    DO iS2 = 1, nNodesX(2)
+    DO iS1 = 1, nNodesX(1)
+
+      iS = iS + 1
+
+      jS = 0
+      DO jS3 = 1, nNodesX(3)
+      DO jS2 = 1, nNodesX(2)
+      DO jS1 = 1, nNodesX(1)
+
+        jS = jS + 1
+
+        nNodes = 2 !! ONLY for 2, NEED FIX
+        ! 1D spherical coordinate
+        IF( iS == 1 )THEN  ! first subgrid cell
+          r_l  = MeshX(1) % Center(iX1) - Half*MeshX(1) % Width(iX1)
+          r_r  = MeshX(1) % Center(iX1)
+        ELSE               ! second subgrid cell
+          r_l  = MeshX(1) % Center(iX1)
+          r_r  = MeshX(1) % Center(iX1) + Half*MeshX(1) % Width(iX1)
+        END IF
+
+        IF( jS == 1 )THEN
+          r_q1 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(1)
+          r_q2 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(2)
+        ELSE
+          r_q1 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(2)
+          r_q2 = MeshX(1) % Center(iX1) + MeshX(1) % Width(iX1) * MeshX(1) % Nodes(1)
+        END IF
+
+        !!$WRITE(*,'(2I4,4ES12.3)') iS, jS, r_l, r_q1, r_q2, r_r
+
+        ProjectionMatrix(iS,jS) &
+        =  3.0e0 * ( r_r**4 - r_l**4 ) &
+           / ( 4.0e0 * ( r_r**3 - r_l**3 ) ) &
+          - r_q2
+
+        ProjectionMatrix(iS,jS) &
+        = ProjectionMatrix(iS,jS) &
+          / ( r_q1 - r_q2 )
+
+      END DO
+      END DO
+      END DO
+
+    END DO
+    END DO
+    END DO
+
+    ReconstructionMatrix = ProjectionMatrix
+
+    CALL DGETRF( nDOFX, nDOFX, ReconstructionMatrix, nDOFX, IPIV, INFO )
+
+    IF( INFO /= 0 )THEN
+      WRITE(*,*)
+      WRITE(*,'(A4,A)') '', 'InitializeSubcellReconstruction:'
+      WRITE(*,*)
+      WRITE(*,'(A6,A,I4.4)') '', 'DGETRF returned INFO = ', INFO
+      WRITE(*,*)
+      STOP
+    END IF
+
+    CALL DGETRI( nDOFX, ReconstructionMatrix, nDOFX, IPIV, WORK, nDOFX, INFO )
+
+    IF( INFO /= 0 )THEN
+      WRITE(*,*)
+      WRITE(*,'(A4,A)') '', 'InitializeSubcellReconstruction:'
+      WRITE(*,*)
+      WRITE(*,'(A6,A,I4.4)') '', 'DGETRI returned INFO = ', INFO
+      WRITE(*,*)
+      STOP
+    END IF
+
+    END IF ! SPHERICAL
+
+  END SUBROUTINE UpdateSubcellReconstruction
 
 
   SUBROUTINE FinalizeSubcellReconstruction
