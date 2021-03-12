f0283e97ae018c3c61cef6a471706df1e9f833b9
Zack <zelledge@vols.utk.edu>
2021-03-12 11:17:00 -0500

    Adding HomogeneousSphere problem

--- SandBox/AMReX/TwoMoment/MF_InitializationModule.F90
+++ SandBox/AMReX/TwoMoment/MF_InitializationModule.F90
@@ -151,6 +151,9 @@ CONTAINS
 
         CALL InitializeFields_StreamingDopplerShift ( MF_uGF, MF_uCR, MF_uCF, V_0 )
 
+      CASE( 'HomogeneousSphere1D' )
+
+        CALL InitializeFields_HomogeneousSphere1D ( MF_uGF, MF_uCR, MF_uCF, V_0 )
 
         IF (Verbose) THEN
           print*, ProgramName
@@ -165,6 +168,7 @@ CONTAINS
           WRITE(*,'(6x,A)')     'SineWaveStreaming'
           WRITE(*,'(6x,A)')     'SineWaveDiffusion'
           WRITE(*,'(6x,A)')     'StreamingDopplerShift'
+          WRITE(*,'(6x,A)')     'HomogeneousSphere1D'
           STOP 'MF_InitializationModule_NonRelativistic_IDEAL.f90'
         END IF
 
@@ -666,7 +670,6 @@ CONTAINS
             X1 = NodeCoordinate( MeshX(1), iX1, iNodeX1 )
 !print*, X1    
             uPF_K(iNodeX,iPF_D ) = 1.0_AR
-
             
             IF( X1 .LT. X_0 )THEN
               uPF_K(iNodeX,iPF_V1) &
@@ -733,7 +736,6 @@ CONTAINS
               X1 = NodeCoordinate( MeshX(1), iX1, iNodeZ2 )
  
               E = NodeCoordinate( MeshE, iZ1, iNodeE )
-             !print*,E
               S = SQRT(1.0_AR+ uPF_K(iNodeX,iPF_V1))/SQRT(1.0_AR - uPF_K(iNodeX,iPF_V1))
               uPR_K( iNodeZ, iZ1, iPR_D, iS ) &
                  !=1d-8
@@ -971,6 +973,181 @@ CONTAINS
   END SUBROUTINE SetInnerBoundary_StreamingDopplerShift
 
 
+  SUBROUTINE InitializeFields_HomogeneousSphere1D( MF_uGF, MF_uCR, MF_uCF )
+
+    TYPE(amrex_multifab), INTENT(in   ) :: MF_uGF(0:nLevels-1)
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uCR(0:nLevels-1)
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uCF(0:nLevels-1)
+    
+
+    ! --- thornado ---
+    INTEGER        :: iDim
+    INTEGER        :: iX1, iX2, iX3, iZ1, iZ2, iZ3, iZ4, iS, iNodeZ, iSpecies
+    INTEGER        :: iNodeX, iNodeX1, iNodeX2, iNodeX3, iNodeZ2, iNodeE
+    REAL(AR)       :: X1, X2, X3, V_0(3)
+    REAL(AR)       :: uCR_K( nDOFZ, nE, nCR, nSpecies )
+    REAL(AR)       :: uPR_K( nDOFZ, nE, nPR, nSpecies )
+    REAL(AR)       :: uGF_K( nDOFX, nGF )
+    REAL(AR)       :: uPF_K( nDOFX, nPF )
+    REAL(AR)       :: uCF_K( nDOFX, nCF )
+    REAL(AR)       :: uAF_K( nDOFX, nAF )
+    TYPE(MeshType) :: MeshX(3)
+
+    ! --- AMReX ---
+    INTEGER                       :: iLevel
+    INTEGER                       :: lo_C(4), hi_C(4)
+    INTEGER                       :: lo_G(4), hi_G(4)
+    INTEGER                       :: lo_F(4), hi_F(4)
+    TYPE(amrex_box)               :: BX
+    TYPE(amrex_mfiter)            :: MFI
+    REAL(AR), CONTIGUOUS, POINTER :: uGF(:,:,:,:)
+    REAL(AR), CONTIGUOUS, POINTER :: uCR(:,:,:,:)
+    REAL(AR), CONTIGUOUS, POINTER :: uCF(:,:,:,:)
+    REAL(AR)                      :: Ones(nDOFE), W
+
+    uCR_K = Zero
+    uPF_K = Zero
+    uCF_K = Zero
+    uGF_K = Zero
+    uAF_K = Zero
+
+    Ones=1.0_AR
+   
+   
+    DO iDim = 1, 3
+
+      CALL CreateMesh &
+             ( MeshX(iDim), nX(iDim), nNodesX(iDim), 0, &
+               xL(iDim), xR(iDim) )
+
+    END DO
+    
+    DO iLevel = 0, nLevels-1
+
+      CALL amrex_mfiter_build( MFI, MF_uCR(iLevel), tiling = .TRUE. )
+
+      DO WHILE( MFI % next() )
+        
+        uGF => MF_uGF(iLevel) % DataPtr( MFI )
+        uCR => MF_uCR(iLevel) % DataPtr( MFI )
+        uCF => MF_uCF(iLevel) % DataPtr( MFI )
+
+        BX = MFI % tilebox()
+
+        lo_G = LBOUND( uGF )
+        hi_G = UBOUND( uGF )
+
+        lo_C = LBOUND( uCR )
+        hi_C = UBOUND( uCR )
+
+        lo_F = LBOUND( uCF )
+        hi_F = UBOUND( uCF )
+
+        DO iX3 = BX % lo(3), BX % hi(3)
+        DO iX2 = BX % lo(2), BX % hi(2)
+        DO iX1 = BX % lo(1), BX % hi(1)         
+          uGF_K &
+            = RESHAPE( uGF(iX1,iX2,iX3,lo_G(4):hi_G(4)), [ nDOFX, nGF ] )
+
+          DO iNodeX = 1, nDOFX
+
+            uPF_K(iNodeX,iPF_D ) = 1.0_AR
+            uPF_K(iNodeX,iPF_V1) = 0.0_DP
+            uPF_K(iNodeX,iPF_V2) = 0.0_DP
+            uPF_K(iNodeX,iPF_V3) = 0.0_DP
+            uPF_K(iNodeX,iPF_E ) = 0.1_AR
+            uPF_K(iNodeX,iPF_Ne) = 0.0_AR
+
+          END DO
+        CALL ComputePressureFromPrimitive &
+                 ( uPF_K(:,iPF_D), uPF_K(:,iPF_E), uPF_K(:,iPF_Ne), &
+                   uAF_K(:,iAF_P) )
+
+        CALL ComputeConserved_Euler_Relativistic &
+               ( uPF_K(:,iPF_D ), &
+                 uPF_K(:,iPF_V1), &
+                 uPF_K(:,iPF_V2), &
+                 uPF_K(:,iPF_V3), &
+                 uPF_K(:,iPF_E ), &
+                 uPF_K(:,iPF_Ne), &
+                 uCF_K(:,iCF_D ), &
+                 uCF_K(:,iCF_S1), &
+                 uCF_K(:,iCF_S2), &
+                 uCF_K(:,iCF_S3), &
+                 uCF_K(:,iCF_E ), &
+                 uCF_K(:,iCF_Ne), &
+                 uGF_K(:,iGF_Gm_dd_11), &
+                 uGF_K(:,iGF_Gm_dd_22), &
+                 uGF_K(:,iGF_Gm_dd_33), &
+                 uAF_K(:,iAF_P)      )
+
+          uCF(iX1,iX2,iX3,lo_F(4):hi_F(4)) &
+            = RESHAPE( uCF_K, [ hi_F(4) - lo_F(4) + 1 ] )   
+      
+          DO iNodeZ = 1, nDOFZ
+                        
+            DO iS = 1, nSpecies
+            DO iZ1 = 1, nE          
+
+              iNodeX = MOD( (iNodeZ-1) / nDOFE, nDOFX ) + 1
+
+              iNodeZ2 = NodeNumberTable(2,iNodeZ)
+
+              uPR_K( iNodeZ, iZ1, iPR_D, iS ) &
+                = 1.0d-8  
+            
+              uPR_K( iNodeZ, iZ1, iPR_I1, iS ) &
+                = 0.0_AR
+         
+              uPR_K( iNodeZ, iZ1, iPR_I2, iS ) &
+                = 0.0_AR
+
+              uPR_K( iNodeZ, iZ1, iPR_I3, iS ) &
+                = 0.0_AR
+
+              
+            CALL ComputeConserved_TwoMoment &
+                   ( uPR_K(iNodeZ,iZ1,iPR_D,iS), &
+                     uPR_K(iNodeZ,iZ1,iPR_I1,iS), &
+                     uPR_K(iNodeZ,iZ1,iPR_I2,iS), &
+                     uPR_K(iNodeZ,iZ1,iPR_I3,iS), &
+                     uCR_K(iNodeZ,iZ1,iCR_N,iS), &
+                     uCR_K(iNodeZ,iZ1,iCR_G1,iS), &
+                     uCR_K(iNodeZ,iZ1,iCR_G2,iS), &
+                     uCR_K(iNodeZ,iZ1,iCR_G3,iS), &
+                     uPF_K(iNodeX,iPF_V1),        &
+                     uPF_K(iNodeX,iPF_V2),        &
+                     uPF_K(iNodeX,iPF_V3),        &                         
+                     uGF_K(iNodeX,iGF_Gm_dd_11), &
+                     uGF_K(iNodeX,iGF_Gm_dd_22), &
+                     uGF_K(iNodeX,iGF_Gm_dd_33), &
+                     0.0_AR, 0.0_AR, 0.0_AR,     &
+                     uGF_K(iNodeX,iGF_Alpha), &
+                     uGF_K(iNodeX,iGF_Beta_1), &
+                     uGF_K(iNodeX,iGF_Beta_2), &
+                     uGF_K(iNodeX,iGF_Beta_3) )
+
+              
+            END DO
+            END DO
+!Reshape here instead of up top look at Hydro example
+          END DO 
+           
+            uCR(iX1,iX2,iX3,lo_C(4):hi_C(4)) &
+              = RESHAPE( uCR_K, [ hi_C(4) - lo_C(4) + 1 ] )
+
+ 
+        END DO
+        END DO
+        END DO
+
+      END DO
+      
+      CALL amrex_mfiter_destroy( MFI )
+
+    END DO
+  
+  END SUBROUTINE InitializeFields_HomogeneousSphere1D
 
 
 
