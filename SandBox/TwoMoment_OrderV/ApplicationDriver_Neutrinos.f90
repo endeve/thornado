9f3f470158bf3be8c4b0c56ceeee794c9074e001
Eirik Endeve <endevee@ornl.gov>
2021-04-15 09:59:48 -0400

    Added time-stepping loop to driver.  Progress on application coupling neutrinos and fluid.

--- SandBox/TwoMoment_OrderV/ApplicationDriver_Neutrinos.f90
+++ SandBox/TwoMoment_OrderV/ApplicationDriver_Neutrinos.f90
@@ -1,7 +1,7 @@
 PROGRAM ApplicationDriver_Neutrinos
 
   USE KindModule, ONLY: &
-    DP, Zero, One
+    DP, Zero, One, Two, SqrtTiny
   USE UnitsModule, ONLY: &
     Kilometer, &
     Millisecond, &
@@ -17,9 +17,15 @@ PROGRAM ApplicationDriver_Neutrinos
   USE FluidFieldsModule, ONLY: &
     uCF
   USE RadiationFieldsModule, ONLY: &
-    uCR
+    uCR, uPR
   USE InputOutputModuleHDF, ONLY: &
     WriteFieldsHDF
+  USE TwoMoment_UtilitiesModule_OrderV, ONLY: &
+    ComputeFromConserved_TwoMoment
+  USE TwoMoment_SlopeLimiterModule_OrderV, ONLY: &
+    ApplySlopeLimiter_TwoMoment
+  USE TwoMoment_PositivityLimiterModule_OrderV, ONLY: &
+    ApplyPositivityLimiter_TwoMoment
   USE TwoMoment_DiscretizationModule_Collisions_Neutrinos_OrderV, ONLY: &
     ComputeIncrement_TwoMoment_Implicit
   USE TwoMoment_TimeSteppingModule_OrderV, ONLY: &
@@ -37,14 +43,16 @@ PROGRAM ApplicationDriver_Neutrinos
   CHARACTER(64) :: OpacityTableName_Iso
   CHARACTER(64) :: OpacityTableName_NES
   CHARACTER(64) :: OpacityTableName_Pair
-  LOGICAL       :: UseSlopeLimiter
-  LOGICAL       :: UsePositivityLimiter
+  LOGICAL       :: UseSlopeLimiter_TwoMoment
+  LOGICAL       :: UsePositivityLimiter_TwoMoment
+  LOGICAL       :: FixedTimeStep
   INTEGER       :: nSpecies
   INTEGER       :: nNodes
   INTEGER       :: nE, bcE, nX(3), bcX(3)
+  INTEGER       :: iCycle, iCycleD, iCycleW
   REAL(DP)      :: xL(3), xR(3), ZoomX(3) = One
   REAL(DP)      :: eL, eR, ZoomE = One
-  REAL(DP)      :: dt, t_end
+  REAL(DP)      :: t, dt, dt_CFL, dt_FXD, t_end
 
   ProgramName = 'Relaxation'
 
@@ -55,6 +63,8 @@ PROGRAM ApplicationDriver_Neutrinos
   OpacityTableName_Iso  = 'wl-Op-SFHo-15-25-50-E40-B85-Iso.h5'
   OpacityTableName_NES  = 'wl-Op-SFHo-15-25-50-E40-B85-NES.h5'
   OpacityTableName_Pair = 'wl-Op-SFHo-15-25-50-E40-B85-Pair.h5'
+
+  FixedTimeStep = .FALSE.
 
   SELECT CASE( TRIM( ProgramName ) )
 
@@ -78,9 +88,39 @@ PROGRAM ApplicationDriver_Neutrinos
 
       t_end = 1.0d0 * Millisecond
 
-      UseSlopeLimiter = .FALSE.
+      FixedTimeStep = .TRUE.
+      dt_FXD        = 1.0d-3 * Millisecond
+      iCycleD       = 1
+      iCycleW       = 10
 
-      UsePositivityLimiter = .FALSE.
+      UseSlopeLimiter_TwoMoment      = .FALSE.
+      UsePositivityLimiter_TwoMoment = .FALSE.
+
+    CASE( 'Deleptonization' )
+
+      nSpecies = 2
+      nNodes   = 2
+
+      nX  = [ 64, 1, 1 ]
+      xL  = [ - 50.0_DP, 0.0_DP, 0.0_DP ] * Kilometer
+      xR  = [ + 50.0_DP, 1.0_DP, 1.0_DP ] * Kilometer
+      bcX = [ 31, 1, 1 ]
+
+      nE    = 16
+      eL    = 0.0d0 * MeV
+      eR    = 3.0d2 * MeV
+      bcE   = 10
+      ZoomE = 1.266038160710160_DP
+
+      TimeSteppingScheme = 'IMEX_PDARS'
+
+      t_end = 1.0d0 * Millisecond
+
+      iCycleD = 1
+      iCycleW = 10
+
+      UseSlopeLimiter_TwoMoment      = .FALSE.
+      UsePositivityLimiter_TwoMoment = .TRUE.
 
     CASE DEFAULT
 
@@ -95,16 +135,91 @@ PROGRAM ApplicationDriver_Neutrinos
 
   CALL InitializeFields
 
+  ! --- Apply Slope Limiter to Initial Data ---
+
+  CALL ApplySlopeLimiter_TwoMoment &
+         ( iZ_B0, iZ_E0, iZ_B1, iZ_E1, uGE, uGF, uCF, uCR )
+
+  ! --- Apply Positivity Limiter to Initial Data ---
+
+  CALL ApplyPositivityLimiter_TwoMoment &
+         ( iZ_B0, iZ_E0, iZ_B1, iZ_E1, uGE, uGF, uCF, uCR )
+
   CALL WriteFieldsHDF &
          ( Time = 0.0_DP, &
            WriteGF_Option = .TRUE., &
            WriteFF_Option = .TRUE., &
            WriteRF_Option = .TRUE. )
 
-  dt = 1.0d-3 * Millisecond
+  ! --- Evolve ---
 
-  CALL Update_IMEX_RK &
-         ( dt, uGE, uGF, uCF, uCR, ComputeIncrement_TwoMoment_Implicit )
+  t = Zero
+
+  WRITE(*,*)
+  WRITE(*,'(A6,A,ES8.2E2,A8,ES8.2E2)') &
+    '', 'Evolving from t = ', t / Millisecond, ' to t = ', t_end / Millisecond
+  WRITE(*,*)
+
+  iCycle = 0
+  DO WHILE( t < t_end )
+
+    iCycle = iCycle + 1
+
+    IF( FixedTimeStep )THEN
+
+      dt = dt_FXD
+
+    ELSE
+
+      dt_CFL = 0.3_DP * MINVAL( (xR-xL)/DBLE(nX) ) / ( Two*DBLE(nNodes-1)+One )
+
+      dt = dt_CFL
+
+    END IF
+
+    IF( t + dt > t_end )THEN
+
+      dt = t_end - t
+
+    END IF
+
+    IF( MOD( iCycle, iCycleD ) == 0 )THEN
+
+      WRITE(*,'(A8,A8,I8.8,A2,A4,ES12.6E2,A1,A5,ES12.6E2)') &
+          '', 'Cycle = ', iCycle, &
+          '', 't = '    ,  t / Millisecond, &
+          '', 'dt = '   , dt / Millisecond
+
+    END IF
+
+    CALL Update_IMEX_RK &
+           ( dt, uGE, uGF, uCF, uCR, ComputeIncrement_TwoMoment_Implicit )
+
+    t = t + dt
+
+    IF( MOD( iCycle, iCycleW ) == 0 )THEN
+
+      CALL ComputeFromConserved_TwoMoment &
+             ( iZ_B0, iZ_E0, iZ_B1, iZ_E1, uGF, uCF, uCR, uPR )
+
+      CALL WriteFieldsHDF &
+             ( Time = t, &
+               WriteGF_Option = .TRUE., &
+               WriteFF_Option = .TRUE., &
+               WriteRF_Option = .TRUE. )
+
+    END IF
+
+  END DO
+
+  CALL ComputeFromConserved_TwoMoment &
+         ( iZ_B0, iZ_E0, iZ_B1, iZ_E1, uGF, uCF, uCR, uPR )
+
+  CALL WriteFieldsHDF &
+         ( Time = t, &
+           WriteGF_Option = .TRUE., &
+           WriteFF_Option = .TRUE., &
+           WriteRF_Option = .TRUE. )
 
   CALL FinalizeDriver
 
@@ -141,6 +256,12 @@ CONTAINS
       InitializeOpacities_TABLE
     USE TwoMoment_ClosureModule, ONLY: &
       InitializeClosure_TwoMoment
+    USE TwoMoment_TroubledCellIndicatorModule, ONLY: &
+      InitializeTroubledCellIndicator_TwoMoment
+    USE TwoMoment_SlopeLimiterModule_OrderV, ONLY: &
+      InitializeSlopeLimiter_TwoMoment
+    USE TwoMoment_PositivityLimiterModule_OrderV, ONLY: &
+      InitializePositivityLimiter_TwoMoment
     USE TwoMoment_TimeSteppingModule_OrderV, ONLY: &
       Initialize_IMEX_RK
 
@@ -237,6 +358,38 @@ CONTAINS
 
     CALL InitializeClosure_TwoMoment
 
+    ! --- Initialize Troubled Cell Indicator (Two-Moment) ---
+
+    CALL InitializeTroubledCellIndicator_TwoMoment &
+           ( UseTroubledCellIndicator_Option &
+               = .FALSE., &
+             C_TCI_Option &
+               = Zero, &
+             Verbose_Option &
+               = .TRUE. )
+
+    ! --- Initialize Slope Limiter (Two-Moment) ---
+
+    CALL InitializeSlopeLimiter_TwoMoment &
+           ( BetaTVD_Option &
+               = 1.75_DP, &
+             UseSlopeLimiter_Option &
+               = UseSlopeLimiter_TwoMoment, &
+             Verbose_Option &
+               = .TRUE. )
+
+    ! --- Initialize Positivity Limiter (Two-Moment) ---
+
+    CALL InitializePositivityLimiter_TwoMoment &
+           ( Min_1_Option &
+               = SqrtTiny, &
+             Min_2_Option &
+               = SqrtTiny, &
+             UsePositivityLimiter_Option &
+               = UsePositivityLimiter_TwoMoment, &
+             Verbose_Option &
+               = .TRUE. )
+
     ! --- Initialize Time Stepper ---
 
     CALL Initialize_IMEX_RK( TRIM( TimeSteppingScheme ) )
@@ -246,6 +399,18 @@ CONTAINS
 
   SUBROUTINE FinalizeDriver
 
+    USE TwoMoment_TimeSteppingModule_OrderV, ONLY: &
+      Finalize_IMEX_RK
+    USE EquationOfStateModule_TABLE, ONLY: &
+      FinalizeEquationOfState_TABLE
+    USE OpacityModule_TABLE, ONLY: &
+      FinalizeOpacities_TABLE
+    USE TwoMoment_TroubledCellIndicatorModule, ONLY: &
+      FinalizeTroubledCellIndicator_TwoMoment
+    USE TwoMoment_SlopeLimiterModule_OrderV, ONLY: &
+      FinalizeSlopeLimiter_TwoMoment
+    USE TwoMoment_PositivityLimiterModule_OrderV, ONLY: &
+      FinalizePositivityLimiter_TwoMoment
     USE ReferenceElementModuleX, ONLY: &
       FinalizeReferenceElementX
     USE ReferenceElementModuleX_Lagrange, ONLY: &
@@ -260,12 +425,6 @@ CONTAINS
       FinalizeReferenceElement
     USE ReferenceElementModule_Lagrange, ONLY: &
       FinalizeReferenceElement_Lagrange
-    USE EquationOfStateModule_TABLE, ONLY: &
-      FinalizeEquationOfState_TABLE
-    USE OpacityModule_TABLE, ONLY: &
-      FinalizeOpacities_TABLE
-    USE TwoMoment_TimeSteppingModule_OrderV, ONLY: &
-      Finalize_IMEX_RK
     USE ProgramInitializationModule, ONLY: &
       FinalizeProgram
     USE TwoMoment_TimersModule_OrderV, ONLY: &
@@ -276,6 +435,12 @@ CONTAINS
     CALL FinalizeEquationOfState_TABLE
 
     CALL FinalizeOpacities_TABLE
+
+    CALL FinalizeTroubledCellIndicator_TwoMoment
+
+    CALL FinalizeSlopeLimiter_TwoMoment
+
+    CALL FinalizePositivityLimiter_TwoMoment
 
     CALL FinalizeReferenceElementX
 
