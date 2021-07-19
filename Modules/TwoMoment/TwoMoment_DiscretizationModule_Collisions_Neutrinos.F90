2c9a9d46ccc255f69a10bfb421ff1f4999994265
Austin Harris <jaharris87@users.noreply.github.com>
2021-07-19 17:50:42 -0400

    Missing array in PRESENT

--- Modules/TwoMoment/TwoMoment_DiscretizationModule_Collisions_Neutrinos.F90
+++ Modules/TwoMoment/TwoMoment_DiscretizationModule_Collisions_Neutrinos.F90
@@ -644,7 +644,7 @@ CONTAINS
 #elif defined(THORNADO_OACC)
     !$ACC PARALLEL LOOP GANG VECTOR COLLAPSE(5) &
     !$ACC PRIVATE( iN_X ) &
-    !$ACC PRESENT( nX, iX_B0, dU_F, CF_N, dF_N )
+    !$ACC PRESENT( nX, iX_B0, iX_E0, dU_F, CF_N, dF_N )
 #elif defined(THORNADO_OMP)
     !$OMP PARALLEL DO COLLAPSE(5) &
     !$OMP PRIVATE( iN_X )
