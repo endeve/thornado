1cb950c9a482f1e235659f64925f0c7f69436752
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2021-09-17 14:30:33 -0400

    Added OffGrid flux to AdiabaticCollapse_CFA

--- SandBox/GravitationalCollapse_CFA/TimeSteppingModule_SSPRK.f90
+++ SandBox/GravitationalCollapse_CFA/TimeSteppingModule_SSPRK.f90
@@ -30,6 +30,10 @@ MODULE TimeSteppingModule_SSPRK
     TimersStart_Euler, &
     TimersStop_Euler,  &
     Timer_Euler_UpdateFluid
+  USE Euler_dgDiscretizationModule, ONLY: &
+    OffGridFlux_Euler
+  USE Euler_TallyModule_Relativistic, ONLY: &
+    IncrementOffGridTally_Euler_Relativistic
 
   IMPLICIT NONE
   PRIVATE
@@ -206,6 +210,10 @@ CONTAINS
                          iX_B0(2):iX_E0(2), &
                          iX_B0(3):iX_E0(3))
 
+    REAL(DP) :: dM_OffGrid_Euler(nCF)
+
+    dM_OffGrid_Euler = Zero
+
     CALL TimersStart_Euler( Timer_Euler_UpdateFluid )
 
     CALL MultiplyByPsi6( iX_B1, iX_E1, G, U )
@@ -269,6 +277,10 @@ CONTAINS
                  G, Ustar, D, Dstar(:,:,:,:,:,iS), &
                  UseXCFC_Option = .TRUE. )
 
+        dM_OffGrid_Euler &
+          = dM_OffGrid_Euler &
+              + dt * w_SSPRK(iS) * OffGridFlux_Euler
+
       END IF
 
     END DO
@@ -313,6 +325,8 @@ CONTAINS
            ( iX_B0, iX_E0, iX_B1, iX_E1, E, S, Si, G )
 
     CALL DivideByPsi6( iX_B1, iX_E1, G, U )
+
+    CALL IncrementOffGridTally_Euler_Relativistic( dM_OffGrid_Euler )
 
     CALL TimersStop_Euler( Timer_Euler_UpdateFluid )
 
