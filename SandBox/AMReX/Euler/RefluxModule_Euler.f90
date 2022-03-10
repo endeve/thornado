d5fd97364feb5d7658a56b8c79994557276921e1
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-03-10 16:36:09 -0500

    Added RefluxModule_Euler

---
+++ SandBox/AMReX/Euler/RefluxModule_Euler.f90
@@ -0,0 +1,84 @@
+MODULE RefluxModule_Euler
+
+  ! --- AMReX Modules ---
+
+  USE amrex_multifab_module, ONLY: &
+    amrex_multifab
+  USE amrex_amrcore_module, ONLY: &
+    amrex_max_level
+
+  ! --- thornado Modules ---
+
+  USE FluidFieldsModule, ONLY: &
+    nCF
+  USE MeshModule, ONLY: &
+    MeshX
+
+  ! --- Local Modules ---
+
+  USE MF_FieldsModule, ONLY: &
+    FluxRegister
+  USE MF_MeshModule, ONLY: &
+    CreateMesh_MF, &
+    DestroyMesh_MF
+  USE AverageDownModule, ONLY: &
+    AverageDownTo
+  USE InputParsingModule, ONLY: &
+    do_reflux
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: Reflux_Euler_MF
+
+  INTERFACE Reflux_Euler_MF
+    MODULE PROCEDURE Reflux_Euler_MF_SingleLevel
+    MODULE PROCEDURE Reflux_Euler_MF_MultipleLevels
+  END INTERFACE Reflux_Euler_MF
+
+
+CONTAINS
+
+
+  SUBROUTINE Reflux_Euler_MF_MultipleLevels( MF )
+
+    TYPE(amrex_multifab), INTENT(inout) :: MF(0:amrex_max_level)
+
+    INTEGER :: iLevel
+
+    IF( .NOT. do_reflux ) RETURN
+
+    DO iLevel = 0, amrex_max_level
+
+      IF( iLevel .GT. 0 ) &
+        CALL Reflux_Euler_MF_SingleLevel( iLevel, MF )
+
+    END DO
+
+  END SUBROUTINE Reflux_Euler_MF_MultipleLevels
+
+
+  SUBROUTINE Reflux_Euler_MF_SingleLevel( FineLevel, MF )
+
+    INTEGER             , INTENT(in)    :: FineLevel
+    TYPE(amrex_multifab), INTENT(inout) :: MF(0:amrex_max_level)
+
+    CALL CreateMesh_MF( FineLevel-1, MeshX )
+
+    ASSOCIATE( dX1 => MeshX(1) % Width, &
+               dX2 => MeshX(2) % Width, &
+               dX3 => MeshX(3) % Width )
+
+    CALL FluxRegister( FineLevel ) &
+           % reflux_dg( MF(FineLevel-1), nCF, dX1, dX2, dX3 )
+
+    END ASSOCIATE
+
+    CALL DestroyMesh_MF( MeshX )
+
+    CALL AverageDownTo( FineLevel-1, MF )
+
+  END SUBROUTINE Reflux_Euler_MF_SingleLevel
+
+
+END MODULE RefluxModule_Euler
