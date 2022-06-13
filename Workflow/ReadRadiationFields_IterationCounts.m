c9e8657e893413927e068a23801292135cf99bb1
Eirik Endeve <endevee@ornl.gov>
2022-06-13 19:55:43 -0400

    Adding time and coordinates for iteration counts reader.

--- Workflow/ReadRadiationFields_IterationCounts.m
+++ Workflow/ReadRadiationFields_IterationCounts.m
@@ -1,4 +1,4 @@
-function [ nIter_Outer, nIter_Inner ]...
+function [ Time, X1, X2, X3, nIter_Outer, nIter_Inner ]...
   = ReadRadiationFields_IterationCounts( AppName, FileNumber, Directory)
 
   if( exist( 'Directory', 'var' ) )
@@ -8,6 +8,11 @@ function [ nIter_Outer, nIter_Inner ]...
   end
   
   FileName = [ DirName '/' AppName '_RadiationFields_' sprintf( '%06d', FileNumber ) '.h5' ];
+  
+  Time = h5read( FileName, '/Time' );  
+  X1   = h5read( FileName, '/Spatial Grid/X1' );
+  X2   = h5read( FileName, '/Spatial Grid/X2' );
+  X3   = h5read( FileName, '/Spatial Grid/X3' );
   
   nIter_Outer = h5read( FileName, '/Iteration Counts/Outer Iterations' );
   nIter_Inner = h5read( FileName, '/Iteration Counts/Inner Iterations' );
