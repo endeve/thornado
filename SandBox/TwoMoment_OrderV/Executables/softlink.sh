6a216192156a2891a36b0c47ac4f9e7e0b386a74
Quan, Shaoping <shaoping.quan@intel.com>
2022-08-05 11:34:55 -0400

    added the softlink.sh

---
+++ SandBox/TwoMoment_OrderV/Executables/softlink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+WEAKLIB_TABLES_DIR='/localdisk/quanshao/ExaStar/weaklib-tables'
+ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-Op-SFHo-15-25-50-E40-B85-AbEm.h5
+ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-Op-SFHo-15-25-50-E40-B85-Iso.h5
+ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-Op-SFHo-15-25-50-E40-B85-NES.h5
+ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-Op-SFHo-15-25-50-E40-B85-Pair.h5
+ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-Op-SFHo-15-25-50-E40-HR98-Brem.h5
+ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-EOS-SFHo-15-25-50.h5 
+#ln -s $WEAKLIB_TABLES_DIR/SFHo/LowRes/wl-EOS-SFHo-15-25-50.h5 EquationOfStateTable.h5 ## Legacy one: wrong
