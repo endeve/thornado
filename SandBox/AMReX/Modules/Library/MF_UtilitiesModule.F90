95a5519348cd2862199cf4e828a8f99678624c67
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2022-10-18 09:50:11 -0400

    allowed writing nodal data to file in parallel

--- SandBox/AMReX/Modules/Library/MF_UtilitiesModule.F90
+++ SandBox/AMReX/Modules/Library/MF_UtilitiesModule.F90
@@ -15,7 +15,8 @@ MODULE MF_UtilitiesModule
     amrex_geometry
   USE amrex_parallel_module, ONLY: &
     amrex_parallel_ioprocessor, &
-    amrex_parallel_reduce_sum
+    amrex_parallel_reduce_sum, &
+    amrex_parallel_myproc
 
   ! --- thornado Modules ---
 
@@ -115,7 +116,8 @@ MODULE MF_UtilitiesModule
     nE, &
     swX, &
     swE, &
-    nSpecies
+    nSpecies, &
+    StepNo
   USE MF_MeshModule, ONLY: &
     CreateMesh_MF, &
     DestroyMesh_MF
@@ -164,14 +166,14 @@ CONTAINS
 
   SUBROUTINE ShowVariableFromMultiFab_Single &
     ( iLevel, MF, iField, iMF_Mask_Option, &
-      swXX_Option, WriteToFile_Option, FileName_Option )
+      swXX_Option, WriteToFile_Option, FileNameBase_Option )
 
     INTEGER              , INTENT(in) :: iLevel, iField
     TYPE(amrex_multifab) , INTENT(in) :: MF
     TYPE(amrex_imultifab), INTENT(in), OPTIONAL :: iMF_Mask_Option
     INTEGER              , INTENT(in), OPTIONAL :: swXX_Option(3)
     LOGICAL              , INTENT(in), OPTIONAL :: WriteToFile_Option
-    CHARACTER(*)         , INTENT(in), OPTIONAL :: FileName_Option
+    CHARACTER(*)         , INTENT(in), OPTIONAL :: FileNameBase_Option
 
     INTEGER                       :: iX1, iX2, iX3, iNX
     INTEGER                       :: lo(4), hi(4)
@@ -180,9 +182,10 @@ CONTAINS
     REAL(DP), CONTIGUOUS, POINTER :: F(:,:,:,:)
     INTEGER , CONTIGUOUS, POINTER :: Mask(:,:,:,:)
     INTEGER                       :: swXX(3)
+    INTEGER                       :: iFileNo
     LOGICAL                       :: WriteToFile
     CHARACTER(128)                :: FMT
-    CHARACTER(128)                :: FileName
+    CHARACTER(128)                :: FileNameBase, FileName
 
     REAL(DP) :: NodesX1(nNodesX(1))
     REAL(DP) :: NodesX2(nNodesX(2))
@@ -194,23 +197,34 @@ CONTAINS
     WriteToFile = .FALSE.
     IF( PRESENT( WriteToFile_Option ) ) WriteToFile = WriteToFile_Option
 
-    FileName = ''
-    IF( PRESENT( FileName_Option ) ) FileName = TRIM( FileName_Option )
-
     WRITE(FMT,'(A,I2.2,A,I2.2,A,I2.2,A,I3.3,A)') &
-      '(I2.2,3I5.3,3ES12.03E3,', &
-      nNodesX(1),  'ES12.03E3,', &
-      nNodesX(2),  'ES12.03E3,', &
-      nNodesX(3),  'ES12.03E3,', &
-      nDOFX     ,  'ES12.03E3)'
-
-    IF( WriteToFile ) OPEN( 100, FILE = TRIM( FileName ), POSITION = 'APPEND' )
+      '(I2.2,3I5.3,3ES25.16E3,', &
+      nNodesX(1),  'ES25.16E3,', &
+      nNodesX(2),  'ES25.16E3,', &
+      nNodesX(3),  'ES25.16E3,', &
+      nDOFX     ,  'ES25.16E3)'
 
     CALL amrex_mfiter_build( MFI, MF, tiling = UseTiling )
 
     CALL CreateMesh_MF( iLevel, MeshX )
 
     DO WHILE( MFI % next() )
+
+      IF( WriteToFile )THEN
+
+        iFileNo = 100 + amrex_parallel_myproc()
+
+        FileNameBase = 'NodalData'
+        IF( PRESENT( FileNameBase_Option ) ) &
+          FileNameBase = TRIM( FileNameBase_Option )
+
+        WRITE(FileName,'(A,A,I3.3,A,I3.3,A,I8.8,A)') &
+          TRIM( FileNameBase ), '_level', iLevel, '_proc', &
+          amrex_parallel_myproc(), '_', StepNo(0), '.dat'
+
+        OPEN( iFileNo, FILE = TRIM( FileName ), POSITION = 'APPEND' )
+
+      END IF
 
       IF( PRESENT( iMF_Mask_Option ) ) &
         Mask => iMF_Mask_Option % DataPtr( MFI )
@@ -252,7 +266,7 @@ CONTAINS
 
         IF( WriteToFile )THEN
 
-          WRITE(100,TRIM(FMT)) &
+          WRITE(iFileNo,TRIM(FMT)) &
             iLevel, iX1, iX2, iX3, &
             MeshX(1) % Width(iX1), &
             MeshX(2) % Width(iX2), &
@@ -276,39 +290,33 @@ CONTAINS
       END DO
       END DO
 
+      IF( WriteToFile ) CLOSE( iFileNo )
+
     END DO
 
     CALL amrex_mfiter_destroy( MFI )
 
     CALL DestroyMesh_MF( MeshX )
 
-    IF( WriteToFile )THEN
-
-      CLOSE(100)
-
-    ELSE
-
-      WRITE(*,*)
-
-    END IF
+    IF( .NOT. WriteToFile ) WRITE(*,*)
 
   END SUBROUTINE ShowVariableFromMultiFab_Single
 
 
   SUBROUTINE ShowVariableFromMultiFab_Vector &
-    ( MF, iField, swXX_Option, WriteToFile_Option, FileName_Option )
+    ( MF, iField, swXX_Option, WriteToFile_Option, FileNameBase_Option )
 
     INTEGER             , INTENT(in) :: iField
     TYPE(amrex_multifab), INTENT(in) :: MF(0:)
     INTEGER             , INTENT(in), OPTIONAL :: swXX_Option(3)
     LOGICAL             , INTENT(in), OPTIONAL :: WriteToFile_Option
-    CHARACTER(*)        , INTENT(in), OPTIONAL :: FileName_Option
+    CHARACTER(*)        , INTENT(in), OPTIONAL :: FileNameBase_Option
 
     INTEGER :: iLevel
 
     INTEGER        :: swXX(3)
     LOGICAL        :: WriteToFile
-    CHARACTER(128) :: FileName
+    CHARACTER(128) :: FileNameBase
 
     TYPE(amrex_imultifab) :: iMF_Mask
 
@@ -318,8 +326,9 @@ CONTAINS
     WriteToFile = .FALSE.
     IF( PRESENT( WriteToFile_Option ) ) WriteToFile = WriteToFile_Option
 
-    FileName = ''
-    IF( PRESENT( FileName_Option ) ) FileName = TRIM( FileName_Option )
+    FileNameBase = ''
+    IF( PRESENT( FileNameBase_Option ) ) &
+      FileNameBase = TRIM( FileNameBase_Option )
 
     DO iLevel = 0, nLevels-1
 
@@ -330,7 +339,7 @@ CONTAINS
                iMF_Mask_Option = iMF_Mask, &
                swXX_Option = swXX, &
                WriteToFile_Option = WriteToFile, &
-               FileName_Option = TRIM( FileName ) )
+               FileNameBase_Option = TRIM( FileNameBase ) )
 
       CALL DestroyFineMask( iLevel, iMF_Mask )
 
@@ -1400,7 +1409,7 @@ CONTAINS
 
         iD_spatial = iD_Spatial + 1
 
-        Data_amrex_spatial(iZ2,iZ3,iZ4,iD_spatial) & 
+        Data_amrex_spatial(iZ2,iZ3,iZ4,iD_spatial) &
           = Data_amrex(iZ2,iZ3,iZ4,iD)
 
       END DO
@@ -1450,7 +1459,7 @@ iD_spatial = 0
                 + ( iZ1 - 1 ) * nDOFZ + iNodeZ
 
         iD_spatial = iD_Spatial + 1
-        Data_amrex(iZ2,iZ3,iZ4,iD) & 
+        Data_amrex(iZ2,iZ3,iZ4,iD) &
           = Data_amrex_spatial(iZ2,iZ3,iZ4,iD_spatial)
 
       END DO
