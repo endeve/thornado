0c2a396942f8a8e15df743895286a5cf142f7cda
vass <mewesv@ornl.gov>
2023-02-20 14:55:56 -0500

    removed nonsensical lines

--- SandBox/Interface_FLASH/ThornadoInitializationModule.F90
+++ SandBox/Interface_FLASH/ThornadoInitializationModule.F90
@@ -273,8 +273,6 @@ contains
 
     call CreateMesh &
            ( MeshE, nE, nNodesE, swE, eL, eR, zoomOption = zoomE )
-write(*,*) size(MeshE % NodesE)
-stop
 
     call CreateGeometryFieldsE &
            ( nE, swE, Verbose_Option = Verbose )
