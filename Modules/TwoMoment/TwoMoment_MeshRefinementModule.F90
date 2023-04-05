d82079e1942801048fee871c63c3eafc057dd790
Austin Harris <jaharris87@users.noreply.github.com>
2023-04-04 23:48:20 -0400

    Rework coarsen and refine routines to use only one matrix multiply

--- Modules/TwoMoment/TwoMoment_MeshRefinementModule.F90
+++ Modules/TwoMoment/TwoMoment_MeshRefinementModule.F90
@@ -23,17 +23,13 @@ MODULE TwoMoment_MeshRefinementModule
 
   PUBLIC :: InitializeMeshRefinement_TwoMoment
   PUBLIC :: FinalizeMeshRefinement_TwoMoment
-  PUBLIC :: Refine_TwoMoment
   PUBLIC :: RefineX_TwoMoment
-  PUBLIC :: RefineX_TwoMoment_Vector
-  PUBLIC :: Coarsen_TwoMoment
   PUBLIC :: CoarsenX_TwoMoment
-  PUBLIC :: CoarsenX_TwoMoment_Vector
 
   INTEGER  :: nFine, nFineX(3)
   REAL(DP), PUBLIC :: VolumeRatio
-  REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix  (:,:,:)
-  REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix_T(:,:,:) ! --- Transpose ---
+  REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix  (:,:)
+  REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix_T(:,:) ! --- Transpose ---
 
 CONTAINS
 
@@ -43,7 +39,7 @@ CONTAINS
     CHARACTER(2)  :: MeshString
     CHARACTER(32) :: MatrixName
     INTEGER :: iFine, iFineX1, iFineX2, iFineX3
-    INTEGER :: i, k, iN1, iN2, iN3
+    INTEGER :: i, j, k, iN1, iN2, iN3
     REAL(DP), ALLOCATABLE :: xiX1(:), xiX2(:), xiX3(:)
 
     nFineX      = 1
@@ -55,12 +51,14 @@ CONTAINS
     END DO
     nFine = PRODUCT( nFineX )
 
-    ALLOCATE( ProjectionMatrix  (nDOFX,nDOFX,nFine) )
-    ALLOCATE( ProjectionMatrix_T(nDOFX,nDOFX,nFine) )
+    ALLOCATE( ProjectionMatrix  (nDOFX*nFine,nDOFX      ) )
+    ALLOCATE( ProjectionMatrix_T(nDOFX      ,nDOFX*nFine) )
 
     ALLOCATE( xiX1(nNodesX(1)) )
     ALLOCATE( xiX2(nNodesX(2)) )
     ALLOCATE( xiX3(nNodesX(3)) )
+
+    ProjectionMatrix = 0.0_DP
 
     iFine = 0
     DO iFineX3 = 1, nFineX(3)
@@ -87,16 +85,17 @@ CONTAINS
         xiX3 = Zero
       END IF
 
-      ProjectionMatrix(:,:,iFine) = 0.0_DP
       DO k = 1, nDOFX
       DO i = 1, nDOFX
+
+        j = ( iFine - 1 ) * nDOFX + i
 
         DO iN3 = 1, nNodesX(3)
         DO iN2 = 1, nNodesX(2)
         DO iN1 = 1, nNodesX(1)
 
-          ProjectionMatrix(i,k,iFine) &
-            = ProjectionMatrix(i,k,iFine) &
+          ProjectionMatrix(j,k) &
+            = ProjectionMatrix(j,k) &
                 + WeightsX1(iN1) * WeightsX2(iN2) * WeightsX3(iN3) &
                   * L_X1(IndLX_Q(1,i)) % P( NodesX1(iN1) ) &
                   * L_X2(IndLX_Q(2,i)) % P( NodesX2(iN2) ) &
@@ -112,11 +111,24 @@ CONTAINS
       END DO
       END DO
 
-      ProjectionMatrix_T(:,:,iFine) &
-        = TRANSPOSE( ProjectionMatrix(:,:,iFine) )
+    END DO
+    END DO
+    END DO
 
-    END DO
-    END DO
+    ProjectionMatrix_T &
+      = TRANSPOSE( ProjectionMatrix )
+
+    DO k = 1, nDOFX
+      DO iFine = 1, nFine
+        DO i = 1, nDOFX
+
+          j = ( iFine - 1 ) * nDOFX + i
+
+          ProjectionMatrix  (j,k) = ProjectionMatrix  (j,k) / WeightsX_Q(i)
+          ProjectionMatrix_T(k,j) = ProjectionMatrix_T(k,j) / WeightsX_Q(k)
+
+        END DO
+      END DO
     END DO
 
     DEALLOCATE( xiX1 )
@@ -125,10 +137,10 @@ CONTAINS
 
 #if   defined( THORNADO_OMP_OL )
     !$OMP TARGET ENTER DATA
-    !$OMP MAP( to: ProjectionMatrix, ProjectionMatrix_T, VolumeRatio )
+    !$OMP MAP( to: ProjectionMatrix, ProjectionMatrix_T )
 #elif defined( THORNADO_OACC   )
     !$ACC ENTER DATA &
-    !$ACC COPYIN(  ProjectionMatrix, ProjectionMatrix_T, VolumeRatio )
+    !$ACC COPYIN(  ProjectionMatrix, ProjectionMatrix_T )
 #endif
 
   END SUBROUTINE InitializeMeshRefinement_TwoMoment
@@ -138,10 +150,10 @@ CONTAINS
 
 #if   defined( THORNADO_OMP_OL )
     !$OMP TARGET EXIT DATA
-    !$OMP MAP( release: ProjectionMatrix, ProjectionMatrix_T, VolumeRatio )
+    !$OMP MAP( release: ProjectionMatrix, ProjectionMatrix_T )
 #elif defined( THORNADO_OACC   )
     !$ACC EXIT DATA &
-    !$ACC DELETE(       ProjectionMatrix, ProjectionMatrix_T, VolumeRatio )
+    !$ACC DELETE(       ProjectionMatrix, ProjectionMatrix_T )
 #endif
 
     DEALLOCATE( ProjectionMatrix )
@@ -150,291 +162,82 @@ CONTAINS
   END SUBROUTINE FinalizeMeshRefinement_TwoMoment
 
 
-  SUBROUTINE Refine_TwoMoment( nE, nX, U_Crs, U_Fin )
+  SUBROUTINE RefineX_TwoMoment( nX, nVar, U_Crs, U_Fin )
 
-    INTEGER,  INTENT(in)  :: nE, nX(3)
-    REAL(DP), INTENT(in)  :: U_Crs(1:nDOF,1:nE)
-    REAL(DP), INTENT(out) :: U_Fin(1:nDOF,1:nE,1:nX(1),1:nX(2),1:nX(3))
+    INTEGER,  INTENT(in)  :: nX(3), nVar
+    REAL(DP), INTENT(in)  :: U_Crs(nDOFX,nX(1),nX(2),nX(3),nVar)
+    REAL(DP), INTENT(out) :: U_Fin(nDOFX,nFine,nX(1),nX(2),nX(3),nVar)
 
-    INTEGER :: iFine, iFineX1, iFineX2, iFineX3, iE
-    REAL(DP) :: U_Crs_P(nDOFX,nDOFE*nE)
-    REAL(DP) :: U_Fin_P(nDOFX,nDOFE*nE)
+    INTEGER :: nP_X
 
-    iFine = 0
-    DO iFineX3 = 1, nFineX(3)
-    DO iFineX2 = 1, nFineX(2)
-    DO iFineX1 = 1, nFineX(1)
-
-      iFine = iFine + 1
-
-      U_Crs_P = Pack_TwoMoment( nE, U_Crs )
-
-      U_Fin_P(:,:) = MATMUL( ProjectionMatrix(:,:,iFine), U_Crs_P(:,:) )
-
-      DO iE = 1, nDOFE * nE
-
-        U_Fin_P(:,iE) = U_Fin_P(:,iE) / WeightsX_q(:)
-
-      END DO
-
-      U_Fin(:,:,iFineX1,iFineX2,iFineX3) = Unpack_TwoMoment( nE, U_Fin_P )
-
-    END DO
-    END DO
-    END DO
-
-  END SUBROUTINE Refine_TwoMoment
-
-
-  FUNCTION RefineX_TwoMoment( iFine, U_Crs ) RESULT( U_Fin )
-
-    INTEGER,  INTENT(in)  :: iFine
-    REAL(DP), INTENT(in)  :: U_Crs(1:nDOFX)
-    REAL(DP)              :: U_Fin(1:nDOFX)
-
-    U_Fin = MATMUL( ProjectionMatrix(:,:,iFine), U_Crs ) / WeightsX_q
-
-  END FUNCTION RefineX_TwoMoment
-
-
-  SUBROUTINE RefineX_TwoMoment_Vector( nX, U_Crs, U_Fin )
-
-    INTEGER,  INTENT(in)  :: nX(3)
-    REAL(DP), INTENT(in)  :: U_Crs(nDOFX,nX(1),nX(2),nX(3))
-    REAL(DP), INTENT(out) :: U_Fin(nDOFX,nX(1),nX(2),nX(3),nFine)
-
-    INTEGER :: iNodeX, iX1, iX2, iX3
-    INTEGER :: nP_X, iFine
-
-    nP_X = PRODUCT( nX )
+    nP_X = PRODUCT( nX ) * nVar
 
 #if   defined( THORNADO_OMP_OL )
     !$OMP TARGET ENTER DATA
-    !$OMP MAP( to:    nX, U_Crs, ProjectionMatrix, WeightsX_Q ) &
+    !$OMP MAP( to:    U_Crs, ProjectionMatrix ) &
     !$OMP MAP( alloc: U_Fin )
 #elif defined( THORNADO_OACC   )
     !$ACC ENTER DATA &
-    !$ACC COPYIN(     nX, U_Crs, Projectionmatrix, WeightsX_Q ) &
+    !$ACC COPYIN(     U_Crs, Projectionmatrix ) &
     !$ACC CREATE(     U_Fin )
 #endif
 
-    DO iFine = 1, nFine
-      CALL MatrixMatrixMultiply &
-             ( 'N', 'N', nDOFX, nP_X, nDOFX, &
-               One, ProjectionMatrix(:,:,iFine), nDOFX,  &
-               U_Crs, nDOFX, &
-               Zero, U_Fin(:,:,:,:,iFine), nDOFX )
-    END DO
-
-#if   defined( THORNADO_OMP_OL )
-    !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO SIMD COLLAPSE(5)
-#elif defined( THORNADO_OACC   )
-    !$ACC PARALLEL LOOP GANG VECTOR COLLAPSE(5)
-#elif defined( THORNADO_OMP    )
-    !$OMP PARALLEL DO COLLAPSE(5)
-#endif
-    DO iFine = 1, nFine
-      DO iX3 = 1, nX(3)
-        DO iX2 = 1, nX(2)
-          DO iX1 = 1, nX(1)
-            DO iNodeX = 1, nDOFX
-              U_Fin(iNodeX,iX1,iX2,iX3,iFine) = U_Fin(iNodeX,iX1,iX2,iX3,iFine) / WeightsX_Q(iNodeX)
-            END DO
-          END DO
-        END DO
-      END DO
-    END DO
+    CALL MatrixMatrixMultiply &
+           ( 'N', 'N', nDOFX*nFine, nP_X, nDOFX, &
+             One, ProjectionMatrix, nDOFX*nFine,  &
+             U_Crs, nDOFX, &
+             Zero, U_Fin, nDOFX*nFine )
 
 #if   defined( THORNADO_OMP_OL )
     !$OMP TARGET EXIT DATA
     !$OMP MAP( from:    U_Fin ) &
-    !$OMP MAP( release: nX, U_Crs, ProjectionMatrix, WeightsX_Q )
+    !$OMP MAP( release: U_Crs, ProjectionMatrix )
 #elif defined( THORNADO_OACC   )
     !$ACC EXIT DATA &
     !$ACC COPYOUT(      U_Fin ) &
-    !$ACC DELETE(       nX, U_Crs, ProjectionMatrix, WeightsX_Q )
+    !$ACC DELETE(       U_Crs, ProjectionMatrix )
 #endif
 
-  END SUBROUTINE RefineX_TwoMoment_Vector
+  END SUBROUTINE RefineX_TwoMoment
 
 
-  SUBROUTINE Coarsen_TwoMoment( nE, nX, U_Crs, U_Fin )
+  SUBROUTINE CoarsenX_TwoMoment( nX, nVar, U_Fin, U_Crs )
 
-    INTEGER,  INTENT(in)  :: nE, nX(3)
-    REAL(DP), INTENT(out) :: U_Crs(1:nDOF,1:nE)
-    REAL(DP), INTENT(in)  :: U_Fin(1:nDOF,1:nE,1:nX(1),1:nX(2),1:nX(3))
+    INTEGER,  INTENT(in)  :: nX(3), nVar
+    REAL(DP), INTENT(in)  :: U_Fin(nDOFX,nFine,nX(1),nX(2),nX(3),nVar)
+    REAL(DP), INTENT(out) :: U_Crs(nDOFX,nX(1),nX(2),nX(3),nVar)
 
-    INTEGER :: iFine, iFineX1, iFineX2, iFineX3, iE
-    REAL(DP) :: U_Crs_P(nDOFX,nDOFE*nE)
-    REAL(DP) :: U_Fin_P(nDOFX,nDOFE*nE)
+    INTEGER :: nP_X
 
-    U_Crs = 0.0_DP
-
-    iFine = 0
-    DO iFineX3 = 1, nFineX(3)
-    DO iFineX2 = 1, nFineX(2)
-    DO iFineX1 = 1, nFineX(1)
-
-      iFine = iFine + 1
-
-      U_Fin_P = Pack_TwoMoment( nE, U_Fin(:,:,iFineX1,iFineX2,iFineX3) )
-
-      U_Crs_P(:,:) = MATMUL( ProjectionMatrix_T(:,:,iFine), U_Fin_P(:,:) )
-
-      DO iE = 1, nDOFE * nE
-
-        U_Crs_P(:,iE) = U_Crs_P(:,iE) / WeightsX_q(:)
-
-      END DO
-
-      U_Crs(:,:) = U_Crs(:,:) + VolumeRatio * Unpack_TwoMoment( nE, U_Crs_P )
-
-    END DO
-    END DO
-    END DO
-
-  END SUBROUTINE Coarsen_TwoMoment
-
-
-  FUNCTION CoarsenX_TwoMoment( iFine, U_Fin ) RESULT( U_Crs )
-
-    INTEGER,  INTENT(in)    :: iFine
-    REAL(DP), INTENT(in)    :: U_Fin(1:nDOFX)
-    REAL(DP)                :: U_Crs(1:nDOFX)
-
-    U_Crs = VolumeRatio * MATMUL( ProjectionMatrix_T(:,:,iFine), U_Fin ) / WeightsX_q
-
-  END FUNCTION CoarsenX_TwoMoment
-
-
-  SUBROUTINE CoarsenX_TwoMoment_Vector( nX, U_Fin, U_Crs )
-
-    INTEGER,  INTENT(in)  :: nX(3)
-    REAL(DP), INTENT(in)  :: U_Fin(nDOFX,nX(1),nX(2),nX(3),nFine)
-    REAL(DP), INTENT(out) :: U_Crs(nDOFX,nX(1),nX(2),nX(3))
-
-    INTEGER :: iNodeX, iX1, iX2, iX3
-    INTEGER :: nP_X, iFine
-
-    nP_X = PRODUCT( nX )
+    nP_X = PRODUCT( nX ) * nVar
 
 #if   defined( THORNADO_OMP_OL )
     !$OMP TARGET ENTER DATA
-    !$OMP MAP( to:    nX, U_Fin, ProjectionMatrix_T, WeightsX_Q ) &
+    !$OMP MAP( to:    U_Fin, ProjectionMatrix_T ) &
     !$OMP MAP( alloc: U_Crs )
 #elif defined( THORNADO_OACC   )
     !$ACC ENTER DATA &
-    !$ACC COPYIN(     nX, U_Fin, Projectionmatrix_T, WeightsX_Q ) &
+    !$ACC COPYIN(     U_Fin, Projectionmatrix_T ) &
     !$ACC CREATE(     U_Crs )
 #endif
 
     CALL MatrixMatrixMultiply &
-           ( 'N', 'N', nDOFX, nP_X, nDOFX, &
-             One, ProjectionMatrix_T(:,:,1), nDOFX,  &
-             U_Fin(:,:,:,:,1), nDOFX, &
+           ( 'N', 'N', nDOFX, nP_X, nDOFX*nFine, &
+             VolumeRatio, ProjectionMatrix_T, nDOFX,  &
+             U_Fin, nDOFX*nFine, &
              Zero, U_Crs, nDOFX )
-    DO iFine = 2, nFine
-      CALL MatrixMatrixMultiply &
-             ( 'N', 'N', nDOFX, nP_X, nDOFX, &
-               One, ProjectionMatrix_T(:,:,iFine), nDOFX,  &
-               U_Fin(:,:,:,:,iFine), nDOFX, &
-               One, U_Crs, nDOFX )
-    END DO
-
-#if   defined( THORNADO_OMP_OL )
-    !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO SIMD COLLAPSE(4)
-#elif defined( THORNADO_OACC   )
-    !$ACC PARALLEL LOOP GANG VECTOR COLLAPSE(4)
-#elif defined( THORNADO_OMP    )
-    !$OMP PARALLEL DO COLLAPSE(4)
-#endif
-    DO iX3 = 1, nX(3)
-      DO iX2 = 1, nX(2)
-        DO iX1 = 1, nX(1)
-          DO iNodeX = 1, nDOFX
-            U_Crs(iNodeX,iX1,iX2,iX3) = VolumeRatio * U_Crs(iNodeX,iX1,iX2,iX3) / WeightsX_Q(iNodeX)
-          END DO
-        END DO
-      END DO
-    END DO
 
 #if   defined( THORNADO_OMP_OL )
     !$OMP TARGET EXIT DATA
     !$OMP MAP( from:    U_Crs ) &
-    !$OMP MAP( release: nX, U_Fin, ProjectionMatrix_T, WeightsX_Q )
+    !$OMP MAP( release: U_Fin, ProjectionMatrix_T )
 #elif defined( THORNADO_OACC   )
     !$ACC EXIT DATA &
     !$ACC COPYOUT(      U_Crs ) &
-    !$ACC DELETE(       nX, U_Fin, ProjectionMatrix_T, WeightsX_Q )
+    !$ACC DELETE(       U_Fin, ProjectionMatrix_T )
 #endif
 
-  END SUBROUTINE CoarsenX_TwoMoment_Vector
-
-
-  FUNCTION Pack_TwoMoment( nE, U ) RESULT( U_P )
-
-    INTEGER,  INTENT(in) :: nE
-    REAL(DP), INTENT(in) :: U(nDOF,nE)
-    REAL(DP)             :: U_P(nDOFX,nDOFE*nE)
-
-    INTEGER :: iNodeX, iNodeX1, iNodeX2, iNodeX3
-    INTEGER :: iNodeE, iE, iNode
-
-    DO iNodeX3 = 1, nNodesX(3)
-    DO iNodeX2 = 1, nNodesX(2)
-    DO iNodeX1 = 1, nNodesX(1)
-
-      iNodeX = NodeNumberTableX3D(iNodeX1,iNodeX2,iNodeX3)
-
-      DO iE     = 1, nE
-      DO iNodeE = 1, nNodesE
-
-        iNode = NodeNumberTable4D(iNodeE,iNodeX1,iNodeX2,iNodeX3)
-
-        U_P(iNodeX,(iE-1)*nNodesE+iNodeE) = U(iNode,iE)
-
-      END DO
-      END DO
-
-    END DO
-    END DO
-    END DO
-
-    RETURN
-  END FUNCTION Pack_TwoMoment
-
-
-  FUNCTION Unpack_TwoMoment( nE, U_P ) RESULT( U )
-
-    INTEGER,  INTENT(in) :: nE
-    REAL(DP), INTENT(in) :: U_P(nDOFX,nDOFE*nE)
-    REAL(DP)             :: U(nDOF,nE)
-
-    INTEGER :: iNodeX, iNodeX1, iNodeX2, iNodeX3
-    INTEGER :: iNodeE, iE, iNode
-
-    DO iNodeX3 = 1, nNodesX(3)
-    DO iNodeX2 = 1, nNodesX(2)
-    DO iNodeX1 = 1, nNodesX(1)
-
-      iNodeX = NodeNumberTableX3D(iNodeX1,iNodeX2,iNodeX3)
-
-      DO iE     = 1, nE
-      DO iNodeE = 1, nNodesE
-
-        iNode = NodeNumberTable4D(iNodeE,iNodeX1,iNodeX2,iNodeX3)
-
-        U(iNode,iE) = U_P(iNodeX,(iE-1)*nNodesE+iNodeE)
-
-      END DO
-      END DO
-
-    END DO
-    END DO
-    END DO
-
-    RETURN
-  END FUNCTION Unpack_TwoMoment
+  END SUBROUTINE CoarsenX_TwoMoment
 
 
 END MODULE TwoMoment_MeshRefinementModule
