07e1394c6611dc493bb10fa1fc79c0308f40762d
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-05-25 16:27:56 -0400

    consolidated duplicate code in Euler_ErrorModule

--- Modules/Euler/Euler_ErrorModule.f90
+++ Modules/Euler/Euler_ErrorModule.f90
@@ -163,45 +163,9 @@ CONTAINS
            WRITE(*,'(2x,A)') &
             'No Root in Interval'
           WRITE(*,*)
-          WRITE(*,'(2x,A,I8.8)') &
-            'ITERATION:           ', Int_Option(1)
-          IF( Int_Option(2) .NE. 99999999 ) &
-            WRITE(*,'(2x,A,I9.8)') &
-              'iNX (Packed array): ', Int_Option(2)
-          IF( TRIM( Char_Option(1) ) .NE. 'NA' ) &
-            WRITE(*,'(2x,A,A)') &
-              'iDimX: ', TRIM( Char_Option(1) )
-          WRITE(*,'(2x,A,4I7.5)') &
-            'iX1, iX2, iX3, iNX: ', &
-             Int_Option(10), Int_Option(11), Int_Option(12), Int_Option(9)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_B0:              ', Int_Option(3), Int_Option(4), Int_Option(5)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_E0:              ', Int_Option(6), Int_Option(7), Int_Option(8)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'X1_C, X2_C, X3_C: ', &
-             Real_Option(1), Real_Option(2), Real_Option(3)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'dX1, dX2, dX3:    ', &
-             Real_Option(4), Real_Option(5), Real_Option(6)
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_D       ) = ', Real_Option(7), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S1      ) = ', Real_Option(8), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S2      ) = ', Real_Option(9), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S3      ) = ', Real_Option(10), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_E       ) = ', Real_Option(11), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_Ne      ) = ', Real_Option(12), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_11) = ', Real_Option(13), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_22) = ', Real_Option(14), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_33) = ', Real_Option(15), '_DP'
+
+          CALL WriteOutput( Int_Option, Real_Option, Char_Option )
+
           WRITE(*,'(2x,A)') TRIM( Message )
 
           CALL thornado_abort
@@ -246,45 +210,8 @@ CONTAINS
             'Reason for failure: CF_D .LT. Min_D'
           WRITE(*,*)
 
-          WRITE(*,'(2x,A,I8.8)') &
-            'ITERATION:           ', Int_Option(1)
-          IF( Int_Option(2) .NE. 99999999 ) &
-            WRITE(*,'(2x,A,I9.8)') &
-              'iNX (Packed array): ', Int_Option(2)
-          IF( TRIM( Char_Option(1) ) .NE. 'NA' ) &
-            WRITE(*,'(2x,A,A16)') &
-              'iDimX: ', TRIM( Char_Option(1) )
-          WRITE(*,'(2x,A,4I7.5)') &
-            'iX1, iX2, iX3, iNX: ', &
-             Int_Option(10), Int_Option(11), Int_Option(12), Int_Option(9)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_B0:              ', Int_Option(3), Int_Option(4), Int_Option(5)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_E0:              ', Int_Option(6), Int_Option(7), Int_Option(8)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'X1_C, X2_C, X3_C: ', &
-             Real_Option(1), Real_Option(2), Real_Option(3)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'dX1 , dX2 , dX3 : ', &
-             Real_Option(4), Real_Option(5), Real_Option(6)
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_D       ) = ', Real_Option(7), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S1      ) = ', Real_Option(8), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S2      ) = ', Real_Option(9), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S3      ) = ', Real_Option(10), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_E       ) = ', Real_Option(11), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_Ne      ) = ', Real_Option(12), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_11) = ', Real_Option(13), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_22) = ', Real_Option(14), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_33) = ', Real_Option(15), '_DP'
+          CALL WriteOutput( Int_Option, Real_Option, Char_Option )
+
           WRITE(*,'(2x,A)') TRIM( Message )
 
           CALL thornado_abort
@@ -301,45 +228,8 @@ CONTAINS
             'Reason for failure: ITERATION .EQ. MAX_IT'
           WRITE(*,*)
 
-          WRITE(*,'(2x,A,I8.8)') &
-            'ITERATION:           ', Int_Option(1)
-          IF( Int_Option(2) .NE. 99999999 ) &
-            WRITE(*,'(2x,A,I9.8)') &
-              'iNX (Packed array): ', Int_Option(2)
-          IF( TRIM( Char_Option(1) ) .NE. 'NA' ) &
-            WRITE(*,'(2x,A,A16)') &
-              'iDimX: ', TRIM( Char_Option(1) )
-          WRITE(*,'(2x,A,4I7.5)') &
-            'iX1, iX2, iX3, iNX: ', &
-             Int_Option(10), Int_Option(11), Int_Option(12), Int_Option(9)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_B0:              ', Int_Option(3), Int_Option(4), Int_Option(5)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_E0:              ', Int_Option(6), Int_Option(7), Int_Option(8)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'X1_C, X2_C, X3_C: ', &
-             Real_Option(1), Real_Option(2), Real_Option(3)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'dX1 , dX2 , dX3 : ', &
-             Real_Option(4), Real_Option(5), Real_Option(6)
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_D       ) = ', Real_Option(7), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S1      ) = ', Real_Option(8), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S2      ) = ', Real_Option(9), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S3      ) = ', Real_Option(10), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_E       ) = ', Real_Option(11), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_Ne      ) = ', Real_Option(12), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_11) = ', Real_Option(13), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_22) = ', Real_Option(14), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_33) = ', Real_Option(15), '_DP'
+          CALL WriteOutput( Int_Option, Real_Option, Char_Option )
+
           WRITE(*,'(2x,A)') TRIM( Message )
 
           CALL thornado_abort
@@ -356,45 +246,8 @@ CONTAINS
             'Reason for failure: NANS IN'
           WRITE(*,*)
 
-          WRITE(*,'(2x,A,I8.8)') &
-            'ITERATION:           ', Int_Option(1)
-          IF( Int_Option(2) .NE. 99999999 ) &
-            WRITE(*,'(2x,A,I9.8)') &
-              'iNX (Packed array): ', Int_Option(2)
-          IF( TRIM( Char_Option(1) ) .NE. 'NA' ) &
-            WRITE(*,'(2x,A,A16)') &
-              'iDimX: ', TRIM( Char_Option(1) )
-          WRITE(*,'(2x,A,4I7.5)') &
-            'iX1, iX2, iX3, iNX: ', &
-             Int_Option(10), Int_Option(11), Int_Option(12), Int_Option(9)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_B0:              ', Int_Option(3), Int_Option(4), Int_Option(5)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_E0:              ', Int_Option(6), Int_Option(7), Int_Option(8)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'X1_C, X2_C, X3_C: ', &
-             Real_Option(1), Real_Option(2), Real_Option(3)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'dX1 , dX2 , dX3 : ', &
-             Real_Option(4), Real_Option(5), Real_Option(6)
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_D       ) = ', Real_Option(7), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S1      ) = ', Real_Option(8), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S2      ) = ', Real_Option(9), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S3      ) = ', Real_Option(10), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_E       ) = ', Real_Option(11), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_Ne      ) = ', Real_Option(12), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_11) = ', Real_Option(13), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_22) = ', Real_Option(14), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_33) = ', Real_Option(15), '_DP'
+          CALL WriteOutput( Int_Option, Real_Option, Char_Option )
+
           WRITE(*,'(2x,A)') TRIM( Message )
 
           CALL thornado_abort
@@ -411,45 +264,8 @@ CONTAINS
             'Reason for failure: NANS OUT'
           WRITE(*,*)
 
-          WRITE(*,'(2x,A,I8.8)') &
-            'ITERATION:           ', Int_Option(1)
-          IF( Int_Option(2) .NE. 99999999 ) &
-            WRITE(*,'(2x,A,I9.8)') &
-              'iNX (Packed array): ', Int_Option(2)
-          IF( TRIM( Char_Option(1) ) .NE. 'NA' ) &
-            WRITE(*,'(2x,A,A16)') &
-              'iDimX: ', TRIM( Char_Option(1) )
-          WRITE(*,'(2x,A,4I7.5)') &
-            'iX1, iX2, iX3, iNX: ', &
-             Int_Option(10), Int_Option(11), Int_Option(12), Int_Option(9)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_B0:              ', Int_Option(3), Int_Option(4), Int_Option(5)
-          WRITE(*,'(2x,A,3I7.5)') &
-            'iX_E0:              ', Int_Option(6), Int_Option(7), Int_Option(8)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'X1_C, X2_C, X3_C: ', &
-             Real_Option(1), Real_Option(2), Real_Option(3)
-          WRITE(*,'(2x,A,SP3ES15.6E3)') &
-            'dX1 , dX2 , dX3 : ', &
-             Real_Option(4), Real_Option(5), Real_Option(6)
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_D       ) = ', Real_Option(7), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S1      ) = ', Real_Option(8), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S2      ) = ', Real_Option(9), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_S3      ) = ', Real_Option(10), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_E       ) = ', Real_Option(11), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'U(iCF_Ne      ) = ', Real_Option(12), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_11) = ', Real_Option(13), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_22) = ', Real_Option(14), '_DP'
-          WRITE(*,'(2x,A,SPES24.16E3,A)') &
-            'G(iGF_Gm_dd_33) = ', Real_Option(15), '_DP'
+          CALL WriteOutput( Int_Option, Real_Option, Char_Option )
+
           WRITE(*,'(2x,A)') TRIM( Message )
 
           CALL thornado_abort
@@ -464,6 +280,55 @@ CONTAINS
     END SELECT
 
   END SUBROUTINE DescribeError_Euler
+
+
+  SUBROUTINE WriteOutput( IntArray, RealArray, CharArray )
+
+    INTEGER     , INTENT(in) :: IntArray(:)
+    REAL(DP)    , INTENT(in) :: RealArray(:)
+    CHARACTER(*), INTENT(in) :: CharArray(:)
+
+    WRITE(*,'(2x,A,I8.8)') &
+      'ITERATION:           ', IntArray(1)
+    IF( IntArray(2) .NE. 99999999 ) &
+      WRITE(*,'(2x,A,I9.8)') &
+        'iNX (Packed array): ', IntArray(2)
+    IF( TRIM( CharArray(1) ) .NE. 'NA' ) &
+      WRITE(*,'(2x,A,A)') &
+        'iDimX: ', TRIM( CharArray(1) )
+    WRITE(*,'(2x,A,4I7.5)') &
+      'iX1, iX2, iX3, iNX: ', &
+       IntArray(10), IntArray(11), IntArray(12), IntArray(9)
+    WRITE(*,'(2x,A,3I7.5)') &
+      'iX_B0:              ', IntArray(3), IntArray(4), IntArray(5)
+    WRITE(*,'(2x,A,3I7.5)') &
+      'iX_E0:              ', IntArray(6), IntArray(7), IntArray(8)
+    WRITE(*,'(2x,A,SP3ES15.6E3)') &
+      'X1_C, X2_C, X3_C: ', &
+       RealArray(1), RealArray(2), RealArray(3)
+    WRITE(*,'(2x,A,SP3ES15.6E3)') &
+      'dX1, dX2, dX3:    ', &
+       RealArray(4), RealArray(5), RealArray(6)
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'U(iCF_D       ) = ', RealArray(7), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'U(iCF_S1      ) = ', RealArray(8), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'U(iCF_S2      ) = ', RealArray(9), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'U(iCF_S3      ) = ', RealArray(10), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'U(iCF_E       ) = ', RealArray(11), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'U(iCF_Ne      ) = ', RealArray(12), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'G(iGF_Gm_dd_11) = ', RealArray(13), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'G(iGF_Gm_dd_22) = ', RealArray(14), '_DP'
+    WRITE(*,'(2x,A,SPES24.16E3,A)') &
+      'G(iGF_Gm_dd_33) = ', RealArray(15), '_DP'
+
+  END SUBROUTINE WriteOutput
 
 
 END MODULE Euler_ErrorModule
