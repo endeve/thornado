c5a8aadadd1ec3df4693a16646d89c492390341a
Austin Harris <jaharris87@gmail.com>
2023-10-23 15:43:32 -0400

    get rid of always before release (redundant)

--- Modules/Opacities/OpacityModule_TABLE.F90
+++ Modules/Opacities/OpacityModule_TABLE.F90
@@ -583,7 +583,7 @@ CONTAINS
 
 #if defined(THORNADO_OMP_OL)
       !$OMP TARGET EXIT DATA &
-      !$OMP MAP( always, release: LogEs_T, LogDs_T, LogTs_T, Ys_T, LogEtas_T, &
+      !$OMP MAP( release: LogEs_T, LogDs_T, LogTs_T, Ys_T, LogEtas_T, &
       !$OMP               OS_EmAb, OS_Iso, OS_NES, OS_Pair, OS_Brem, &
       !$OMP               EmAb_T, Iso_T, NES_T, Pair_T, Brem_T, &
       !$OMP               NES_AT, Pair_AT, Brem_AT, C1, C2 )
