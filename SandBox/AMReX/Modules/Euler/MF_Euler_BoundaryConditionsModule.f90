1ff8a765bfa01eef80a9535bb48a9d70bc28b26b
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-12-05 16:35:59 -0500

    added Euler BCs for operating on multi-level multifabs

--- SandBox/AMReX/Modules/Euler/MF_Euler_BoundaryConditionsModule.f90
+++ SandBox/AMReX/Modules/Euler/MF_Euler_BoundaryConditionsModule.f90
@@ -49,6 +49,7 @@ MODULE MF_Euler_BoundaryConditionsModule
   INTERFACE ApplyBoundaryConditions_Euler_MF
     MODULE PROCEDURE ApplyBoundaryConditions_Euler_MF_MultiLevel
     MODULE PROCEDURE ApplyBoundaryConditions_Euler_MF_SingleLevel
+    MODULE PROCEDURE ApplyBoundaryConditions_Euler_MF_SingleLevel_Box
   END INTERFACE ApplyBoundaryConditions_Euler_MF
 
 CONTAINS
@@ -58,6 +59,22 @@ CONTAINS
 
     TYPE(amrex_multifab), INTENT(inout) :: MF_uCF(0:)
 
+    INTEGER :: iLevel
+
+    DO iLevel = 0, nLevels-1
+
+      CALL ApplyBoundaryConditions_Euler_MF( iLevel, MF_uCF(iLevel) )
+
+    END DO
+
+  END SUBROUTINE ApplyBoundaryConditions_Euler_MF_MultiLevel
+
+
+  SUBROUTINE ApplyBoundaryConditions_Euler_MF_SingleLevel( iLevel, MF_uCF )
+
+    INTEGER             , INTENT(in)    :: iLevel
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uCF
+
     TYPE(amrex_mfiter) :: MFI
     TYPE(amrex_box)    :: BX
     TYPE(EdgeMap)      :: Edge_Map
@@ -65,58 +82,54 @@ CONTAINS
     REAL(DP), CONTIGUOUS, POINTER :: uCF(:,:,:,:)
     REAL(DP), ALLOCATABLE         :: U  (:,:,:,:,:)
 
-    INTEGER :: iLevel, iX_B0(3), iX_E0(3), iX_B1(3), iX_E1(3), iLo_MF(4)
+    INTEGER :: iX_B0(3), iX_E0(3), iX_B1(3), iX_E1(3), iLo_MF(4)
 
-    DO iLevel = 0, nLevels-1
+    CALL CreateMesh_MF( iLevel, MeshX )
 
-      CALL CreateMesh_MF( iLevel, MeshX )
+    CALL amrex_mfiter_build( MFI, MF_uCF, tiling = UseTiling )
 
-      CALL amrex_mfiter_build( MFI, MF_uCF(iLevel), tiling = UseTiling )
+    DO WHILE( MFI % next() )
 
-      DO WHILE( MFI % next() )
+      uCF => MF_uCF % DataPtr( MFI )
 
-        uCF => MF_uCF(iLevel) % DataPtr( MFI )
+      iLo_MF = LBOUND( uCF )
 
-        iLo_MF = LBOUND( uCF )
+      BX = MFI % tilebox()
 
-        BX = MFI % tilebox()
+      iX_B0 = BX % lo
+      iX_E0 = BX % hi
+      iX_B1 = BX % lo - swX
+      iX_E1 = BX % hi + swX
 
-        iX_B0 = BX % lo
-        iX_E0 = BX % hi
-        iX_B1 = BX % lo - swX
-        iX_E1 = BX % hi + swX
+      CALL AllocateArray_X &
+             ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
+               [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nCF ], &
+               U )
 
-        CALL AllocateArray_X &
-               ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
-                 [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nCF ], &
-                 U )
+      CALL amrex2thornado_X( nCF, iX_B1, iX_E1, iLo_MF, iX_B1, iX_E1, uCF, U )
 
-        CALL amrex2thornado_X( nCF, iX_B1, iX_E1, iLo_MF, iX_B1, iX_E1, uCF, U )
+      ! --- Apply boundary conditions to physical boundaries ---
 
-        ! --- Apply boundary conditions to physical boundaries ---
+      CALL ConstructEdgeMap( iLevel, BX, Edge_Map )
 
-        CALL ConstructEdgeMap( iLevel, BX, Edge_Map )
+      CALL ApplyBoundaryConditions_Euler_MF &
+             ( iX_B0, iX_E0, iX_B1, iX_E1, U, Edge_Map )
 
-        CALL ApplyBoundaryConditions_Euler_MF &
-               ( iX_B0, iX_E0, iX_B1, iX_E1, U, Edge_Map )
+      CALL thornado2amrex_X( nCF, iX_B1, iX_E1, iLo_MF, iX_B1, iX_E1, uCF, U )
 
-        CALL thornado2amrex_X( nCF, iX_B1, iX_E1, iLo_MF, iX_B1, iX_E1, uCF, U )
-
-        CALL DeallocateArray_X &
-               ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
-                 [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nCF ], &
-                 U )
-
-      END DO
-
-      CALL amrex_mfiter_destroy( MFI )
+      CALL DeallocateArray_X &
+             ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
+               [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nCF ], &
+               U )
 
     END DO
 
-  END SUBROUTINE ApplyBoundaryConditions_Euler_MF_MultiLevel
+    CALL amrex_mfiter_destroy( MFI )
+
+  END SUBROUTINE ApplyBoundaryConditions_Euler_MF_SingleLevel
 
 
-  SUBROUTINE ApplyBoundaryConditions_Euler_MF_SingleLevel &
+  SUBROUTINE ApplyBoundaryConditions_Euler_MF_SingleLevel_Box &
     ( iX_B0, iX_E0, iX_B1, iX_E1, U, Edge_Map )
 
     INTEGER,       INTENT(in)    :: &
@@ -133,7 +146,7 @@ CONTAINS
     CALL ApplyBoundaryConditions_Euler &
            ( iX_B0, iX_E0, iX_B1, iX_E1, U, iApplyBC )
 
-  END SUBROUTINE ApplyBoundaryConditions_Euler_MF_SingleLevel
+  END SUBROUTINE ApplyBoundaryConditions_Euler_MF_SingleLevel_Box
 
 
 END MODULE MF_Euler_BoundaryConditionsModule
