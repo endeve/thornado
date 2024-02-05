ab8c11959e6b78ad809842d780c88ce2ce6e384a
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2024-02-05 13:53:59 -0500

    fix(plotting-tools): aesthetic changes

--- Workflow/AMReX/Utilities/GetPlotData.py
+++ Workflow/AMReX/Utilities/GetPlotData.py
@@ -4,8 +4,6 @@ from Utilities.GetFrameData import GetFrameData
 from Utilities.Mesh         import CreateLocations
 from Utilities.Files        import GetFileNumberArray, ChoosePlotFile
 
-
-
 #=============================================#
 #   Included Routines
 #
@@ -27,28 +25,24 @@ def GetPlotData( PlotDirectory,     \
                  argv = ['a'],      \
                  Verbose = "False"  ):
 
-    FileNumberArray = GetFileNumberArray( PlotDirectory, \
-                                          PlotBaseName   )
-                                       
+    FileNumberArray \
+      = GetFileNumberArray( PlotDirectory, PlotBaseName   )
+
     if FrameNumber == -1:
         argv = argv
     else:
         argv = ['a',str(FrameNumber)]
 
-    FileName = ChoosePlotFile( FileNumberArray,     \
-                               PlotBaseName,        \
-                               argv = argv,         \
-                               Verbose = Verbose    )
-
+    FileName \
+      = ChoosePlotFile \
+          ( FileNumberArray, \
+            PlotBaseName   , \
+            argv = argv    , \
+            Verbose = Verbose )
 
     File = PlotDirectory + FileName
 
-    Data, DataUnit,     \
-    X1_C, X2_C, X3_C,   \
-    dX1, dX2, dX3,      \
-    Time = GetFrameData( File,       \
-                        DataType,    \
-                        Field        )
-
+    Data, DataUnit, X1_C, X2_C, X3_C, dX1, dX2, dX3, Time \
+      = GetFrameData( File, DataType, Field )
 
     return Data, DataUnit, Time, X1_C, X2_C, X3_C, dX1, dX2, dX3
