70080afc05eef3081700c5999e5d596cb02ea0b9
Nick Roberts <jrober50@vols.utk.edu>
2024-02-14 11:33:35 -0500

    Updating plotting routines from InterfaceWithPoseidon.

---
+++ Workflow/AMReX/Utilities/DecadeFinder.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os.path as os
+import GlobalVariables.Units    as gvU
+import GlobalVariables.Settings as gvS
+
+from Utilities.Files        import GetFileNumberArray
+from Utilities.GetFrameData import GetFrameData, GetCentralDensity
+from Utilities.Mesh         import CreateLocations
+
+import Utilities.BounceFinder   as BF
+
+
+global DecadeList
+global DecadeTimeList
+global DecadeFrameList
+
+DecadeList = 0.0
+DecadeTimeList = 0.0
+DecadeFrameList = 0
+
+
+ #=============================================#
+#                                               #
+#   CreateDecadeData                            #
+#                                               #
+ #=============================================#
+def CreateDecadeData(   PlotDirectories,    \
+                        PlotBaseName,       \
+                        DataDirectories,    \
+                        DataType            ):
+    
+    global DecadeList
+    global DecadeFrameList
+    global DecadeTimeList
+    
+                        
+    print( '\n  Creating Density Decade Data' )
+    print( '--------------------------------' )
+    DecadeList      = [0.0]*gvS.nDirs
+    DecadeTimeList  = [0.0]*gvS.nDirs
+    DecadeFrameList = [0]*gvS.nDirs
+    for i in range(gvS.nDirs):
+        Decade, DecadeFrame, DecadeTime = FindDecades(  PlotDirectories[i],    \
+                                                        PlotBaseName,          \
+                                                        DataDirectories[i],    \
+                                                        DataType[i]            )
+
+        DecadeList[i]       = Decade
+        DecadeFrameList[i]  = np.array(DecadeFrame)
+        DecadeTimeList[i]   = DecadeTime
+
+
+    return DecadeList, DecadeFrameList, DecadeTimeList
+                        
+                        
+ #=============================================#
+#                                               #
+#   FindDecades                                 #
+#                                               #
+ #=============================================#
+def FindDecades( PlotDirectory,      \
+                 PlotBaseName,       \
+                 PathDataDirectory,  \
+                 DataType        ):
+ 
+
+    DecadeFile = PathDataDirectory + '/' + '{:}.dat'.format( 'Decades' )
+    Check = os.isfile(DecadeFile)
+    
+    if Check:
+    
+        print("Reading Decade Information")
+        DecadeData = np.loadtxt(DecadeFile)
+
+        Decades     = []
+        DecadeFrames= []
+        DecadeTimes = []
+        
+        nDecades = len(DecadeData)
+        for i in range(nDecades):
+            Decades.append(int(DecadeData[i][0]))
+            DecadeFrames.append(int(DecadeData[i][1]))
+            DecadeTimes.append(DecadeData[i][2])
+        
+    
+    else:
+        print("Calculating Decade Information")
+        Decades, DecadeFrames, DecadeTimes = FindDecadeMethod( PlotDirectory,      \
+                                                               PlotBaseName,       \
+                                                               PathDataDirectory,  \
+                                                               DataType,           \
+                                                               DecadeFile          )
+
+#    print(Decades)
+#    print(DecadeFrames)
+#    print(DecadeTimes)
+#    exit()
+    return Decades, DecadeFrames, DecadeTimes
+
+
+
+
+
+
+
+
+ #=============================================#
+#                                               #
+#   FindDecadeMethod                            #
+#                                               #
+ #=============================================#
+def FindDecadeMethod( PlotDirectory,      \
+                      PlotBaseName,       \
+                      PathDataDirectory,  \
+                      DataType,           \
+                      DecadeFile          ):
+
+    FileNumberArray = GetFileNumberArray( PlotDirectory,    \
+                                          PlotBaseName,     \
+                                          -1, -1, 1         )
+
+    NumPltFiles = len(FileNumberArray)
+
+    
+    DecadeLimit = 14
+    MaxDecade = -1
+    Decades = []
+    DecadeFrames = []
+    DecadeTimes = []
+    for i in range(NumPltFiles):
+
+        print( '\r {:}/{:}'.format( i+1, NumPltFiles ), end = '\r' )
+
+        PlotFileNumber    = FileNumberArray[i]
+        if DataType.lower() == 'amrex':
+            PathPlotDirectory = PlotDirectory       \
+                              + PlotBaseName    \
+                              + '{:}'.format( str(PlotFileNumber).zfill(8) )
+        else:
+            PathPlotDirectory = PlotDirectory       \
+                              + PlotBaseName[:-4]   \
+                              + '_{:}'.format( str(PlotFileNumber).zfill(6) )
+
+
+                            
+                            
+        Density, DensityUnits, Time = GetCentralDensity( PathPlotDirectory,  \
+                                                         DataType            )
+            
+        CurDecade = int(np.floor(np.log10(Density)))
+          
+
+        if CurDecade > MaxDecade:
+            MaxDecade = CurDecade
+            Decades.append(MaxDecade)
+            DecadeFrames.append(PlotFileNumber)
+            DecadeTimes.append(Time.tolist())
+
+        if CurDecade >= DecadeLimit: break
+        
+
+
+
+    
+    
+    
+    with open( DecadeFile, 'w' ) as FileOut:
+
+        FileOut.write( '# {:}\n'.format( DecadeFile  ) )
+        FileOut.write( '# Decade frame_Decade t_Decade[{:}] \n'.format( gvU.TimeUnits),  )
+
+        DecadeLen = len(Decades)
+        for i in range(DecadeLen):
+            FileOut.write(str(Decades[i])+' '+str(DecadeFrames[i]) + ' ' + str(DecadeTimes[i]) + '\n')
+
+
+    return Decades, DecadeFrames, DecadeTimes
+
+
+
+
+ #=============================================#
+#                                               #
+#   FindDecadeMethod                            #
+#                                               #
+ #=============================================#
+def FindDecadeMethod_Bisect( PlotDirectory,      \
+                             PlotBaseName,       \
+                             PathDataDirectory,  \
+                             DataType,           \
+                             DecadeFile          ):
+
+    FileNumberArray = GetFileNumberArray( PlotDirectory,    \
+                                          PlotBaseName,     \
+                                          -1, -1, 1         )
+
+    NumPltFiles = len(FileNumberArray)
+
+    # GetFrameData needs inputs for these values.
+    # But, they aren't used when fetching density.
+    # So we create theses phonies.
+    X1 = np.array([0.0])
+    X2 = np.array([0.0])
+    X3 = np.array([0.0])
+    dX1 = np.array([0.0])
+    dX2 = np.array([0.0])
+    dX3 = np.array([0.0])
+
+    Decade = 0.0
+    for i in range(NumPltFiles):
+
+        print( '\r {:}/{:}'.format( i, NumPltFiles ), end = '\r' )
+
+        PlotFileNumber    = FileNumberArray[i]
+        if DataType.lower() == 'amrex':
+            PathPlotDirectory = PlotDirectory       \
+                              + PlotBaseName    \
+                              + '{:}'.format( str(PlotFileNumber).zfill(8) )
+        else:
+            PathPlotDirectory = PlotDirectory       \
+                              + PlotBaseName[:-4]   \
+                              + '_{:}'.format( str(PlotFileNumber).zfill(6) )
+
+#        X1, X2, X3, dX1, dX2, dX3, xL, xH               \
+#            = CreateLocations(  PathPlotDirectory,      \
+#                                TypeIn = "Leaf"      )
+
+
+        Density, DensityUnits, \
+        X1, X2, X3,         \
+        dX1, dX2, dX3,      \
+        Time                \
+            = GetFrameData( PathPlotDirectory,  \
+                            DataType,           \
+                            'PF_D',             \
+                            'True'              )
+
+
+        CurMaxDensity = max(Density)
+
+        if CurMaxDensity > Decade:
+            Decade = CurMaxDensity
+            DecadeFrame   = i
+            DecadeTime    = Time
+
+
+    
+    with open( DecadeFile, 'w' ) as FileOut:
+
+        FileOut.write( '# {:}\n'.format( DecadeFile  ) )
+        FileOut.write( '# frame_Decade t_Decade [{:}] Central Density at Decade [{:}]\n'.format( gvU.TimeUnits, DensityUnits ),  )
+
+        
+        FileOut.write( str(DecadeFrame ) + ' ')
+        FileOut.write( str(DecadeTime  ) + ' ')
+        FileOut.write( str(Decade) + ' ')
+        FileOut.write( '\n' )
+
+    return DecadeFrame, DecadeTime, Decade
+
--- Workflow/AMReX/plotFieldsAMReX2D.py
+++ Workflow/AMReX/plotFieldsAMReX2D.py
@@ -1,20 +1,129 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import yt
 
+from Utilities.Files import GetFileNumberArray
+
+### Beginning of user input ###
+
 # Specify name of problem
-ProblemName = 'KelvinHelmholtz2D'
+ProblemName = 'RiemannProblem2D_dZB2002'
 
 # Specify directory containing amrex Plotfiles
 #PlotDirectory = '/Users/nickroberts/thornado/SandBox/AMReX/Applications/YahilCollapse_XCFC/'
 PlotDirectory = '/home/kkadoogan/Work/Codes/thornado/SandBox/AMReX/dgExperiments_Euler_Relativistic_IDEAL/'
 
+FigName = 'fig.png'
+
 # Specify plot file base name
 PlotBaseName = ProblemName + '.plt'
+
+FileNumberArray \
+  = GetFileNumberArray \
+      ( PlotDirectory, \
+        PlotBaseName, \
+        -1, -1, \
+        1 )
+
+# For a single file, replace `FileNumberArray[-1]` with cycle number;
+# e.g., plt00001310 --> 1310
+ds = yt.load( PlotDirectory + PlotBaseName + str( FileNumberArray[-1] ).zfill( 8 ) )
+
+# Get lower and higher boundaries and convert them to numpy arrays
+xL = ds.domain_left_edge.to_ndarray()
+xH = ds.domain_right_edge.to_ndarray()
+
+# Units for axes (dimensionless -> code_length)
+LengthUnitX = [ 'code_length', 'code_length' ]
+
+# Set xlabel and ylabel
+xLabel = r'$x$'
+yLabel = r'$y$'
+
+PlotTitle = 'dZB2002'
+
+# Set center and width of plot window
+center = 0.5 * ( xL + xH )
+width  = 1.0 * ( xH - xL )
+
+# Zoom in?
+Zoom = 1.0
 
 # Specify field to plot
 Field = 'PF_D'
 
-ds = yt.load( PlotDirectory + PlotBaseName + '00000000' )
+# Colormap for plot
+cmap = 'viridis'
 
-yt.SlicePlot( ds, "z", ("boxlib",Field) ).save()
+# Label for colorbar (z-axis)
+zLabel = r'$\rho$'
+
+# Use custom limits for colorbar
+UseCustomZmin = False ; zmin = 0.01
+UseCustomZmax = False ; zmax = 5.0e1
+
+# Use log scale for z-axis
+UseLogScaleZ = False
+
+# Include minor tickmarks (x,Y)
+ShowMinorTicksXY = True
+
+# Include minor tickmarks (Z)
+ShowMinorTicksZ = True
+
+# Show mesh
+ShowMesh  = True
+MeshAlpha = 0.5
+
+# Overplot contours
+OverplotContours = False
+nLevels = 5
+
+### End of user input ###
+
+bl = 'boxlib'
+blField = (bl,Field)
+
+slc \
+  = yt.SlicePlot \
+      ( ds, 'z', blField, \
+        origin = 'native', \
+        center = center, \
+        width = width )
+
+slc.set_axes_unit( LengthUnitX )
+
+#slc.annotate_grids()
+if ( ShowMesh ) : slc.annotate_cell_edges \
+                    ( line_width = 1.0e-12, alpha = MeshAlpha, color = 'black' )
+
+slc.set_cmap( blField, cmap )
+
+slc.set_colorbar_label( Field, zLabel )
+
+if ( not UseCustomZmin ) : zmin = 'min'
+if ( not UseCustomZmax ) : zmax = 'max'
+
+slc.set_zlim( Field, zmin = zmin, zmax = zmax )
+
+if ( OverplotContours ) :
+    slc.annotate_contour \
+      ( blField, levels = nLevels, clim = (zmin,zmax) )
+
+slc.set_log( Field, log = UseLogScaleZ )
+
+slc.set_minorticks         ( Field, ShowMinorTicksXY )
+slc.set_colorbar_minorticks( Field, ShowMinorTicksZ  )
+
+slc.annotate_title( PlotTitle )
+
+slc.set_xlabel( xLabel )
+slc.set_ylabel( yLabel )
+
+slc.zoom( Zoom )
+
+try:
+    slc.save( FigName )
+except:
+    slc.save()
