1b3a9d85261d5482c71d03e2743dc33418ef9bd4
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2024-02-21 13:36:11 -0500

    feat(plotting): plot spherical 2D amrex data

--- Workflow/AMReX/plotFieldsAMReX2D.py
+++ Workflow/AMReX/plotFieldsAMReX2D.py
@@ -3,18 +3,25 @@
 import yt
 
 # Specify name of problem
-ProblemName = 'KelvinHelmholtz2D'
+ProblemName = 'YahilCollapse_XCFC'
+
+coordinateSystem = 'spherical'
 
 # Specify directory containing amrex Plotfiles
-#PlotDirectory = '/Users/nickroberts/thornado/SandBox/AMReX/Applications/YahilCollapse_XCFC/'
-PlotDirectory = '/home/kkadoogan/Work/Codes/thornado/SandBox/AMReX/dgExperiments_Euler_Relativistic_IDEAL/'
+PlotDirectory = '/Users/nickroberts/thornado/SandBox/AMReX/Applications/YahilCollapse_XCFC/'
+#PlotDirectory = '/home/kkadoogan/Work/Codes/thornado/SandBox/AMReX/Applications/YahilCollapse_XCFC/'
 
 # Specify plot file base name
 PlotBaseName = ProblemName + '.plt'
 
 # Specify field to plot
-Field = 'PF_D'
+Field = 'PF_V2'
 
 ds = yt.load( PlotDirectory + PlotBaseName + '00000000' )
 
-yt.SlicePlot( ds, "z", ("boxlib",Field) ).save()
+if ( coordinateSystem == 'spherical' ) :
+    axis = 'phi'
+else:
+    axis = 'z'
+
+yt.SlicePlot( ds, axis, ('boxlib',Field) ).save()
