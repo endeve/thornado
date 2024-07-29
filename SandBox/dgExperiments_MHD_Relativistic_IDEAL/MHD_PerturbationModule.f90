35d643d8145483ceefb2666c562c66eb552b243f
jbuffal <jbuffal1@utk.edu>
2024-07-28 21:59:48 -0500

    Start reincorporating perturbation module.

---
+++ SandBox/dgExperiments_MHD_Relativistic_IDEAL/MHD_PerturbationModule.f90
@@ -0,0 +1,203 @@
+MODULE MHD_PerturbationModule
+
+  USE KindModule, ONLY: &
+    DP, &
+    Zero, &
+    Half, &
+    One, &
+    Pi, &
+    FourPi
+  USE UnitsModule, ONLY: &
+    Gram, &
+    Centimeter, &
+    Kilometer
+  USE ProgramHeaderModule, ONLY: &
+    nDOFX
+  USE ReferenceElementModuleX, ONLY: &
+    NodeNumberTableX
+  USE MeshModule, ONLY: &
+    MeshX, &
+    NodeCoordinate
+  USE GeometryFieldsModule, ONLY: &
+    nGF, &
+    iGF_h_1, &
+    iGF_h_2, &
+    iGF_h_3, &
+    iGF_Gm_dd_11, &
+    iGF_Gm_dd_22, &
+    iGF_Gm_dd_33, &
+    iGF_SqrtGm, &
+    iGF_Alpha, &
+    iGF_Beta_1, &
+    iGF_Beta_2, &
+    iGF_Beta_3
+  USE MagnetofluidFieldsModule, ONLY: &
+    nCM, &
+    iCM_D, &
+    iCM_S1, &
+    iCM_S2, &
+    iCM_S3, &
+    iCM_E, &
+    iCM_Ne, &
+    iCM_B1, &
+    iCM_B2, &
+    iCM_B3, &
+    iCM_Chi, &
+    nPM, &
+    iPM_D, &
+    iPM_V1, &
+    iPM_V2, &
+    iPM_V3, &
+    iPM_E, &
+    iPM_Ne, &
+    iPM_B1, &
+    iPM_B2, &
+    iPM_B3, &
+    iPM_Chi, &
+    nAM, &
+    iAM_P, &
+    iAM_T, &
+    iAM_Ye, &
+    iAM_S, &
+    iAM_E, &
+    iAM_Gm, &
+    iAM_Cs, &
+    nDM, &
+    iDM_Sh_X1, &
+    iDM_Sh_X2, &
+    iDM_Sh_X3, &
+    iDM_Div
+   USE MHD_UtilitiesModule_Relativistic, ONLY: &
+    ComputeConserved_MHD_Relativistic, &
+    ComputeFromConserved_MHD_Relativistic
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: InitializeRandPerturbations, &
+            ApplyRandPerturbations, &
+            FinalizeRandPerturbations
+
+  CHARACTER(LEN=32), PUBLIC :: PerturbType
+
+  LOGICAL,  PUBLIC :: UseUnits
+  INTEGER,  PUBLIC :: Rand_Points
+  INTEGER,  PUBLIC, ALLOCATABLE :: Rand_X1(:), Rand_X2(:), Rand_X3(:)
+  INTEGER,  PUBLIC, ALLOCATABLE :: Rand_NodeX(:)
+  REAL(DP), PUBLIC :: Rand_Amplitude
+
+CONTAINS
+
+
+  SUBROUTINE InitializeRandPerturbations &
+               ( Size_X1, Size_X2, Size_X3, Size_NodeX, &
+                 Rand_Points_Option, Rand_Amplitude_Option )
+
+    INTEGER,  INTENT(in) :: Size_X1, Size_X2, Size_X3
+    INTEGER,  INTENT(in) :: Size_NodeX
+    INTEGER,  INTENT(in), OPTIONAL :: Rand_Points_Option
+    REAL(DP), INTENT(in), OPTIONAL :: Rand_Amplitude_Option
+
+    INTEGER  :: Size1, Size2, Size3, SizeN
+    INTEGER  :: iPoint
+    REAL(DP) :: Rand1, Rand2, Rand3
+    REAL(DP) :: RandN
+
+    IF( PRESENT( Rand_Points_Option ) ) THEN
+      Rand_Points = Rand_Points_Option
+    ELSE
+      Rand_Points = 1
+    END IF
+
+    DO iPoint = 1, Rand_Points
+
+      CALL RANDOM_SEED  ( SIZE = Size1 )
+      CALL RANDOM_NUMBER( Rand1 )
+
+      CALL RANDOM_SEED  ( SIZE = Size2 )
+      CALL RANDOM_NUMBER( Rand2 )
+
+      CALL RANDOM_SEED  ( SIZE = Size3 )
+      CALL RANDOM_NUMBER( Rand3 )
+
+      CALL RANDOM_SEED  ( SIZE = SizeN )
+      CALL RANDOM_NUMBER( RandN )
+
+      Rand_X1(iPoint) = CEILING( Rand1 * ( Size_X1 ) )
+      Rand_X2(iPoint) = CEILING( Rand2 * ( Size_X2 ) )
+      Rand_X3(iPoint) = CEILING( Rand3 * ( Size_X3 ) )
+
+    END DO
+
+  END SUBROUTINE InitializeRandPerturbations
+
+
+  SUBROUTINE ApplyRandPerturbations( iX_B0, iX_E0, iX_B1, iX_E1, G, U )
+
+    ! --- Still in progress. ---
+
+    INTEGER, INTENT(in) :: &
+      iX_B0(3), iX_E0(3), iX_B1(3), iX_E1(3)
+    REAL(DP), INTENT(in) :: &
+      G(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:)
+    REAL(DP), INTENT(inout) :: &
+      U(1:,iX_B1(1):,iX_B1(2):,iX_B1(3):,1:)
+
+    INTEGER :: iX1, iX2, iX3
+
+    REAL(DP) :: &
+      P(1:nDOFX,iX_B1(1):iX_E1(1),iX_B1(2):iX_E1(2),iX_B1(3):iX_E1(3),1:nPM)
+    REAL(DP) :: &
+      A(1:nDOFX,iX_B1(1):iX_E1(1),iX_B1(2):iX_E1(2),iX_B1(3):iX_E1(3),1:nAM)
+
+    ! --- Applying the random radial velocity      ---
+    ! --- perturbations from Muller & Janka (2017) ---
+
+    CALL ComputeFromConserved_MHD_Relativistic &
+           ( iX_B0, iX_E0, iX_B1, iX_E1, G, U, P, A )
+
+    P(Rand_NodeX(:),Rand_X1(:),Rand_X2(:),Rand_X3(:),iPM_V1) &
+      = ( One + Rand_Amplitude ) &
+        * P(Rand_NodeX(:),Rand_X1(:),Rand_X2(:),Rand_X3(:),iPM_V1)
+
+    DO iX3 = iX_B0(3), iX_E0(3)
+    DO iX2 = iX_B0(2), iX_E0(2)
+    DO iX1 = iX_B0(1), iX_E0(1)
+
+     CALL ComputeConserved_MHD_Relativistic &
+             ( P(:,iX1,iX2,iX3,iPM_D ), P(:,iX1,iX2,iX3,iPM_V1 ), &
+               P(:,iX1,iX2,iX3,iPM_V2), P(:,iX1,iX2,iX3,iPM_V3 ), &
+               P(:,iX1,iX2,iX3,iPM_E ), P(:,iX1,iX2,iX3,iPM_Ne ), &
+               P(:,iX1,iX2,iX3,iPM_B1), P(:,iX1,iX2,iX3,iPM_B2 ), &
+               P(:,iX1,iX2,iX3,iPM_B3), P(:,iX1,iX2,iX3,iPM_Chi), &
+               U(:,iX1,iX2,iX3,iCM_D ), U(:,iX1,iX2,iX3,iCM_S1 ), &
+               U(:,iX1,iX2,iX3,iCM_S2), U(:,iX1,iX2,iX3,iCM_S3 ), &
+               U(:,iX1,iX2,iX3,iCM_E ), U(:,iX1,iX2,iX3,iCM_Ne ), &
+               U(:,iX1,iX2,iX3,iCM_B1), U(:,iX1,iX2,iX3,iCM_B2 ), &
+               U(:,iX1,iX2,iX3,iCM_B3), U(:,iX1,iX2,iX3,iCM_B3 ), &
+               G(:,iX1,iX2,iX3,iGF_Gm_dd_11), &
+               G(:,iX1,iX2,iX3,iGF_Gm_dd_22), &
+               G(:,iX1,iX2,iX3,iGF_Gm_dd_33), &
+               G(:,iX1,iX2,iX3,iGF_Alpha   ), &
+               G(:,iX1,iX2,iX3,iGF_Beta_1  ), &
+               G(:,iX1,iX2,iX3,iGF_Beta_1  ), &
+               G(:,iX1,iX2,iX3,iGF_Beta_1  ), &
+               EvolveOnlyMagnetic )
+
+    END DO
+    END DO
+    END DO
+
+  END SUBROUTINE ApplyRandPerturbations
+
+
+  SUBROUTINE FinalizeRandPerturbations
+
+    DEALLOCATE( Rand_X1 )
+    DEALLOCATE( Rand_X2 )
+    DEALLOCATE( Rand_X3 )
+
+  END SUBROUTINE FinalizeRandPerturbations
+
+
+END MODULE MHD_PerturbationModule
