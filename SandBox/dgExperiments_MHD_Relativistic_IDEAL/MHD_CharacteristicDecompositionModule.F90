b865ad1c5e53c859749047b47876d44955aabf96
jbuffal <jbuffal1@utk.edu>
2024-08-06 14:00:12 -0500

    Start work on MHD characteristic decomposition.

---
+++ SandBox/dgExperiments_MHD_Relativistic_IDEAL/MHD_CharacteristicDecompositionModule.F90
@@ -0,0 +1,45 @@
+MODULE MHD_CharacteristicDecompositionModule
+
+  USE KindModule, ONLY: &
+    DP
+  USE MagnetofluidFieldsModule, ONLY: &
+    nCM
+  USE GeometryFieldsModule, ONLY: &
+    nGF
+
+#ifdef HYDRO_RELATIVISTIC
+
+!  USE MHD_CharacteristicDecompositionModule_Relativistic_IDEAL
+
+#endif
+
+
+  IMPLICIT NONE
+  PRIVATE
+
+  PUBLIC :: ComputeCharacteristicDecomposition_MHD
+
+
+CONTAINS
+
+
+  SUBROUTINE ComputeCharacteristicDecomposition_MHD &
+    ( iDim, G, U, R, invR )
+
+    INTEGER,  INTENT(in)    :: iDim
+    REAL(DP), INTENT(in)    :: G(nGF)
+    REAL(DP), INTENT(inout) :: U(nCM)
+    REAL(DP), INTENT(out)   :: R(nCM,nCM)
+    REAL(DP), INTENT(out)   :: invR(nCM,nCM)
+
+#ifdef HYDRO_RELATIVISTIC
+
+!    CALL ComputeCharacteristicDecomposition_MHD_Relativistic_IDEAL &
+!           ( iDim, G, U, R, invR )
+
+#endif
+
+  END SUBROUTINE ComputeCharacteristicDecomposition_MHD
+
+
+END MODULE MHD_CharacteristicDecompositionModule
