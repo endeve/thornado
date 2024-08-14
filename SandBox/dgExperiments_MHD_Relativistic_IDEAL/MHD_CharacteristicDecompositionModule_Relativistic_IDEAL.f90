80fe7a2487b881eff0c56747f6cd9b3b70e08f6a
jbuffal <jbuffal1@utk.edu>
2024-08-13 21:02:38 -0400

    Fixes to PrimitiveConservedJacobian in MHD relativistic characteristic decomp. module.

--- SandBox/dgExperiments_MHD_Relativistic_IDEAL/MHD_CharacteristicDecompositionModule_Relativistic_IDEAL.f90
+++ SandBox/dgExperiments_MHD_Relativistic_IDEAL/MHD_CharacteristicDecompositionModule_Relativistic_IDEAL.f90
@@ -134,11 +134,13 @@ CONTAINS
               + Gmdd22 * Shift2 * bu2 &
               + Gmdd33 * Shift3 * bu3 )
 
+    Ye = Ne / D
+
     CALL ComputePrimitiveConservedJacobian &
            ( D, Vu1, Vu2, Vu3, Vd1, Vd2, Vd3, VSq, &
              bu0, bu1, bu2, bu3, bd1, bd2, bd3,    &
              Gmdd11, Gmdd22, Gmdd33,               &
-             W, h, hStar, dUdV )
+             W, h, hStar, Ye, dUdV )
 
     SELECT CASE( iDim )
       CASE(1)
@@ -233,13 +235,13 @@ CONTAINS
     ( D, Vu1, Vu2, Vu3, Vd1, Vd2, Vd3, VSq,   &
       bu0, bu1, bu2, bu3, bd1, bd2, bd3,      &
       Gmdd11, Gmdd22, Gmdd33,                 &
-      W, h, hStar, dUdV )
+      W, h, hStar, Ye, dUdV )
 
     REAL(DP), INTENT(in)  :: D
     REAL(DP), INTENT(in)  :: Vu1, Vu2, Vu3, Vd1, Vd2, Vd3, VSq
     REAL(DP), INTENT(in)  :: bu0, bu1, bu2, bu3, bd1, bd2, bd3
     REAL(DP), INTENT(in)  :: Gmdd11, Gmdd22, Gmdd33
-    REAL(DP), INTENT(in)  :: W, h, hStar
+    REAL(DP), INTENT(in)  :: W, h, hStar, Ye
     REAL(DP), INTENT(out) :: dUdV(nCM,nCM)
 
     ! --- Upper 1st Index, Lower 2nd Index ---
@@ -342,132 +344,132 @@ CONTAINS
     ! --- Temp ---
 
     dUdV(1,:) &
-      = [ W * Vu1,                      &
-          D * W**3 * Vd1 * Vu1 + D * W, &
-          D * W**3 * Vd2 * Vu1,         &
-          D * W**3 * Vd3 * Vu1,         &
-          Zero,                         &
-          Zero,                         &
-          Zero,                         &
-          Zero,                         &
-          Zero,                         &
+      = [ W,              &
+          D * W**3 * Vd1, &
+          D * W**3 * Vd2, &
+          D * W**3 * Vd3, &
+          Zero,           &
+          Zero,           &
+          Zero,           &
+          Zero,           &
+          Zero,           &
           Zero ]
 
     dUdV(2,:) &
-      = [ W**2 * v1v1,                                                  &
-          -b0b1 * ( del11 + Two * W**2 * v1v1 ) &
-          + Z * ( Two * W**2 * vd1 * v1v1 + del11 * vd1 + gm11 * vu1 ), &
-          -b0b2 * ( del11 + Two * W**2 * v1v1 ) &
-          + Z * ( Two * W**2 * vd2 * v1v1 + del12 * vd1 + gm12 * vu1 ), &
-          -b0b3 * ( del11 + Two * W**2 * v1v1 ) &
-          + Z * ( Two * W**2 * vd3 * v1v1 + del13 * vd1 + gm13 * vu1 ), &
-          del11 + G * W**2 * v1v1,                                      &
-          Zero,                                                         &
-          ( -b0v1 + bd1 ) * ( del11 + Two * W**2 * v1v1 ) &
-          - bd1 * del11 - gm11 * bu1,                                   &
-          ( -b0v2 + bd2 ) * ( del11 + Two * W**2 * v1v1 ) &
-          - bd1 * del12 - gm12 * bu1,                                   &
-          ( -b0v3 + bd3 ) * ( del11 + Two * W**2 * v1v1 ) &
-          - bd1 * del13 - gm13 * bu1,                                   &
+      = [ W**2 * vd1,                                          &
+          - Two * b0b1 * W**2 * vd1 &
+          + Z * ( Two * W**2 * vd1 * vd1 + gm11 ) - bd1 * bd1, &
+          - Two * b0b2 * W**2 * vd1 &
+          + Z * ( Two * W**2 * vd2 * vd1 + gm12 ) - bd2 * bd1, &
+          - Two * b0b3 * W**2 * vd1 &
+          + Z * ( Two * W**2 * vd3 * vd1 + gm13 ) - bd3 * bd1, &
+          G * W**2 * vd1,                                      &
+          Zero,                                                &
+          ( - b0v1 + bd1 ) * ( Two * W**2 * vd1 ) &
+          - bd1 * vd1 - gm11 * bu0,                            &
+          ( - b0v2 + bd2 ) * ( Two * W**2 * vd1 ) &
+          - bd1 * vd2 - gm12 * bu0,                            &
+          ( - b0v3 + bd3 ) * ( Two * W**2 * vd1 ) &
+          - bd1 * vd3 - gm13 * bu0,                            &
           Zero ]
 
     dUdV(3,:) &
-      = [ W**2 * v1v2,                                                  &
-          -b0b1 * ( del12 + Two * W**2 * v1v2 ) &
-          + Z * ( Two * W**2 * vd1 * v1v2 + del11 * vd2 + gm21 * vu1 ), &
-          -b0b2 * ( del12 + Two * W**2 * v1v2 ) &
-          + Z * ( Two * W**2 * vd2 * v1v2 + del12 * vd2 + gm22 * vu1 ), &
-          -b0b3 * ( del12 + Two * W**2 * v1v2 ) &
-          + Z * ( Two * W**2 * vd3 * v1v2 + del13 * vd2 + gm23 * vu1 ), &
-          del12 + G * W**2 * v1v2,                                      &
-          Zero,                                                         &
-          ( -b0v1 + bd1 ) * ( del12 + Two * W**2 * v1v2 ) &
-          - bd2 * del11 - gm21 * bu1,                                   &
-          ( -b0v2 + bd2 ) * ( del12 + Two * W**2 * v1v2 ) &
-          - bd2 * del12 - gm22 * bu1,                                   &
-          ( -b0v3 + bd3 ) * ( del12 + Two * W**2 * v1v2 ) &
-          - bd2 * del13 - gm23 * bu1,                                   &
+      = [ W**2 * vd2,                                          &
+          - Two * b0b1 * W**2 * vd2 &
+          + Z * ( Two * W**2 * vd1 * vd2 + gm21 ) - bd1 * bd2, &
+          - Two * b0b2 * W**2 * vd2 &
+          + Z * ( Two * W**2 * vd2 * vd2 + gm22 ) - bd2 * bd2, &
+          - Two * b0b3 * W**2 * vd2 &
+          + Z * ( Two * W**2 * vd3 * vd2 + gm23 ) - bd3 * bd2, &
+          G * W**2 * vd2,                                      &
+          Zero,                                                &
+          ( - b0v1 + bd1 ) * ( Two * W**2 * vd2 ) &
+          - bd2 * vd1 - gm21 * bu0,                            &
+          ( - b0v2 + bd2 ) * ( Two * W**2 * vd2 ) &
+          - bd2 * vd2 - gm22 * bu0,                            &
+          ( - b0v3 + bd3 ) * ( Two * W**2 * vd2 ) &
+          - bd2 * vd3 - gm23 * bu0,                            &
           Zero ]
 
     dUdV(4,:) &
-      = [ W**2 * v1v1,                                                  &
-          -b0b1 * ( del13 + Two * W**2 * v1v3 ) &
-          + Z * ( Two * W**2 * vd1 * v1v3 + del11 * vd3 + gm31 * vu1 ), &
-          -b0b2 * ( del13 + Two * W**2 * v1v3 ) &
-          + Z * ( Two * W**2 * vd2 * v1v3 + del12 * vd3 + gm32 * vu1 ), &
-          -b0b3 * ( del13 + Two * W**2 * v1v3 ) &
-          + Z * ( Two * W**2 * vd3 * v1v3 + del13 * vd3 + gm33 * vu1 ), &
-          del13 + G * W**2 * v1v3,                                      &
-          Zero,                                                         &
-          ( -b0v1 + bd1 ) * ( del13 + Two * W**2 * v1v3 ) &
-          - bd3 * del11 - gm31 * bu1,                                   &
-          ( -b0v2 + bd2 ) * ( del13 + Two * W**2 * v1v3 ) &
-          - bd3 * del12 - gm32 * bu1,                                   &
-          ( -b0v3 + bd3 ) * ( del13 + Two * W**2 * v1v3 ) &
-          - bd3 * del13 - gm33 * bu1,                                   &
+      = [ W**2 * vd3,                                          &
+          - Two * b0b1 * W**2 * vd3 &
+          + Z * ( Two * W**2 * vd1 * vd3 + gm31 ) - bd1 * bd3, &
+          - Two * b0b2 * W**2 * vd3 &
+          + Z * ( Two * W**2 * vd2 * vd3 + gm32 ) - bd2 * bd3, &
+          - Two * b0b3 * W**2 * vd3 &
+          + Z * ( Two * W**2 * vd3 * vd3 + gm33 ) - bd3 * bd3, &
+          G * W**2 * vd3,                                      &
+          Zero,                                                &
+          ( - b0v1 + bd1 ) * ( Two * W**2 * vd3 ) &
+          - bd3 * vd1 - gm31 * bu0,                            &
+          ( - b0v2 + bd2 ) * ( Two * W**2 * vd3 ) &
+          - bd3 * vd2 - gm32 * bu0,                            &
+          ( - b0v3 + bd3 ) * ( Two * W**2 * vd3 ) &
+          - bd3 * vd3 - gm33 * bu0,                            &
           Zero ]
 
     dUdV(5,:) &
-      = [ ( W**2 - W ) * vu1,            &
-          Z * ( Two * W**2 * v1v1 + del11 ) - Two * W**2 * b0b1 * vu1 - b1b1 &
-          - D * W**3 * v1v1 - D * W * del11, &
-          Z * ( Two * W**2 * v1v2 + del12 ) - Two * W**2 * b0b2 * vu1 - b1b2 &
-          - D * W**3 * v1v2 - D * W * del12, &
-          Z * ( Two * W**2 * v1v3 + del13 ) - Two * W**2 * b0b3 * vu1 - b1b3 &
-          - D * W**3 * v1v3 - D * W * del13, &
-          G * W**2 * vu1,            &
-          Zero,            &
-          Two * ( -b0v1 + bd1 ) * W**2 * vu1 - bu1 * vd1 - bu0 * del11, &
-          Two * ( -b0v2 + bd2 ) * W**2 * vu1 - bu1 * vd2 - bu0 * del12, &
-          Two * ( -b0v3 + bd3 ) * W**2 * vu1 - bu1 * vd3 - bu0 * del13, &
+      = [ ( W**2 - W ),                                             &
+          Z * ( Two * W**2 * vd1 ) - D * W**3 * vd1 &
+          - b0b1 * ( One + Two * W**2 ),                            &
+          Z * ( Two * W**2 * vd2 ) - D * W**3 * vd2 &
+          - b0b2 * ( One + Two * W**2 ),                            &
+          Z * ( Two * W**2 * vd3 ) - D * W**3 * vd3 &
+          - b0b3 * ( One + Two * W**2 ),                            &
+          G * W**2 - One,                                           &
+          Zero,                                                     &
+          ( Two * W**2 - One ) * bd1 - ( Two * W**2 + One ) * b0v1, &
+          ( Two * W**2 - One ) * bd2 - ( Two * W**2 + One ) * b0v2, &
+          ( Two * W**2 - One ) * bd3 - ( Two * W**2 + One ) * b0v3, &
           Zero ]
 
     dUdV(6,:) &
-      = [ Zero, &
-          Zero, &
-          Zero, &
-          Zero, &
-          Zero, &
-          Zero, &
-          Zero, &
-          Zero, &
-          Zero, &
+      = [ Ye * W,              &
+          D * Ye * W**3 * vd1, &
+          D * Ye * W**3 * vd2, &
+          D * Ye * W**3 * vd3, &
+          Zero,                &
+          D * W,               &
+          Zero,                &
+          Zero,                &
+          Zero,                &
           Zero ]
 
     dUdV(7,:) &
-      = [ Zero,                                                             &
-          W**3 * vd1 * ( v1b1 - v1b1 ) + W * ( del11 * bu1 - del11 * bu1 ), &
-          W**3 * vd2 * ( v1b1 - v1b1 ) + W * ( del12 * bu1 - del12 * bu1 ), &
-          W**3 * vd3 * ( v1b1 - v1b1 ) + W * ( del13 * bu1 - del13 * bu1 ), &
-          Zero,                                                             &
-          Zero,                                                             &
-          W * ( vu1 * del11 - vu1 * del11 ),                                &
-          W * ( vu1 * del12 - vu1 * del12 ),                                &
-          W * ( vu1 * del13 - vu1 * del13 ),                                &
+      = [ Zero,                                                               &
+          W**3 * vd1 * ( bu1 - bu0 * vu1 ) - W * ( bd1 * vu1 + bu0 * del11 ), &
+          W**3 * vd2 * ( bu1 - bu0 * vu1 ) - W * ( bd2 * vu1 + bu0 * del12 ), &
+          W**3 * vd3 * ( bu1 - bu0 * vu1 ) - W * ( bd3 * vu1 + bu0 * del13 ), &
+          Zero,                                                               &
+          Zero,                                                               &
+          W * ( del11 - v1v1 ),                                               &
+          W * ( del12 - v1v2 ),                                               &
+          W * ( del13 - v1v3 ),                                               &
           Zero ]
 
     dUdV(8,:) &
-      = [ Zero,            &
-          W**3 * vd1 * ( v1b2 - v2b1 ) + W * ( del11 * bu2 - del21 * bu1 ), &
-          W**3 * vd2 * ( v1b2 - v2b1 ) + W * ( del12 * bu2 - del22 * bu1 ), &
-          W**3 * vd3 * ( v1b2 - v2b1 ) + W * ( del13 * bu2 - del23 * bu1 ), &
-          Zero,            &
-          Zero,            &
-          W * ( vu1 * del21 - vu2 * del11 ),            &
-          W * ( vu1 * del22 - vu2 * del12 ),            &
-          W * ( vu1 * del23 - vu2 * del13 ),            &
+      = [ Zero,                                                               &
+          W**3 * vd1 * ( bu2 - bu0 * vu2 ) - W * ( bd1 * vu2 + bu0 * del21 ), &
+          W**3 * vd2 * ( bu2 - bu0 * vu2 ) - W * ( bd2 * vu2 + bu0 * del22 ), &
+          W**3 * vd3 * ( bu2 - bu0 * vu2 ) - W * ( bd3 * vu2 + bu0 * del23 ), &
+          Zero,                                                               &
+          Zero,                                                               &
+          W * ( del21 - v2v1 ),                                               &
+          W * ( del22 - v2v2 ),                                               &
+          W * ( del23 - v2v3 ),                                               &
           Zero ]
 
     dUdV(9,:) &
-      = [ Zero,            &
-          W**3 * vd1 * ( v1b3 - v3b1 ) + W * ( del11 * bu3 - del31 * bu1 ), &
-          W**3 * vd2 * ( v1b3 - v3b1 ) + W * ( del12 * bu3 - del32 * bu1 ), &
-          W**3 * vd3 * ( v1b3 - v3b1 ) + W * ( del13 * bu3 - del33 * bu1 ), &
-          Zero,            &
-          Zero,            &
-          W * ( vu1 * del31 - vu3 * del11 ),            &
-          W * ( vu1 * del32 - vu3 * del12 ),            &
-          W * ( vu1 * del33 - vu3 * del13 ),            &
+      = [ Zero,                                                               &
+          W**3 * vd1 * ( bu3 - bu0 * vu3 ) - W * ( bd1 * vu3 + bu0 * del31 ), &
+          W**3 * vd2 * ( bu3 - bu0 * vu3 ) - W * ( bd2 * vu3 + bu0 * del32 ), &
+          W**3 * vd3 * ( bu3 - bu0 * vu3 ) - W * ( bd3 * vu3 + bu0 * del33 ), &
+          Zero,                                                               &
+          Zero,                                                               &
+          W * ( del31 - v3v1 ),                                               &
+          W * ( del32 - v3v2 ),                                               &
+          W * ( del33 - v3v3 ),                                               &
           Zero ]
 
     dUdV(10,:) &
@@ -480,7 +482,7 @@ CONTAINS
           Zero, &
           Zero, &
           Zero, &
-          Zero ]
+          One ]
 
     ! --- ---
 
