330a10ef42902687bdef504e1ce268d221580f8f
jbuffal <jbuffal1@utk.edu>
2024-08-22 22:33:39 -0400

    Adjust relative tolerance.

--- RegressionTesting/regression_test.py
+++ RegressionTesting/regression_test.py
@@ -100,11 +100,11 @@ class Regression:
     PF_E_Gold  = self.load_output(fn_gold)
 
     # comparison
-    PF_D_Status_Eq  = np.allclose(PF_D,  PF_D_Gold,  atol = 0.0, rtol = 1e-12)
-    PF_V1_Status_Eq = np.allclose(PF_V1, PF_V1_Gold, atol = 0.0, rtol = 1e-12)
-    PF_V2_Status_Eq = np.allclose(PF_V2, PF_V2_Gold, atol = 0.0, rtol = 1e-12)
-    PF_V3_Status_Eq = np.allclose(PF_V3, PF_V3_Gold, atol = 0.0, rtol = 1e-12)
-    PF_E_Status_Eq  = np.allclose(PF_E,  PF_E_Gold,  atol = 0.0, rtol = 1e-12)
+    PF_D_Status_Eq  = np.allclose(PF_D,  PF_D_Gold,  atol = 0.0, rtol = 1e-13)
+    PF_V1_Status_Eq = np.allclose(PF_V1, PF_V1_Gold, atol = 0.0, rtol = 1e-13)
+    PF_V2_Status_Eq = np.allclose(PF_V2, PF_V2_Gold, atol = 0.0, rtol = 1e-13)
+    PF_V3_Status_Eq = np.allclose(PF_V3, PF_V3_Gold, atol = 0.0, rtol = 1e-13)
+    PF_E_Status_Eq  = np.allclose(PF_E,  PF_E_Gold,  atol = 0.0, rtol = 1e-13)
 
     equal_success = all([PF_D_Status_Eq,  PF_V1_Status_Eq, \
                          PF_V2_Status_Eq, PF_V3_Status_Eq,
