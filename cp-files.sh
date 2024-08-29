6b381cab447c4e226ae06945161ca319269f354b
Quan, Shaoping <shaoping.quan@intel.com>
2024-08-29 17:50:34 -0400

    more clean up

--- cp-files.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-
-cp TwoMoment_TimeSteppingModule_OrderV.F90                  SandBox/TwoMoment_OrderV/TwoMoment_TimeSteppingModule_OrderV.F90 
-cp TwoMoment_DiscretizationModule_Streaming_OrderV.F90      SandBox/TwoMoment_OrderV/TwoMoment_DiscretizationModule_Streaming_OrderV.F90 
-cp LinearAlgebraModule.F90                                  Modules/Library/LinearAlgebraModule.F90 
