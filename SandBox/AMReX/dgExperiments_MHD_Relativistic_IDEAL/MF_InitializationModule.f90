71493de8bc2244a1e2c1b717f8e776fcf1b94caa
jbuffal <jbuffal1@utk.edu>
2024-10-30 12:49:12 -0400

    Initial attempt at shearing disk in new AMReX implementation.

--- SandBox/AMReX/dgExperiments_MHD_Relativistic_IDEAL/MF_InitializationModule.f90
+++ SandBox/AMReX/dgExperiments_MHD_Relativistic_IDEAL/MF_InitializationModule.f90
@@ -32,6 +32,9 @@ MODULE MF_InitializationModule
     ComputePressureFromPrimitive_IDEAL, &
     Gamma_IDEAL
   USE GeometryFieldsModule, ONLY: &
+    iGF_h_1, &
+    iGF_h_2, &
+    iGF_h_3, &
     iGF_Gm_dd_11, &
     iGF_Gm_dd_22, &
     iGF_Gm_dd_33, &
@@ -39,7 +42,10 @@ MODULE MF_InitializationModule
     iGF_Beta_1, &
     iGF_Beta_2, &
     iGF_Beta_3, &
+    iGF_Psi, &
     nGF
+  USE GeometryComputationModule, ONLY: &
+    ComputeGeometryX_FromScaleFactors
   USE MagnetofluidFieldsModule, ONLY: &
     iCM_D, &
     iCM_S1, &
@@ -65,6 +71,20 @@ MODULE MF_InitializationModule
     nPM
   USE MHD_UtilitiesModule, ONLY: &
     ComputeConserved_MHD
+  USE UnitsModule, ONLY: &
+    Kilometer,    &
+    Second,       &
+    SolarMass,    &
+    Gram,         &
+    Centimeter,   &
+    Erg,          &
+    SpeedOfLight, &
+    GravitationalConstant, &
+    Millisecond, &
+    Gauss
+  USE UtilitiesModule, ONLY: &
+    Locate, &
+    Interpolate1D_Linear
 
   ! --- Local Modules ---
 
@@ -90,9 +110,15 @@ MODULE MF_InitializationModule
     ConstructEdgeMap
   USE MF_MHD_BoundaryConditionsModule, ONLY: &
     ApplyBoundaryConditions_MHD_MF
+  USE MF_GeometryModule, ONLY: &
+    ApplyBoundaryConditions_Geometry_MF  
+
+  USE HDF5
 
   IMPLICIT NONE
   PRIVATE
+
+  INTEGER :: HDFERR
 
   PUBLIC :: InitializeFields_MF
 
@@ -103,7 +129,7 @@ CONTAINS
     ( iLevel, MF_uGF, MF_uCM )
 
     INTEGER             , INTENT(in)    :: iLevel
-    TYPE(amrex_multifab), INTENT(in)    :: MF_uGF
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uGF
     TYPE(amrex_multifab), INTENT(inout) :: MF_uCM
 
     IF( iLevel .EQ. 0 .AND. amrex_parallel_ioprocessor() )THEN
@@ -140,6 +166,10 @@ CONTAINS
       CASE( 'Advection3D' )
 
         CALL InitializeFields_Advection3D( iLevel, MF_uGF, MF_uCM )
+
+      CASE( 'ShearingDisk' )
+
+        CALL InitializeFields_ShearingDisk( iLevel, MF_uGF, MF_uCM )
 
       CASE DEFAULT
 
@@ -189,6 +219,8 @@ CONTAINS
     REAL(DP) :: Amp
     REAL(DP) :: X1_0
     REAL(DP) :: sigmaX1
+
+    uPM = Zero
 
     AdvectionProfile = 'SineWaveX1'
     CALL amrex_parmparse_build( PP, 'thornado' )
@@ -1575,6 +1607,316 @@ CONTAINS
     CALL amrex_mfiter_destroy( MFI )
 
   END SUBROUTINE InitializeFields_Advection3D
+
+
+  SUBROUTINE InitializeFields_ShearingDisk( iLevel, MF_uGF, MF_uCM )
+
+    INTEGER             , INTENT(in)    :: iLevel
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uGF
+    TYPE(amrex_multifab), INTENT(inout) :: MF_uCM
+
+    TYPE(amrex_mfiter) :: MFI
+    TYPE(amrex_box)    :: BX
+
+    INTEGER  :: iNX, iX1, iX2, iX3
+    INTEGER  :: iX_B0(3), iX_E0(3), iX_B1(3), iX_E1(3)
+    REAL(DP) :: uPM(nDOFX,nPM)
+    REAL(DP) :: Pressure
+
+    REAL(DP), ALLOCATABLE :: U(:,:,:,:,:)
+    REAL(DP), ALLOCATABLE :: G(:,:,:,:,:)
+
+    REAL(DP), CONTIGUOUS, POINTER :: uGF(:,:,:,:)
+    REAL(DP), CONTIGUOUS, POINTER :: uCM(:,:,:,:)
+
+    TYPE(EdgeMap) :: Edge_Map
+
+    ! --- Problem-specific Parameters ---
+
+    INTEGER  :: iNX1, iNX2, iNX3
+    REAL(DP) :: X1, X2, X3
+
+    CHARACTER(256) :: FileName
+
+    INTEGER        :: nX_Data
+    INTEGER(HID_T) :: FILE_ID
+
+    REAL(DP) :: CB1, CB2, CB3, V1, V2, V3, VSq, W, VdotB
+    REAL(DP), ALLOCATABLE :: PressureArr(:), DensityArr(:), V3Arr(:), &
+                             AlphaArr(:), PsiArr(:), X1Arr(:)
+
+    uPM = Zero
+
+    FileName &
+      = "/home/jbuffal/thornado_MHD_3D/Workflow/MHD/ShearingDisk/GR_LR_diffrot.h5"
+    nX_Data = 10000
+
+    ! --- Populate arrays ---
+
+    CALL H5OPEN_F( HDFERR )
+
+    CALL H5FOPEN_F( TRIM( FileName ), H5F_ACC_RDONLY_F, FILE_ID, HDFERR )
+
+    ALLOCATE( PressureArr(nX_Data), DensityArr(nX_Data), V3Arr(nX_Data), &
+              AlphaArr(nX_Data), PsiArr(nX_Data), X1Arr(nX_Data) )
+
+    CALL ReadDataset1DHDF( PsiArr,      '/psi',   FILE_ID )
+    CALL ReadDataset1DHDF( AlphaArr,    '/alpha', FILE_ID )
+    CALL ReadDataset1DHDF( X1Arr,       '/r',     FILE_ID )
+    CALL ReadDataset1DHDF( PressureArr, '/pres',  FILE_ID )
+    CALL ReadDataset1DHDF( DensityArr,  '/rho',   FILE_ID )
+    CALL ReadDataset1DHDF( V3Arr,       '/V3',    FILE_ID )
+
+    X1Arr       = X1Arr       * Centimeter
+    DensityArr  = DensityArr  * ( Gram / Centimeter**3 )
+    PressureArr = PressureArr * ( Erg  / Centimeter**3 )
+    V3Arr       = V3Arr       * ( One  / Second )
+
+    IF( iLevel .EQ. 0 .AND. amrex_parallel_ioprocessor() )THEN
+
+      WRITE(*,'(6x,A)')   'Shearing Disk'
+      WRITE(*,'(6x,A,A)') '-----------------'
+      WRITE(*,*)
+      WRITE(*,*)
+
+    END IF
+
+    CALL amrex_mfiter_build( MFI, MF_uGF, tiling = UseTiling )
+
+    DO WHILE( MFI % next() )
+
+      uGF => MF_uGF % DataPtr( MFI )
+      uCM => MF_uCM % DataPtr( MFI )
+
+      BX = MFI % tilebox()
+
+      iX_B0 = BX % lo
+      iX_E0 = BX % hi
+      iX_B1 = iX_B0 - swX
+      iX_E1 = iX_E0 + swX
+
+      CALL AllocateArray_X &
+             ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
+               [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nGF ], &
+               G )
+
+      CALL AllocateArray_X &
+             ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
+               [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nCM ], &
+               U )
+
+      CALL amrex2thornado_X &
+             ( nGF, iX_B1, iX_E1, LBOUND( uGF ), iX_B0, iX_E0, uGF, G )
+
+      DO iX3 = iX_B1(3), iX_E1(3)
+      DO iX2 = iX_B1(2), iX_E1(2)
+      DO iX1 = iX_B1(1), iX_E1(1)
+      DO iNX = 1       , nDOFX
+
+        iNX1 = NodeNumberTableX(1,iNX)
+        iNX2 = NodeNumberTableX(2,iNX)
+        iNX3 = NodeNumberTableX(3,iNX)
+
+        X1 = NodeCoordinate( MeshX(1), iX1, iNX1 )
+        X2 = NodeCoordinate( MeshX(2), iX2, iNX2 )
+        X3 = NodeCoordinate( MeshX(3), iX3, iNX3 )
+
+        G(iNX,iX1,iX2,iX3,iGF_Alpha) &
+           = Interpolate1D( X1Arr, AlphaArr, SIZE( X1Arr ), X1 )
+
+        G(iNX,iX1,iX2,iX3,iGF_Psi) &
+             = Interpolate1D( X1Arr, PsiArr, SIZE( X1Arr ), X1 )
+
+        G(iNX,iX1,iX2,iX3,iGF_h_1) &
+          = G(iNX,iX1,iX2,iX3,iGF_Psi)**2
+        G(iNX,iX1,iX2,iX3,iGF_h_2) &
+          = G(iNX,iX1,iX2,iX3,iGF_Psi)**2
+        G(iNX,iX1,iX2,iX3,iGF_h_3) &
+          = G(iNX,iX1,iX2,iX3,iGF_Psi)**2 * X1
+
+        CALL ComputeGeometryX_FromScaleFactors( G(:,iX1,iX2,iX3,:) )
+
+        G(iNX,iX1,iX2,iX3,iGF_Beta_1) = Zero
+        G(iNX,iX1,iX2,iX3,iGF_Beta_2) = Zero
+        G(iNX,iX1,iX2,iX3,iGF_Beta_3) = Zero
+
+        ! --- Fluid Fields ---
+
+        uPM(iNX,iPM_D) &
+          = Interpolate1D( X1Arr, DensityArr, SIZE( X1Arr ), X1 )
+
+        V1 = Zero
+        V2 = Zero
+        V3 = Interpolate1D( X1Arr, V3Arr, SIZE( X1Arr ), X1 )
+
+        VSq = G(iNX,iX1,iX2,iX3,iGF_Gm_dd_11) * V1**2 &
+              + G(iNX,iX1,iX2,iX3,iGF_Gm_dd_22) * V2**2 &
+              + G(iNX,iX1,iX2,iX3,iGF_Gm_dd_33) * V3**2
+
+        W = One / SQRT( One - VSq )
+
+        uPM(iNX,iPM_V1) = V1
+        uPM(iNX,iPM_V2) = V2
+        uPM(iNX,iPM_V3) = V3
+
+        uPM(iNX,iPM_E) &
+          = Interpolate1D( X1Arr, PressureArr, SIZE( X1Arr ), X1 ) &
+            / ( Gamma_IDEAL - One )
+
+        CB1 = Zero
+        CB2 = 2.0 * 1.0d13 * Gauss
+        CB3 = Zero
+
+        VdotB = G(iNX,iX1,iX2,iX3,iGF_Gm_dd_11) * V1 * CB1 &
+                + G(iNX,iX1,iX2,iX3,iGF_Gm_dd_22) * V2 * CB2 &
+                + G(iNX,iX1,iX2,iX3,iGF_Gm_dd_33) * V3 * CB3
+
+        uPM(iNX,iPM_B1) = W * VdotB * V1 + CB1 / W
+        uPM(iNX,iPM_B2) = W * VdotB * V2 + CB2 / W
+        uPM(iNX,iPM_B3) = W * VdotB * V3 + CB3 / W
+
+        uPM(iNX,iPM_Chi) = Zero
+
+        CALL ComputePressureFromPrimitive_IDEAL &
+               ( uPM(iNX,iPM_D ), uPM(iNX,iPM_E), &
+                 uPM(iNX,iPM_Ne), Pressure )
+
+        CALL ComputeConserved_MHD &
+               ( uPM(iNX,iPM_D  ), &
+                 uPM(iNX,iPM_V1 ), &
+                 uPM(iNX,iPM_V2 ), &
+                 uPM(iNX,iPM_V3 ), &
+                 uPM(iNX,iPM_E  ), &
+                 uPM(iNX,iPM_Ne ), &
+                 uPM(iNX,iPM_B1 ), &
+                 uPM(iNX,iPM_B2 ), &
+                 uPM(iNX,iPM_B3 ), &
+                 uPM(iNX,iPM_Chi), &
+                 U  (iNX,iX1,iX2,iX3,iCM_D  ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_S1 ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_S2 ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_S3 ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_E  ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_Ne ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_B1 ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_B2 ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_B3 ), &
+                 U  (iNX,iX1,iX2,iX3,iCM_Chi), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Gm_dd_11), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Gm_dd_22), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Gm_dd_33), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Alpha), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Beta_1), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Beta_2), &
+                 G  (iNX,iX1,iX2,iX3,iGF_Beta_3), &
+                 Pressure, &
+                 EvolveOnlyMagnetic )
+
+      END DO
+      END DO
+      END DO
+      END DO
+
+      CALL ConstructEdgeMap( iLevel, BX, Edge_Map )
+
+      CALL ApplyBoundaryConditions_MHD_MF &
+             ( Zero, iX_B0, iX_E0, iX_B1, iX_E1, U, Edge_Map )
+
+      CALL thornado2amrex_X &
+             ( nGF, iX_B1, iX_E1, LBOUND( uGF ), iX_B1, iX_E1, uGF, G )
+
+      CALL thornado2amrex_X &
+             ( nCM, iX_B1, iX_E1, LBOUND( uCM ), iX_B1, iX_E1, uCM, U )
+
+      CALL DeallocateArray_X &
+             ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
+               [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nCM ], &
+               U )
+
+      CALL DeallocateArray_X &
+             ( [ 1    , iX_B1(1), iX_B1(2), iX_B1(3), 1   ], &
+               [ nDOFX, iX_E1(1), iX_E1(2), iX_E1(3), nGF ], &
+               G )
+
+    END DO
+
+    CALL amrex_mfiter_destroy( MFI )
+
+  END SUBROUTINE InitializeFields_ShearingDisk
+
+
+  SUBROUTINE ReadDataset1DHDF( Dataset, DatasetName, FILE_ID )
+
+    REAL(DP),         INTENT(out) :: Dataset(:)
+    CHARACTER(LEN=*), INTENT(in)  :: DatasetName
+    INTEGER(HID_T),   INTENT(in)  :: FILE_ID
+
+    INTEGER(HID_T) :: DATASET_ID
+    INTEGER(HID_T) :: DATASIZE(1)
+
+    DATASIZE = SHAPE( Dataset )
+
+    CALL H5DOPEN_F( FILE_ID, TRIM( DatasetName ), DATASET_ID, HDFERR )
+
+    CALL H5DREAD_F( DATASET_ID, H5T_NATIVE_DOUBLE, Dataset, DATASIZE, HDFERR )
+
+    CALL H5DCLOSE_F( DATASET_ID, HDFERR )
+
+  END SUBROUTINE ReadDataset1DHDF
+
+
+  REAL(DP) FUNCTION Interpolate1D( x, y, n, xq )
+
+    INTEGER,                INTENT(in) :: n
+    REAL(DP), DIMENSION(n), INTENT(in) :: x, y
+    REAL(DP),               INTENT(in) :: xq
+
+    INTEGER :: i
+
+    i = Locate( xq, x, n )
+
+    !PRINT*, 'i: ', i
+
+    IF( i == 0 )THEN
+
+      ! --- Extrapolate Left ---
+
+      Interpolate1D &
+        = Interpolate1D_Linear( xq, x(1), x(2), y(1), y(2) )
+
+      !PRINT*, 'x(1): ', x(1)
+      !PRINT*, 'x(2): ', x(2)
+      !PRINT*, 'y(1): ', y(1)
+      !PRINT*, 'y(2): ', y(2)
+
+    ELSE IF( i == n )THEN
+
+      ! --- Extrapolate Right ---
+
+      Interpolate1D &
+        = Interpolate1D_Linear( xq, x(n-1), x(n), y(n-1), y(n) )
+
+      !PRINT*, 'x(n-1): ', x(n-1)
+      !PRINT*, 'x(n): ',   x(n)
+      !PRINT*, 'y(n-1): ', y(n-1)
+      !PRINT*, 'y(n): ',   y(n)
+
+
+    ELSE
+
+      Interpolate1D &
+        = Interpolate1D_Linear( xq, x(i), x(i+1), y(i), y(i+1) )
+
+      !PRINT*, 'x(i): ', x(i)
+      !PRINT*, 'x(i+1): ', x(i+1)
+      !PRINT*, 'y(i): ', y(i)
+      !PRINT*, 'y(i+1): ', y(i+1)
+
+    END IF
+
+    RETURN
+
+  END FUNCTION Interpolate1D
 
 
 END MODULE MF_InitializationModule
