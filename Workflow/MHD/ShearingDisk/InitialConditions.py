16be4cfdd506d19ea11d1b506c7d69e8db4fda97
jbuffal <jbuffal1@utk.edu>
2024-11-13 09:53:41 -0500

    Update initial conditions generation script for shearing disk.

--- Workflow/MHD/ShearingDisk/InitialConditions.py
+++ Workflow/MHD/ShearingDisk/InitialConditions.py
@@ -2,19 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import h5py
+from numpy import pi
 
-G = 6.67 * 10**-8 #
+G = 6.674 * 10**-8 #
+c =  2.99792458 * 10**10 # cm / s
 
 gamma = 4.0 / 3.0
 gamma_mo = 1.0 / 3.0
-K = 4.897 * 10**14 # cgs
-M = 1.37 * 10**33 # g
-c =  2.99792458 * 10**10 # cm / s
-rl = 1.45 * 10**6 # cm
-ru = 1.65 * 10**6 # cm
-rc = 1.55 * 10**6 # cm
+SolarMass = 1.989e+33
 
-rhol = 3.2 * 10**13 # g / cm**3
+Params =  [1.44 * 10**6, 1.66 * 10**6, 4.897 * 10**14, 0.68, 3.2 * 10**13, 1900.0, -1.25] #Less Relativistic
+#Params = [1.44 * 10**6, 1.66 * 10**6, 9.1 * 10**14, 2.70, 3.2 * 10**13, 0, 0] # 1739 * pi, -1.33] #More Relativistic
+
+Filename = 'GR_LR_diffrot'
+
+rl = Params[0]
+ru = Params[1]
+rc = (rl + ru) / 2
+
+K = Params[2]
+M = Params[3] * SolarMass
+rhol = Params[4]
+Omega0 = Params[5]
+alphaO = Params[6]
+
+def X(r):
+
+	return (G * M) / (2.0 * r * c**2)
 
 def psi(r):
 
@@ -40,37 +54,34 @@ def dgamma33(r):
 
 	return 2.0 * r * psi(r)**4 + 4.0 * r**2 * psi(r)**3 * dpsi(r)
 
-def Omega(r, Omega0, alphaO):
+def Omega(r):
 
 	return Omega0 * (r / rc)**alphaO
 
-def W(r, Omega0, alphaO):
+def W(r):
 
-	return 1.0 / np.sqrt(1.0 - gamma33(r) * Omega(r, Omega0, alphaO)**2 / c**2)
+	return 1.0 / np.sqrt(1.0 - gamma33(r) * Omega(r)**2 / (alpha(r)**2 * c**2))
 
-def X(r):
+def Delta(r):
 
-	return (G * M) / (2.0 * r * c**2)
-
-def Delta(r, Omega0):
-
-	print(r)
-	print(Omega0)
-	print(r**2 * Omega0**2 * (1.0 + X(r))**6)
-	print(c**2 * (1.0 - X(r))**2)
 	return np.sqrt(-r**2 * Omega0**2 * (1.0 + X(r))**6 + c**2 * (1.0 - X(r))**2)
 
-def analyticf_N(r, Omega0, alphaO):
+def p(r, rho):
+
+	return K * rho**gamma
+
+def analyticf_N(r):
 
 	therml = rhol**gamma_mo
 	gravl  = (G * M) / rl
 	grav   = (G * M) / r
-	rotl   = (rl**2 * Omega(rl, Omega0, alphaO)**2) \
+	rotl   = (rl**2 * Omega(rl)**2) \
 			 / (2.0 * alphaO + 2.0)
-	rot    = (r**2  * Omega(r, Omega0, alphaO)**2)   \
+	rot    = (r**2  * Omega(r)**2)   \
              / (2.0 * alphaO + 2.0)
-	return therml + gamma_mo * (grav - gravl + rot - rotl) \
-                   / (K * gamma)
+	print(rotl)
+	print(rot)
+	return therml + (gamma_mo / (K * gamma)) * (grav - gravl + rot - rotl)
 
 def analyticf_GR_norot(r):
 
@@ -80,61 +91,61 @@ def analyticf_GR_norot(r):
 	therml   = (G * M / r) * (gamma_mo / (K * gamma)) * (1.0 / (1.0 - X(r)))
 	return grav + therml
 
-def analyticf_GR_constrot(r, Omega0):
+def analyticf_GR_constrot(r):
 
 	const_GR = 4.0 * c**4 * (rhol**gamma_mo + gamma_mo * c**2 / (K * gamma)) \
-               * (Delta(rl, Omega0) / (1.0 + X(rl)))
-	grav     = const_GR * (1.0 + X(r)) / (4.0 * c**4 * Delta(r, Omega0))
+               * (Delta(rl) / (1.0 + X(rl)))
+	grav     = const_GR * (1.0 + X(r)) / (4.0 * c**4 * Delta(r))
 	therml   = gamma_mo * c**2 / (K * gamma)
 	return grav - therml
 
-def analyticrho_N(r, Omega0, alphaO):
+def analyticrho_N(r):
 
-	return analyticf_N(r, Omega0, alphaO)**(1.0 / gamma_mo)
+	return analyticf_N(r)**(1.0 / gamma_mo)
 
 def analyticrho_GR_norot(r):
 
 	return analyticf_GR_norot(r)**(1.0 / gamma_mo)
 
-def analyticrho_GR_constrot(r, Omega0):
+def analyticrho_GR_constrot(r):
 
-	return analyticf_GR_constrot(r, Omega0)**(1.0 / gamma_mo)
+	return analyticf_GR_constrot(r)**(1.0 / gamma_mo)
 
-def q_N(r, Omega0, alphaO):
+def q_N(r):
 
 	grav = - G * M / r**2
-	rot = r * Omega(r, Omega0, alphaO)**2
+	rot = r * Omega(r)**2
 	return (1.0 / (gamma * K)) * (grav + rot)
 
-def diffEq_N(r, f, Omega0, alphaO):
+def diffEq_N(r, f):
 
-	return gamma_mo * q_N(r, Omega0, alphaO)
+	return gamma_mo * q_N(r)
 
-def q_GR(r, Omega0, alphaO):
-
-	grav = dlnalpha(r)
-	rot = Omega(r, Omega0, alphaO)**2 \
-          * dgamma33(r) / (2.0 * alpha(r)**2 * c**2)
-	return (W(r, Omega0, alphaO)**2 / gamma_mo) * (grav - rot)
-
-def r_GR(r, Omega0, alphaO):
+def q_GR(r):
 
 	grav = dlnalpha(r)
-	rot = Omega(r, Omega0, alphaO)**2 \
+	rot = Omega(r)**2 \
           * dgamma33(r) / (2.0 * alpha(r)**2 * c**2)
-	return (c**2 * W(r, Omega0, alphaO)**2 / (K * gamma)) * (rot - grav)
+	return (W(r)**2 / gamma_mo) * (grav - rot)
 
-def diffEq_GR(r, f, Omega0, alphaO):
+def r_GR(r):
 
-	return gamma_mo * r_GR(r, Omega0, alphaO) \
-           - gamma_mo * q_GR(r, Omega0, alphaO) * f
+	grav = dlnalpha(r)
+	rot = Omega(r)**2 \
+          * dgamma33(r) / (2.0 * alpha(r)**2 * c**2)
+	return (c**2 * W(r)**2 / (K * gamma)) * (rot - grav)
 
-def solve_N(r, Omega0, alphaO):
+def diffEq_GR(r, f):
+
+	return gamma_mo * r_GR(r) \
+           - gamma_mo * q_GR(r) * f
+
+def solve_N(r):
 
 	f0 = [rhol**( gamma_mo )]
 
 	sol = solve_ivp(fun = diffEq_N, t_span = (r[0],r[-1]), y0 = f0, method = 'DOP853',
-    	            t_eval = r, atol = 1e-13, rtol = 1e-13, args = (Omega0, alphaO))
+    	            t_eval = r, atol = 1e-13, rtol = 1e-13)
 
 	f = sol.y[0,:]
 
@@ -142,12 +153,12 @@ def solve_N(r, Omega0, alphaO):
 
 	return f, rho
 
-def solve_GR(r, Omega0, alphaO):
+def solve_GR(r):
 
 	f0 = [rhol**( gamma_mo )]
 
 	sol = solve_ivp(fun = diffEq_GR, t_span = (r[0],r[-1]), y0 = f0, method = 'DOP853',
-    	            t_eval = r, atol = 1e-13, rtol = 1e-13, args = (Omega0, alphaO))
+    	            t_eval = r, atol = 1e-13, rtol = 1e-13)
 
 	f = sol.y[0,:]
 
@@ -157,105 +168,54 @@ def solve_GR(r, Omega0, alphaO):
 
 def output(r, rho, filename):
 
-	file = h5py.File(filename + '.hdf5', 'w')
+	file = h5py.File(filename + '.h5', 'w')
 
-	r_dset   = file.create_dataset("r",        r.shape,   dtype = 'd')
-	rho_dset = file.create_dataset("rho_dset", rho.shape, dtype = 'd')
+	r_dset   = file.create_dataset("r",    r.shape, dtype = 'd')
+	rho_dset = file.create_dataset("rho",  r.shape, dtype = 'd')
+	p_dset   = file.create_dataset("pres", r.shape, dtype = 'd')
+	V3_dset  = file.create_dataset("V3",   r.shape,  dtype = 'd')
+
+	alpha_dset = file.create_dataset("alpha", r.shape, dtype = 'd')
+	psi_dset   = file.create_dataset("psi",   r.shape, dtype = 'd')
 
 	r_dset[:] = r
 	rho_dset[:] = rho
+	p_dset[:] = p(r, rho)
+	V3_dset[:] = Omega(r) / alpha(r)
+
+	alpha_dset[:] = alpha(r)
+	psi_dset[:] = psi(r)
+
+	file.close()
 
 r = np.linspace( rl, ru, 10000 )
 
-arho_N_norot_NRparams     = analyticrho_N(r, 0, 0)
-arho_N_constrot_NRparams  = analyticrho_N(r, 1900, 0)
-arho_N_diffrot_NRparams   = analyticrho_N(r, 1900, -1.25)
-arho_GR_norot_NRparams    = analyticrho_GR_norot(r)
-arho_GR_constrot_NRparams = analyticrho_GR_constrot(r, 1900)
+arho_N = analyticrho_N(r)
 
-rho_N_norot_NRparams     = solve_N(r, 0, 0)[1]
-rho_N_constrot_NRparams  = solve_N(r, 1900, 0)[1]
-rho_N_diffrot_NRparams   = solve_N(r, 1900, -1.25)[1]
-rho_GR_norot_NRparams    = solve_GR(r, 0, 0)[1]
-rho_GR_constrot_NRparams = solve_GR(r, 1900, 0)[1]
-rho_GR_diffrot_NRparams  = solve_GR(r, 1900, -1.25)[1]
+if (alphaO == 0.0 and Omega0 == 0.0):
+	arho_GR = analyticrho_GR_norot(r)
+	print('No Rotation')
+elif(alphaO == 0.0 and Omega0 != 0.0):
+	arho_GR = analyticrho_GR_constrot(r)
+	print('Const. Rotation')
+else:
+	arho_GR = np.zeros(np.size(r))
 
-output(r, rho_GR_norot_NRparams,    'GR_norot_NRparams'   )
-output(r, rho_GR_constrot_NRparams, 'GR_constrot_NRparams')
-output(r, rho_GR_diffrot_NRparams,  'GR_diffrot_NRparams' )
+rho_N  = solve_N(r)[1]
+rho_GR = solve_GR(r)[1]
 
-plt.figure(0)
-plt.plot(r, rho_N_diffrot_NRparams,  '-',  linewidth=3)
-plt.plot(r, arho_N_diffrot_NRparams, '--', linewidth=3)
-plt.title(r'Newtonian ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} = -1.25)$')
-plt.xlabel(r'$\varpi$')
-plt.ylabel(r'$\rho$ ($10^{13}$ $g$ $cm^{-3}$)')
-plt.legend([ 'Numerical', 'Analytic'])
-
-plt.figure(10)
-plt.plot(r, rho_GR_norot_NRparams,  '-',  linewidth=3)
-plt.plot(r, arho_GR_norot_NRparams, '--', linewidth=3)
-plt.title(r'GR ($\Omega_{0} = 0 s^{-1}$, $\alpha_{\Omega} = 0)$')
-plt.xlabel(r'$\varpi$')
-plt.ylabel(r'$\rho$ ($10^{13}$ $g$ $cm^{-3}$)')
-plt.legend([ 'Numerical', 'Analytic'])
-
-plt.figure(20)
-plt.plot(r, rho_GR_constrot_NRparams,  '-',  linewidth=3)
-plt.plot(r, arho_GR_constrot_NRparams, '--', linewidth=3)
-plt.title(r'GR ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} = 0)$')
-plt.xlabel(r'$\varpi$')
-plt.ylabel(r'$\rho$ ($10^{13}$ $g$ $cm^{-3}$)')
-plt.legend([ 'Numerical', 'Analytic'])
-
-# How to plot with two different y-axes
-# https://matplotlib.org/stable/gallery/subplots_axes_and_figures/two_scales.html
-
-plt.figure(2)
-plt.plot(r / 10**5, rho_N_diffrot_NRparams / 10**13, 'k--', linewidth=3)
-plt.plot(r / 10**5, 1900 * (r / rc)**-1.25 / 10**3,'k-.', linewidth=3)
-plt.title(r'Newtonian ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} = -1.25)$')
-plt.xlabel('r ($km$)')
-ax1 = plt.gca()
-ax2 = ax1.twinx()
-ax2.plot(r / 10**5, - G * M / r / 10**19, 'k-', linewidth=3)
-ax1.legend([r'$\rho$ ($10^{13}$ $g$ $cm^{-3}$)', r'$\Omega$ ($10^{3}$ $Hz$)'],
-           loc='upper center', handlelength=3)
-ax2.legend([r'$\Phi$ ($10^{19}$ $erg$ $g^{-1}$)'], loc='lower center')
-ax1.set_ylabel(r'$\rho$ and $\Omega$')
-ax2.set_ylabel(r'$\Phi$')
-
-plt.figure(30)
-plt.plot(r / 10**5, arho_N_norot_NRparams,     linewidth=3, color='b')
-plt.plot(r / 10**5, arho_N_constrot_NRparams,  linewidth=3, color='y')
-plt.plot(r / 10**5, arho_N_diffrot_NRparams,   linewidth=3, color='r')
-plt.plot(r / 10**5, rho_GR_norot_NRparams,     linewidth=3, color='b',  linestyle = '--')
-plt.plot(r / 10**5, rho_GR_constrot_NRparams,  linewidth=3, color='y',  linestyle = '--')
-plt.plot(r / 10**5, rho_GR_diffrot_NRparams,   linewidth=3, color='r',  linestyle = '--')
-plt.xlabel('r ($km$)')
-plt.ylabel(r'$\rho$ ($10^{13}$ $g$ $cm^{-3}$)')
-plt.legend([ 'Analytic Newtonian (No Rotation)',
-             r'Analytic Newtonian ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} =  0.00$)',
-             r'Analytic Newtonian ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} = -1.25$)',
-             'Numerical GR (No Rotation)',
-             r'Numerical GR ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} =  0.00$)',
-             r'Numerical GR ($\Omega_{0} = 1900 s^{-1}$, $\alpha_{\Omega} = -1.25$)'])
+print(rho_N)
+print(rho_GR)
 
 plt.figure(1)
-plt.plot(r, (rho_N_diffrot_NRparams - arho_N_diffrot_NRparams) / rhol, linewidth=3)
+plt.plot(rho_N)
+plt.plot(arho_N)
 
-plt.figure(11)
-plt.plot(r, (rho_GR_norot_NRparams - arho_GR_norot_NRparams) / arho_GR_norot_NRparams, linewidth=3)
+plt.figure(2)
+plt.plot(abs(rho_N  - arho_N ) / arho_N )
+#plt.plot(abs(rho_GR - arho_GR) / arho_GR )
+plt.show()
+print(max(abs(rho_N  - arho_N ) / arho_N ))
+#print(max(abs(rho_GR - arho_GR) / arho_GR ))
 
-plt.figure(21)
-plt.plot(r, (rho_GR_constrot_NRparams - arho_GR_constrot_NRparams) / arho_GR_constrot_NRparams, linewidth=3)
-
-plt.figure(31)
-plt.plot(r / 10**5, psi(r),   linewidth=3)
-plt.plot(r / 10**5, alpha(r), linewidth=3)
-plt.plot(r / 10**5, 1 + (gamma / gamma_mo) * K * arho_GR_norot_NRparams**gamma_mo / c**2, linewidth=3)
-plt.xlabel('r ($km$)')
-plt.title('GR (No Rotation)')
-plt.legend([r'$\psi$', r'$\alpha$', r'h ($c^{2}$)'])
-
-#plt.show()
+output(r, rho_GR, Filename)
