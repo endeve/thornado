0bc790a816758190a18987d217e657bdd6cb81d9
Harris, Austin <harrisja@ornl.gov>
2025-04-23 10:57:28 -0400

    Construct ProjectionMatrix from scale factors instead of SqrtGm

--- Modules/Library/SubcellReconstructionModule.F90
+++ Modules/Library/SubcellReconstructionModule.F90
@@ -12,7 +12,8 @@ MODULE SubcellReconstructionModule
     IndLX_Q, L_X1, L_X2, L_X3
   USE GeometryFieldsModule, ONLY: &
     CoordinateSystem, &
-    uGF, iGF_SqrtGm
+    uGF, iGF_SqrtGm, iGF_h_1, iGF_h_2, iGF_h_3, &
+    ComputeGeometryX_SpatialMetric
   USE MeshModule, ONLY: &
     MeshX
   USE LinearAlgebraModule, ONLY: &
@@ -26,7 +27,7 @@ MODULE SubcellReconstructionModule
   PUBLIC :: CreateSubcellReconstruction
   PUBLIC :: DestroySubcellReconstruction
 
-  REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix0(:,:,:)
+  REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix0(:,:,:,:,:)
   REAL(DP), ALLOCATABLE, PUBLIC :: ProjectionMatrix(:,:,:,:,:)
   REAL(DP), ALLOCATABLE, PUBLIC :: ReconstructionMatrix(:,:,:,:,:)
 
@@ -38,6 +39,8 @@ CONTAINS
     INTEGER  :: iS1, iS2, iS3, iS
     INTEGER  :: jS1, jS2, jS3, jS
     INTEGER  :: kS1, kS2, kS3, kS
+    INTEGER  :: lS1, lS2, lS3, lS
+    INTEGER  :: mS1, mS2, mS3, mS
     INTEGER  :: iN1, iN2, iN3
     INTEGER  :: INFO
     INTEGER  :: IPIV(nDOFX)
@@ -47,7 +50,7 @@ CONTAINS
     REAL(DP) :: Eta_X2(nNodesX(2))
     REAL(DP) :: Eta_X3(nNodesX(3))
 
-    ALLOCATE( ProjectionMatrix0(nDOFX,nDOFX,nDOFX) )
+    ALLOCATE( ProjectionMatrix0(nDOFX,nDOFX,nDOFX,nDOFX,nDOFX) )
 
     ProjectionMatrix0 = Zero
 
@@ -86,19 +89,47 @@ CONTAINS
 
         kS = kS + 1
 
+        lS = 0
+        DO lS3 = 1, nNodesX(3)
+        DO lS2 = 1, nNodesX(2)
+        DO lS1 = 1, nNodesX(1)
+
+        lS = lS + 1
+
+        mS = 0
+        DO mS3 = 1, nNodesX(3)
+        DO mS2 = 1, nNodesX(2)
+        DO mS1 = 1, nNodesX(1)
+
+        mS = mS + 1
+
         DO iN3 = 1, nNodesX(3)
         DO iN2 = 1, nNodesX(2)
         DO iN1 = 1, nNodesX(1)
 
-          ProjectionMatrix0(kS,jS,iS) &
-            = ProjectionMatrix0(kS,jS,iS) &
+          ProjectionMatrix0(kS,jS,lS,mS,iS) &
+            = ProjectionMatrix0(kS,jS,lS,mS,iS) &
                 + WeightsX1(iN1) * WeightsX2(iN2) * WeightsX3(iN3) &
                     * L_X1(IndLX_Q(1,jS)) % P( Eta_X1(iN1) ) &
                     * L_X2(IndLX_Q(2,jS)) % P( Eta_X2(iN2) ) &
                     * L_X3(IndLX_Q(3,jS)) % P( Eta_X3(iN3) ) &
                     * L_X1(IndLX_Q(1,kS)) % P( Eta_X1(iN1) ) &
                     * L_X2(IndLX_Q(2,kS)) % P( Eta_X2(iN2) ) &
-                    * L_X3(IndLX_Q(3,kS)) % P( Eta_X3(iN3) )
+                    * L_X3(IndLX_Q(3,kS)) % P( Eta_X3(iN3) ) &
+                    * L_X1(IndLX_Q(1,lS)) % P( Eta_X1(iN1) ) &
+                    * L_X2(IndLX_Q(2,lS)) % P( Eta_X2(iN2) ) &
+                    * L_X3(IndLX_Q(3,lS)) % P( Eta_X3(iN3) ) &
+                    * L_X1(IndLX_Q(1,mS)) % P( Eta_X1(iN1) ) &
+                    * L_X2(IndLX_Q(2,mS)) % P( Eta_X2(iN2) ) &
+                    * L_X3(IndLX_Q(3,mS)) % P( Eta_X3(iN3) )
+
+        END DO
+        END DO
+        END DO
+
+        END DO
+        END DO
+        END DO
 
         END DO
         END DO
@@ -133,8 +164,8 @@ CONTAINS
     INTEGER  :: IPIV(nDOFX)
     REAL(DP) :: WORK(nDOFX)
 
-    INTEGER  :: iX1, iX2, iX3, iDOFX, jDOFX, kDOFX, iS
-    REAL(DP) :: SUM1
+    INTEGER  :: iX1, iX2, iX3, iDOFX, jDOFX, kDOFX, lDOFX, mDOFX, iS
+    REAL(DP) :: SUM1, SqrtGm, Gm_dd_11, Gm_dd_22, Gm_dd_33
 
     ALLOCATE( ProjectionMatrix    (nDOFX,nDOFX,iX_B1(1):iX_E1(1),iX_B1(2):iX_E1(2),iX_B1(3):iX_E1(3)) )
     ALLOCATE( ReconstructionMatrix(nDOFX,nDOFX,iX_B1(1):iX_E1(1),iX_B1(2):iX_E1(2),iX_B1(3):iX_E1(3)) )
@@ -154,7 +185,19 @@ CONTAINS
 
         SUM1 = Zero
         DO kDOFX = 1, nDOFX
-          SUM1 = SUM1 + ProjectionMatrix0(jDOFX,kDOFX,iS) * uGF(kDOFX,iX1,iX2,iX3,iGF_SqrtGm)
+        DO lDOFX = 1, nDOFX
+        DO mDOFX = 1, nDOFX
+
+          CALL ComputeGeometryX_SpatialMetric &
+                 ( uGF(kDOFX,iX1,iX2,iX3,iGF_h_1), &
+                   uGF(lDOFX,iX1,iX2,iX3,iGF_h_2), &
+                   uGF(mDOFX,iX1,iX2,iX3,iGF_h_3), &
+                   Gm_dd_11, Gm_dd_22, Gm_dd_33, SqrtGm )
+
+          SUM1 = SUM1 + ProjectionMatrix0(jDOFX,kDOFX,lDOFX,mDOFX,iS) * SqrtGm
+
+        END DO
+        END DO
         END DO
 
         ProjectionMatrix(iS,jDOFX,iX1,iX2,iX3) = SUM1
