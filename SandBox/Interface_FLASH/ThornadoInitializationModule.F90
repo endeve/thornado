cb200089feddbf35ef29a621280e4a72f464fc54
frommste <svgtek@gmail.com>
2025-12-10 13:38:56 -0500

    Fix typo

--- SandBox/Interface_FLASH/ThornadoInitializationModule.F90
+++ SandBox/Interface_FLASH/ThornadoInitializationModule.F90
@@ -164,7 +164,7 @@ contains
       DnuMax_Option, FreezeOpacities_Option , &
       ActivateUnits_Option, CoordinateSystem_Option, &
       UseChemicalPotentialShift_Option, &
-      UseSimpleMeshRefinement_Option, Verbose_Option &
+      UseSimpleMeshRefinement_Option, Verbose_Option, &
       TimerStart_Option, TimerStop_Option )
 
     integer,  intent(in) :: nNodes, nDimsX, nE, swE, bcE, nSpecies
