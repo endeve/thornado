e2f908f5dd219420763ee6c6f5c0c4092db8a2b0
frommste <svgtek@gmail.com>
2025-12-10 11:26:43 -0500

    Add external timers

---
+++ Modules/Library/ExternalTimersModule.f90
@@ -0,0 +1,32 @@
+module ExternalTimersModule
+   implicit none
+
+   private
+
+   abstract interface
+      subroutine ExternalTimersType(name)
+         implicit none
+         character(len=*), intent(in) :: name
+      end subroutine ExternalTimersType
+   end interface
+
+   procedure(ExternalTimersType), pointer, save :: ExternalTimerStart => ExternalTimer_Default
+   procedure(ExternalTimersType), pointer, save :: ExternalTimerStop => ExternalTimer_Default
+
+   public :: SetExternalTimer
+   public :: ExternalTimerStart
+   public :: ExternalTimerStop
+
+contains
+
+   subroutine ExternalTimer_Default(name)
+      character(len=*), intent(in) :: name
+   end subroutine ExternalTimer_Default
+
+   subroutine SetExternalTimer(TimerStart, TimerStop)
+      procedure(ExternalTimersType) :: TimerStart, TimerStop
+
+      ExternalTimerStart => TimerStart
+      ExternalTimerStop  => TimerStop
+   end subroutine SetExternalTimer
+end module ExternalTimersModule
