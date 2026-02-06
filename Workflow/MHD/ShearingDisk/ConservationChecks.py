d62e5ea1687afed65d64c184598798462d2d8dc0
jbuffal <jbuffal1@utk.edu>
2026-02-06 13:49:43 -0500

    Add initial attempt at tally plot script.

---
+++ Workflow/MHD/ShearingDisk/ConservationChecks.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+ProblemName = sys.argv[1]
+FileDir     = sys.argv[2]
+PlotVar     = sys.argv[3]
+
+TallyFiles = {
+    "CM_D" : str(ProblemName) + ".Tally_BaryonicMass.dat",
+    "CM_S1": str(ProblemName) + ".Tally_MHDMomentumX1.dat",
+    "CM_S2": str(ProblemName) + ".Tally_MHDMomentumX2.dat",
+    "CM_S3": str(ProblemName) + ".Tally_MHDMomentumX3.dat",
+    "CM_E" : str(ProblemName) + ".Tally_MHDEnergy.dat",
+    "Tem11": str(ProblemName) + ".Tally_Tem11.dat",
+    "Tem33": str(ProblemName) + ".Tally_Tem33.dat"
+}
+
+TallyData = np.loadtxt(FileDir + "/" + TallyFiles[PlotVar], skiprows = 1)
+
+RelDiff = TallyData[:,4] / TallyData[:,3] 
+
+plt.plot(TallyData[:,0], RelDiff[:])
+plt.xlabel("Time [ms]")
+plt.ylabel("Relative Difference in " + str(PlotVar))
+plt.savefig(FileDir + "/" + TallyFiles[PlotVar] + ".png")
